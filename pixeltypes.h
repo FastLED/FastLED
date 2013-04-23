32165dbeaa2f8bac59051b6dc370d478aadc2633
danielgarcia@gmail.com <danielgarcia@gmail.com@4ad4ec5c-605d-bd5c-5796-512c9b60011b>
2013-04-23 01:38:54 +0000

    Add raw array access to hsv structure, and operator[] operations to hsv and rgb classes

--- pixeltypes.h
+++ pixeltypes.h
@@ -9,6 +9,7 @@ struct CRGB {
 		struct { uint8_t r; uint8_t g; uint8_t b; };
 		uint8_t raw[3];
 	};
+	inline uint8_t& operator[] (uint8_t x) __attribute__((always_inline)) { return raw[x]; }
 };
 
 #ifdef SUPPORT_ARGB
@@ -24,17 +25,24 @@ struct CARGB {
 
 
 struct CHSV {
-    union {
-        uint8_t hue;
-        uint8_t h; };
-    union {
-        uint8_t saturation;
-        uint8_t sat;
-        uint8_t s; };
-    union {
-        uint8_t value;
-        uint8_t val;
-        uint8_t v; };
+	union { 
+		struct {
+		    union {
+		        uint8_t hue;
+		        uint8_t h; };
+		    union {
+		        uint8_t saturation;
+		        uint8_t sat;
+		        uint8_t s; };
+		    union {
+		        uint8_t value;
+		        uint8_t val;
+		        uint8_t v; };
+		};
+		uint8_t raw[3];
+	};
+	inline uint8_t& operator[](uint8_t x) __attribute__((always_inline)) { return raw[x]; }
+
 };
 
 
