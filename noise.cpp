5abbf749036e0c684ab08e562c7485361dda3888
Mark Kriegsman <kriegsman@tr.org>
2014-06-02 15:56:24 -0400

    Restoring 'serpentine' functionality. Might be faster to move it out of the loop, but at least it's functional this way.

--- noise.cpp
+++ noise.cpp
@@ -472,7 +472,7 @@ void fill_2dnoise8(CRGB *leds, int width, int height, bool serpentine,
       CRGB led(CHSV(H[h1-i][w1-j],255,V[i][j]));
 
       int pos = j;
-      if(serpentine) {
+      if(serpentine && (i & 0x1)) {
         pos = w1-j;
       }
 
@@ -505,7 +505,7 @@ void fill_2dnoise16(CRGB *leds, int width, int height, bool serpentine,
       CRGB led(CHSV(H[h1-i][w1-j],255,V[i][j]));
 
       int pos = j;
-      if(serpentine) {
+      if(serpentine && (i & 0x1)) {
         pos = w1-j;
       }
 
