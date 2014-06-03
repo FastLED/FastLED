552411bc6df525f291fa3cf2d823d980a5992c25
Daniel Garcia <danielgarcia@gmail.com>
2014-06-03 10:23:00 -0700

    Fix 8bit compile issue

--- lib8tion.h
+++ lib8tion.h
@@ -1584,14 +1584,18 @@ public:
   uint16_t operator*(uint16_t v) { return (v*i) + ((v*f)>>F); }
   int32_t operator*(int32_t v) { return (v*i) + ((v*f)>>F); }
   int16_t operator*(int16_t v) { return (v*i) + ((v*f)>>F); }
+#ifdef FASTLED_ARM
   int operator*(int v) { return (v*i) + ((v*f)>>F); }
+#endif
 };
 
 template<class T, int F, int I> static uint32_t operator*(uint32_t v, q<T,F,I> & q) { return q * v; }
 template<class T, int F, int I> static uint16_t operator*(uint16_t v, q<T,F,I> & q) { return q * v; }
 template<class T, int F, int I> static int32_t operator*(int32_t v, q<T,F,I> & q) { return q * v; }
 template<class T, int F, int I> static int16_t operator*(int16_t v, q<T,F,I> & q) { return q * v; }
+#ifdef FASTLED_ARM
 template<class T, int F, int I> static int operator*(int v, q<T,F,I> & q) { return q * v; }
+#endif
 
 typedef q<uint8_t, 4,4> q44;
 typedef q<uint8_t, 6,2> q62;
