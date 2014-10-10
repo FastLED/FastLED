36c48066c20863716e24adfa2f9282c7e0429107
Daniel Garcia <danielgarcia@gmail.com>
2014-10-09 22:02:09 -0700

    Bring DMX kicking and screaming into the FastLED2.1 world

--- FastLED.h
+++ FastLED.h
@@ -13,6 +13,14 @@
 #include<SmartMatrix.h>
 #endif
 
+#ifdef DmxSimple_h
+#include<DmxSimple.h>
+#endif
+
+#ifdef DmxSerial_h
+#include<DMXSerial.h>
+#endif
+
 #include "controller.h"
 #include "fastpin.h"
 #include "fastspi.h"
@@ -22,7 +30,7 @@
 #include "colorutils.h"
 #include "colorpalettes.h"
 #include "chipsets.h"
-#include "dmx.h"
+#include "./dmx.h"
 #include "smartmatrix_t3.h"
 #include "noise.h"
 #include "power_mgt.h"
@@ -38,23 +46,6 @@ enum ESPIChipsets {
 
 enum ESM { SMART_MATRIX };
 
-enum EClocklessChipsets {
-	DMX
-	// TM1809,
-	// TM1804,
-	// TM1803,
-	// WS2811,
-	// WS2812,
-	// WS2812B,
-	// UCS1903,
-	// UCS1903B,
-	// WS2811_400,
-	// // NEOPIXEL,
-	// GW6205,
-	// GW6205_400,
-	// TM1829
-};
-
 template<uint8_t DATA_PIN> class NEOPIXEL : public WS2811Controller800Khz<DATA_PIN, GRB> {};
 template<uint8_t DATA_PIN, EOrder RGB_ORDER> class TM1829 : public TM1829Controller800Khz<DATA_PIN, RGB_ORDER> {};
 template<uint8_t DATA_PIN, EOrder RGB_ORDER> class TM1809 : public TM1809Controller800Khz<DATA_PIN, RGB_ORDER> {};
@@ -69,6 +60,12 @@ template<uint8_t DATA_PIN, EOrder RGB_ORDER> class WS2811_400 : public WS2811Con
 template<uint8_t DATA_PIN, EOrder RGB_ORDER> class GW6205 : public GW6205Controller800Khz<DATA_PIN, RGB_ORDER> {};
 template<uint8_t DATA_PIN, EOrder RGB_ORDER> class GW6205_400 : public GW6205Controller400Khz<DATA_PIN, RGB_ORDER> {};
 template<uint8_t DATA_PIN, EOrder RGB_ORDER> class LPD1886 : public LPD1886Controller1250Khz<DATA_PIN, RGB_ORDER> {};
+#ifdef DmxSimple_h
+template<uint8_t DATA_PIN, EOrder RGB_ORDER> class DMXSIMPLE : public DMXSimpleController<DATA_PIN, RGB_ORDER> {};
+#endif
+#ifdef DmxSerial_h
+template<EOrder RGB_ORDER> class DMXSERIAL : public DMXSerialController<RGB_ORDER> {};
+#endif
 
 // template <uint8_t DATA_PIN, uint8_t CLOCK_PIN, EOrder RGB_ORDER = RGB,  uint8_t SPI_SPEED = DATA_RATE_MHZ(20)> class LPD8806 : public LPD8806Controller<DATA_PIN, CLOCK_PIN, RGB_ORDER, SPI_SPEED> {};
 // template <uint8_t DATA_PIN, uint8_t CLOCK_PIN, EOrder RGB_ORDER = RGB,  uint8_t SPI_SPEED = DATA_RATE_MHZ(1)> class WS2801 : public WS2801Controller<DATA_PIN, CLOCK_PIN, RGB_ORDER, SPI_SPEED> {};
@@ -160,29 +157,24 @@ public:
 		return addLeds(&c, data, nLedsOrOffset, nLedsIfOffset);
 	}
 
+	template<template<EOrder RGB_ORDER> class CHIPSET>
+	static CLEDController &addLeds(struct CRGB *data, int nLedsOrOffset, int nLedsIfOffset = 0) {
+		static CHIPSET<RGB> c;
+		return addLeds(&c, data, nLedsOrOffset, nLedsIfOffset);
+	}
+
+	template<template<EOrder RGB_ORDER> class CHIPSET, EOrder RGB_ORDER>
+	static CLEDController &addLeds(struct CRGB *data, int nLedsOrOffset, int nLedsIfOffset = 0) {
+		static CHIPSET<RGB_ORDER> c;
+		return addLeds(&c, data, nLedsOrOffset, nLedsIfOffset);
+	}
+
 #ifdef SmartMatrix_h
 	template<ESM CHIPSET>
 	static CLEDController &addLeds(struct CRGB *data, int nLedsOrOffset, int nLedsIfOffset = 0)
 	{
 		switch(CHIPSET) {
 			case SMART_MATRIX: { static CSmartMatrixController controller; return addLeds(&controller, data, nLedsOrOffset, nLedsIfOffset); }
-		}
-	}
-#endif
-
-#ifdef FASTSPI_USE_DMX_SIMPLE
-	template<EClocklessChipsets CHIPSET, uint8_t DATA_PIN>
-	static CLEDController &addLeds(struct CRGB *data, int nLedsOrOffset, int nLedsIfOffset = 0)
-	 {
-		switch(CHIPSET) {
-			case DMX: { static DMXController<DATA_PIN> controller; return addLeds(&controller, data, nLedsOrOffset, nLedsIfOffset); }
-		}
-	}
-
-	template<EClocklessChipsets CHIPSET, uint8_t DATA_PIN, EOrder RGB_ORDER>
-	static CLEDController &addLeds(struct CRGB *data, int nLedsOrOffset, int nLedsIfOffset = 0) {
-		switch(CHIPSET) {
-			case DMX: {static  DMXController<DATA_PIN, RGB_ORDER> controller; return addLeds(&controller, data, nLedsOrOffset, nLedsIfOffset); }
 		}
 	}
 #endif
