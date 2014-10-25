b0a94d0732d187de08f3912897bdc5a1fe02c182
Daniel Garcia <danielgarcia@gmail.com>
2014-10-24 22:10:15 -0400

    Cleaning up the k20 blockless controller w/faster bit swapping

--- clockless_block_arm_k20.h
+++ clockless_block_arm_k20.h
@@ -5,6 +5,11 @@
 // See clockless.h for detailed info on how the template parameters are used.
 #if defined(FASTLED_TEENSY3)
 #define HAS_BLOCKLESS 1
+#define PORT_MASK 0x000000FF
+#define SKIPLIST ~PORT_MASK
+#define LANES 8
+
+#include "kinetis.h"
 
 template <uint8_t NUM_LANES, int T1, int T2, int T3, EOrder RGB_ORDER = RGB, int XTRA0 = 0, bool FLIP = false, int WAIT_TIME = 500>
 class InlineBlockClocklessController : public CLEDController {
@@ -34,17 +39,11 @@ public:
 
 	// set all the leds on the controller to a given color
 	virtual void showColor(const struct CRGB & rgbdata, int nLeds, CRGB scale) {
+		MultiPixelController<LANES,PORT_MASK,RGB_ORDER> pixels(rgbdata,nLeds, scale, getDither() );
 		mWait.wait();
 		cli();
 
-		uint32_t clocks = showRGBInternal(PixelController<RGB_ORDER>(rgbdata, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/*/+nLeds/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/*/+nLeds/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/*/+nLeds/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/*/+nLeds/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/*/+nLeds/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/*/+(2*nLeds)/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/*/+(3*nLeds)/**/, nLeds, scale, getDither()));
+		uint32_t clocks = showRGBInternal(pixels,nLeds);
 
 		// Adjust the timer
 		long microsTaken = CLKS_TO_MICROS(clocks);
@@ -54,17 +53,11 @@ public:
 	}
 
 	virtual void show(const struct CRGB *rgbdata, int nLeds, CRGB scale) {
+		MultiPixelController<LANES,PORT_MASK,RGB_ORDER> pixels(rgbdata,nLeds, scale, getDither() );
 		mWait.wait();
 		cli();
 
-		uint32_t clocks = showRGBInternal(PixelController<RGB_ORDER>(rgbdata, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/**/+(1*nLeds)/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/**/+(2*nLeds)/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/**/+(3*nLeds)/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/**/+(4*nLeds)/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/**/+(5*nLeds)/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/**/+(6*nLeds)/**/, nLeds, scale, getDither()),
-						PixelController<RGB_ORDER>(rgbdata/**/+(7*nLeds)/**/, nLeds, scale, getDither()));
+		uint32_t clocks = showRGBInternal(pixels,nLeds);
 
 		// Adjust the timer
 		long microsTaken = CLKS_TO_MICROS(clocks);
@@ -89,44 +82,6 @@ public:
 	}
 #endif
 
-	typedef union {
-		uint32_t word;
-		struct {
-			uint32_t a0:1;
-			uint32_t a1:1;
-			uint32_t a2:1;
-			uint32_t a3:1;
-			uint32_t a4:1;
-			uint32_t a5:1;
-			uint32_t a6:1;
-			uint32_t a7:1;
-			uint32_t b0:1;
-			uint32_t b1:1;
-			uint32_t b2:1;
-			uint32_t b3:1;
-			uint32_t b4:1;
-			uint32_t b5:1;
-			uint32_t b6:1;
-			uint32_t b7:1;
-			uint32_t c0:1;
-			uint32_t c1:1;
-			uint32_t c2:1;
-			uint32_t c3:1;
-			uint32_t c4:1;
-			uint32_t c5:1;
-			uint32_t c6:1;
-			uint32_t c7:1;
-			uint32_t d0:1;
-			uint32_t d1:1;
-			uint32_t d2:1;
-			uint32_t d3:1;
-			uint32_t d4:1;
-			uint32_t d5:1;
-			uint32_t d6:1;
-			uint32_t d7:1;
-		};
-	} xtype;
-
 
 #define USE_LINES
 	typedef union {
@@ -134,104 +89,63 @@ public:
 		uint32_t raw[2];
 	} Lines;
 
-	__attribute__ ((always_inline)) inline static uint32_t bits(Lines & b) {
-		xtype fuckery;
-		fuckery.word = 0;
-		fuckery.a0 = (b.raw[0] >> 31);
-		fuckery.a1 = (b.raw[0] >> 23);
-		fuckery.a2 = (b.raw[0] >> 15);
-		fuckery.a3 = (b.raw[0] >> 7);
-		fuckery.a4 = (b.raw[1] >> 31);
-		fuckery.a5 = (b.raw[1] >> 23);
-		fuckery.a6 = (b.raw[1] >> 15);
-		fuckery.a7 = (b.raw[1] >> 7);
-		// fuckery.b0 = (b.raw[2] >> 30);
-		// fuckery.b1 = (b.raw[2] >> 22);
-		// fuckery.b2 = (b.raw[2] >> 14);
-		// fuckery.b3 = (b.raw[2] >> 6);
-		// fuckery.b4 = (b.raw[3] >> 30);
-		// fuckery.b5 = (b.raw[3] >> 22)
-		// fuckery.b6 = (b.raw[3] >> 14);
-		// fuckery.b7 = (b.raw[3] >> 6);
-		// fuckery.c0 = (b.raw[4] >> 29);
-		// fuckery.c1 = (b.raw[4] >> 21);
-		// fuckery.c2 = (b.raw[4] >> 13);
-		// fuckery.c3 = (b.raw[4] >> 5);
-		// fuckery.c4 = (b.raw[5] >> 20);
-		// fuckery.c5 = (b.raw[5] >> 21);
-		// fuckery.c6 = (b.raw[5] >> 13);
-		// fuckery.c7 = (b.raw[5] >> 5);
-		// fuckery.d0 = (b.raw[6] >> 28);
-		// fuckery.d1 = (b.raw[6] >> 19);
-		// fuckery.d2 = (b.raw[6] >> 12);
-		// fuckery.d3 = (b.raw[6] >> 4);
-		// fuckery.d4 = (b.raw[7] >> 28);
-		// fuckery.d5 = (b.raw[7] >> 19);
-		// fuckery.d6 = (b.raw[7] >> 12);
-		// fuckery.d7 = (b.raw[7] >> 4);
-		return fuckery.word;
-	}
+	template<int BITS,int PX> __attribute__ ((always_inline)) inline static void writeBits(register uint32_t & next_mark, register Lines & b, Lines & b2, MultiPixelController<LANES, PORT_MASK, RGB_ORDER> &pixels) { // , register uint32_t & b2)  {
+		transpose8(b.raw,b2.raw);
+		register uint8_t d = pixels.template getd<PX>(pixels);
+		register uint8_t scale = pixels.template getscale<PX>(pixels);
 
-	template<int BITS,int PX> __attribute__ ((always_inline)) inline static void writeBits(register uint32_t & next_mark, register Lines & b, Lines & b2, PixelController<RGB_ORDER> *allpixels[8]) { // , register uint32_t & b2)  {
-		uint32_t flipper = bits(b);
 		for(register uint32_t i = 0; i < 8; i++) {
 			while(ARM_DWT_CYCCNT < next_mark);
 			next_mark = ARM_DWT_CYCCNT + (T1+T2+T3);
 			*FastPin<2>::port() = 0xFF;
-			uint32_t flip_mark = next_mark - (T2+T3+2);
+			// uint32_t flip_mark = next_mark - (T2+T3+2);
 
-			while(ARM_DWT_CYCCNT < flip_mark);
-			uint32_t last_flip_mark = next_mark - (T3);
-			*FastPin<2>::port() = flipper;
-			b.raw[0] <<= 1;
-			b.raw[1] <<= 1;
-			flipper = bits(b);
+			while((next_mark - ARM_DWT_CYCCNT) > (T2+T3+6));
+			// while(ARM_DWT_CYCCNT < flip_mark);
+			// uint32_t last_flip_mark = next_mark - (T3);
+			*FastPin<2>::port() = ~b2.raw[i];
 
-
-			while(ARM_DWT_CYCCNT < last_flip_mark);
+			while((next_mark - ARM_DWT_CYCCNT) > (T3));
+			// while(ARM_DWT_CYCCNT < last_flip_mark);
 			*FastPin<2>::port() = 0;
-			switch(PX) {
-				case 0: b2.bytes[i] = allpixels[i]->advanceAndLoadAndScale0(); break;
-				case 1: b2.bytes[i] = allpixels[i]->loadAndScale1(); break;
-				case 2: b2.bytes[i] = allpixels[i]->loadAndScale2(); break;
-			}
+
+			b.bytes[i] = pixels.template loadAndScale<PX>(pixels,i,d,scale);
 		}
 	}
 
+
+
 	// This method is made static to force making register Y available to use for data on AVR - if the method is non-static, then
 	// gcc will use register Y for the this pointer.
-	static uint32_t showRGBInternal(PixelController<RGB_ORDER> pixels, PixelController<RGB_ORDER> pixels2, PixelController<RGB_ORDER> pixels3, PixelController<RGB_ORDER> pixels4,
-								PixelController<RGB_ORDER> pixels5,  PixelController<RGB_ORDER> pixels6, PixelController<RGB_ORDER> pixels7,  PixelController<RGB_ORDER> pixels8) {
+		static uint32_t showRGBInternal(MultiPixelController<LANES, PORT_MASK, RGB_ORDER> &allpixels, int nLeds) {
 		// Get access to the clock
 		ARM_DEMCR    |= ARM_DEMCR_TRCENA;
 		ARM_DWT_CTRL |= ARM_DWT_CTRL_CYCCNTENA;
 		ARM_DWT_CYCCNT = 0;
 
 		// Setup the pixel controller and load/scale the first byte
-		PixelController<RGB_ORDER> *allpixels[8] = {&pixels,&pixels2,&pixels3,&pixels4,&pixels5,&pixels6,&pixels7,&pixels8};
-		pixels.preStepFirstByteDithering();
-		register Lines b0,b1,b2;
+		allpixels.preStepFirstByteDithering();
+		register Lines b0,b1;
 		b0.raw[0] = b0.raw[1] = 0;
 		b1.raw[0] = b1.raw[1] = 0;
-		b2.raw[0] = b2.raw[1] = 0;
+		allpixels->preStepFirstByteDithering();
 		for(int i = 0; i < 8; i++) {
-			allpixels[i]->preStepFirstByteDithering();
-			b0.bytes[i] = allpixels[i]->loadAndScale0();
+			b0.bytes[i] = allpixels->loadAndScale0(i);
 		}
 
 		uint32_t next_mark = ARM_DWT_CYCCNT + (T1+T2+T3);
 
-		while(pixels.has(1)) {
-			pixels.stepDithering();
+		while(nLeds--) {
+			allpixels.stepDithering();
 
 			// Write first byte, read next byte
 			writeBits<8+XTRA0,1>(next_mark, b0, b1, allpixels);
 
 			// Write second byte, read 3rd byte
-			writeBits<8+XTRA0,2>(next_mark, b1, b2, allpixels);
+			writeBits<8+XTRA0,2>(next_mark, b0, b1, allpixels);
 
 			// Write third byte
-			writeBits<8+XTRA0,0>(next_mark, b2, b0, allpixels);
+			writeBits<8+XTRA0,0>(next_mark, b0, b1, allpixels);
 		};
 
 		return ARM_DWT_CYCCNT;
