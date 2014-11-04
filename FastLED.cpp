6ca2090e95ca2a5c9d4bb79fc06c5e6c5a122f34
Daniel Garcia <danielgarcia@gmail.com>
2014-11-04 11:09:13 -0800

    Add extra C++ bits that some environments might need

--- FastLED.cpp
+++ FastLED.cpp
@@ -202,3 +202,34 @@ void CFastLED::setMaxRefreshRate(uint16_t refresh) {
 			m_nMinMicros = 0;
 		}
 }
+
+
+#ifdef NEED_CXX_BITS
+namespace __cxxabiv1
+{
+	extern "C" void __cxa_pure_virtual (void) {}
+	/* guard variables */
+
+	/* The ABI requires a 64-bit type.  */
+	__extension__ typedef int __guard __attribute__((mode(__DI__)));
+
+	extern "C" int __cxa_guard_acquire (__guard *);
+	extern "C" void __cxa_guard_release (__guard *);
+	extern "C" void __cxa_guard_abort (__guard *);
+
+	extern "C" int __cxa_guard_acquire (__guard *g)
+	{
+		return !*(char *)(g);
+	}
+
+	extern "C" void __cxa_guard_release (__guard *g)
+	{
+		*(char *)g = 1;
+	}
+
+	extern "C" void __cxa_guard_abort (__guard *)
+	{
+
+	}
+}
+#endif
