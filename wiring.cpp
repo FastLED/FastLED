6674a8cb67d55ab48ed0047298fe7fd78fdcdca6
Daniel Garcia <danielgarcia@gmail.com>
2014-11-12 09:57:35 -0800

    Improve the math a bit

--- wiring.cpp
+++ wiring.cpp
@@ -46,7 +46,7 @@ unsigned long millis()
         m = FastLED_timer0_overflow_count;  //._long;
         SREG = oldSREG;
 
-        return (m*MICROSECONDS_PER_TIMER0_OVERFLOW)/1000;
+        return (m*(MICROSECONDS_PER_TIMER0_OVERFLOW/8))/(1000/8);
 }
 
 unsigned long micros() {
@@ -89,73 +89,6 @@ void delay(unsigned long ms)
         }
 }
 
-/* Delay for the given number of microseconds.  Assumes a 8 or 16 MHz clock. */
-void delayMicroseconds(unsigned int us)
-{
-        // calling avrlib's delay_us() function with low values (e.g. 1 or
-        // 2 microseconds) gives delays longer than desired.
-        //delay_us(us);
-#if F_CPU >= 20000000L
-        // for the 20 MHz clock on rare Arduino boards
-
-        // for a one-microsecond delay, simply wait 2 cycle and return. The overhead
-        // of the function call yields a delay of exactly a one microsecond.
-        __asm__ __volatile__ (
-                "nop" "\n\t"
-                "nop"); //just waiting 2 cycle
-        if (--us == 0)
-                return;
-
-        // the following loop takes a 1/5 of a microsecond (4 cycles)
-        // per iteration, so execute it five times for each microsecond of
-        // delay requested.
-        us = (us<<2) + us; // x5 us
-
-        // account for the time taken in the preceeding commands.
-        us -= 2;
-
-#elif F_CPU >= 16000000L
-        // for the 16 MHz clock on most Arduino boards
-
-        // for a one-microsecond delay, simply return.  the overhead
-        // of the function call yields a delay of approximately 1 1/8 us.
-        if (--us == 0)
-                return;
-
-        // the following loop takes a quarter of a microsecond (4 cycles)
-        // per iteration, so execute it four times for each microsecond of
-        // delay requested.
-        us <<= 2;
-
-        // account for the time taken in the preceeding commands.
-        us -= 2;
-#else
-        // for the 8 MHz internal clock on the ATmega168
-
-        // for a one- or two-microsecond delay, simply return.  the overhead of
-        // the function calls takes more than two microseconds.  can't just
-        // subtract two, since us is unsigned; we'd overflow.
-        if (--us == 0)
-                return;
-        if (--us == 0)
-                return;
-
-        // the following loop takes half of a microsecond (4 cycles)
-        // per iteration, so execute it twice for each microsecond of
-        // delay requested.
-        us <<= 1;
-
-        // partially compensate for the time taken by the preceeding commands.
-        // we can't subtract any more than this or we'd overflow w/ small delays.
-        us--;
-#endif
-
-        // busy wait
-        __asm__ __volatile__ (
-                "1: sbiw %0,1" "\n\t" // 2 cycles
-                "brne 1b" : "=w" (us) : "0" (us) // 2 cycles
-        );
-}
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 void init()
 {
