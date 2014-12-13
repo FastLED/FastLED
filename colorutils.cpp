f873455c0f27d7bfacf744effc681f585dc52c19
Mark Kriegsman <kriegsman@tr.org>
2014-12-12 20:59:26 -0500

    Added nblendPaletteTowardPalette as a toplevel function for palette crossfade support... I suspect there will be some small changes coming to this, but here it is.

--- colorutils.cpp
+++ colorutils.cpp
@@ -723,3 +723,32 @@ void SetupPartyColors(CRGBPalette16& pal)
 }
 #endif
 
+
+void nblendPaletteTowardPalette( CRGBPalette16& current, CRGBPalette16& target, uint8_t maxChanges)
+{
+    uint8_t* p1;
+    uint8_t* p2;
+    uint8_t  changes = 0;
+    
+    p1 = (uint8_t*)current.entries;
+    p2 = (uint8_t*)target.entries;
+    
+    const uint8_t totalChannels = sizeof(CRGBPalette16);
+    for( byte i = 0; i < totalChannels; i++) {
+        // if the values are equal, no changes are needed
+        if( p1[i] == p2[i] ) { continue; }
+        
+        // if the current value is less than the target, increase it by one
+        if( p1[i] < p2[i] ) { p1[i]++; changes++; }
+        
+        // if the current value is greater than the target,
+        // increase it by one (or two if it's still greater).
+        if( p1[i] > p2[i] ) {
+            p1[i]--; changes++;
+            if( p1[i] > p2[i] ) { p1[i]--; }
+        }
+        
+        // if we've hit the maximum number of changes, exit
+        if( changes >= maxChanges) { break; }
+    }
+}
