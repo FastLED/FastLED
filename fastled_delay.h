c8eb409541eef6f8eb6591d34c8487df1a078190
Daniel Garcia <danielgarcia@gmail.com>
2015-08-08 18:18:21 -0700

    Make sure clock timings adjust in a way such that we never go too fast.

--- fastled_delay.h
+++ fastled_delay.h
@@ -104,13 +104,11 @@ template<> __attribute__((always_inline)) inline void delaycycles<5>() {NOP2;NOP
 
 // Macro to convert from nano-seconds to clocks and clocks to nano-seconds
 // #define NS(_NS) (_NS / (1000 / (F_CPU / 1000000L)))
-#if 1 || (F_CPU < 96000000)
-#define NS(_NS) ( (_NS * (F_CPU / 1000000L))) / 1000
+#define F_CPU_MHZ (F_CPU / 1000000L)
+
+// #define NS(_NS) ( (_NS * (F_CPU / 1000000L))) / 1000
+#define NS(_NS) (((_NS * F_CPU_MHZ) + 999) / 1000)
 #define CLKS_TO_MICROS(_CLKS) ((long)(_CLKS)) / (F_CPU / 1000000L)
-#else
-#define NS(_NS) ( (_NS * (F_CPU / 2000000L))) / 1000
-#define CLKS_TO_MICROS(_CLKS) ((long)(_CLKS)) / (F_CPU / 2000000L)
-#endif
 
 //  Macro for making sure there's enough time available
 #define NO_TIME(A, B, C) (NS(A) < 3 || NS(B) < 3 || NS(C) < 6)
