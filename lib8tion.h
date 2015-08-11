0aadeaa3b407b01e861bc7b70f62dde1d5c5e2a0
Daniel Garcia <danielgarcia@gmail.com>
2015-08-10 22:47:23 -0700

    Adding definitions to various lib8tion types.

--- lib8tion.h
+++ lib8tion.h
@@ -319,26 +319,36 @@ Lib8tion is pronounced like 'libation': lie-BAY-shun
 //                 in steps of 0.00003051757
 //
 
-typedef uint8_t   fract8;   // ANSI: unsigned short _Fract
-typedef int8_t    sfract7;  // ANSI: signed   short _Fract
-typedef uint16_t  fract16;  // ANSI: unsigned       _Fract
-typedef int16_t   sfract15; // ANSI: signed         _Fract
+/// ANSI unsigned short _Fract.  range is 0 to 0.99609375
+///                 in steps of 0.00390625
+typedef uint8_t   fract8;   ///< ANSI: unsigned short _Fract
+
+///  ANSI: signed short _Fract.  range is -0.9921875 to 0.9921875
+///                 in steps of 0.0078125
+typedef int8_t    sfract7;  ///< ANSI: signed   short _Fract
+
+///  ANSI: unsigned _Fract.  range is 0 to 0.99998474121
+///                 in steps of 0.00001525878
+typedef uint16_t  fract16;  ///< ANSI: unsigned       _Fract
+
+///  ANSI: signed _Fract.  range is -0.99996948242 to 0.99996948242
+///                 in steps of 0.00003051757
+typedef int16_t   sfract15; ///< ANSI: signed         _Fract
 
 
 // accumXY types should be interpreted as X bits of integer,
 //         and Y bits of fraction.
 //         E.g., accum88 has 8 bits of int, 8 bits of fraction
 
-typedef uint16_t  accum88;  // ANSI: unsigned short _Accum
-typedef int16_t   saccum78; // ANSI: signed   short _Accum
-typedef uint32_t  accum1616;// ANSI: signed         _Accum
-typedef int32_t   saccum1516;//ANSI: signed         _Accum
-typedef uint16_t  accum124; // no direct ANSI counterpart
-typedef int32_t   saccum114;// no direct ANSI counterpart
+typedef uint16_t  accum88;  ///< ANSI: unsigned short _Accum.  8 bits int, 8 bits fraction
+typedef int16_t   saccum78; ///< ANSI: signed   short _Accum.  7 bits int, 8 bits fraction
+typedef uint32_t  accum1616;///< ANSI: signed         _Accum. 16 bits int, 16 bits fraction
+typedef int32_t   saccum1516;///< ANSI: signed         _Accum. 15 bits int, 16 bits fraction
+typedef uint16_t  accum124; ///< no direct ANSI counterpart. 12 bits int, 4 bits fraction
+typedef int32_t   saccum114;///< no direct ANSI counterpart. 1 bit int, 14 bits fraction
 
 
-// typedef for IEEE754 "binary32" float type internals
-
+/// typedef for IEEE754 "binary32" float type internals
 typedef union {
     uint32_t i;
     float    f;
@@ -729,7 +739,7 @@ LIB8STATIC uint8_t squarewave8( uint8_t in, uint8_t pulsewidth=128)
 
 
 
-
+/// Template class for represneting fractional ints.
 template<class T, int F, int I> class q {
   T i:I;
   T f:F;
@@ -753,9 +763,13 @@ template<class T, int F, int I> static int16_t operator*(int16_t v, q<T,F,I> & q
 template<class T, int F, int I> static int operator*(int v, q<T,F,I> & q) { return q * v; }
 #endif
 
+/// A 4.4 integer (4 bits integer, 4 bits fraction)
 typedef q<uint8_t, 4,4> q44;
+/// A 6.2 integer (6 bits integer, 2 bits fraction)
 typedef q<uint8_t, 6,2> q62;
+/// A 8.8 integer (8 bits integer, 8 bits fraction)
 typedef q<uint16_t, 8,8> q88;
+/// A 12.4 integer (12 bits integer, 4 bits fraction)
 typedef q<uint16_t, 12,4> q124;
 
 
