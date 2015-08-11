e421cdc8d843dfb0072b76e98267a052c6432926
Daniel Garcia <danielgarcia@gmail.com>
2015-08-10 21:59:01 -0700

    adding new files

---
+++ docs/3.1/search/groups_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['noise_20functions',['Noise functions',['../group___noise.html',1,'']]]
+];
---
+++ docs/3.1/search/groups_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['scaling_20functions',['Scaling functions',['../group___scaling.html',1,'']]]
+];
