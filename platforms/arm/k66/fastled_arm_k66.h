7a064b9f16c834c14ef2d7b9c1454fb7d6df632d
Daniel Garcia <danielgarcia@gmail.com>
2017-08-16 13:49:35 -0700

    Fix #475, add support for the octows2811 adapter

--- platforms/arm/k66/fastled_arm_k66.h
+++ platforms/arm/k66/fastled_arm_k66.h
@@ -6,9 +6,10 @@
 #include "fastled_delay.h"
 #include "fastpin_arm_k66.h"
 #include "fastspi_arm_k66.h"
-//#include "octows2811_controller.h"
-//#include "smartmatrix_t3.h"
+#include "../k20/octows2811_controller.h"
+#include "../k20/smartmatrix_t3.h"
 #include "clockless_arm_k66.h"
 #include "clockless_block_arm_k66.h"
 
 #endif
+
