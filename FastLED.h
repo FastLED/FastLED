fcb2030512b883c5bc574401b2025706c35976a7
Daniel Garcia <dgarcia@dgarcia.net>
2019-01-20 15:29:16 -0800

    bumping version number for typo

--- FastLED.h
+++ FastLED.h
@@ -8,12 +8,12 @@
 #define FASTLED_HAS_PRAGMA_MESSAGE
 #endif
 
-#define FASTLED_VERSION 3002001
+#define FASTLED_VERSION 3002003
 #ifndef FASTLED_INTERNAL
 #  ifdef FASTLED_HAS_PRAGMA_MESSAGE
-#    pragma message "FastLED version 3.002.002"
+#    pragma message "FastLED version 3.002.003"
 #  else
-#    warning FastLED version 3.002.002  (Not really a warning, just telling you here.)
+#    warning FastLED version 3.002.003  (Not really a warning, just telling you here.)
 #  endif
 #endif
 
