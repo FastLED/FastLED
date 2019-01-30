6af86cea6cbe110b0ec9432c52db9e74d1704717
Daniel Garcia <dgarcia@dgarcia.net>
2019-01-30 15:23:16 -0800

    Bumping version

--- FastLED.h
+++ FastLED.h
@@ -8,12 +8,12 @@
 #define FASTLED_HAS_PRAGMA_MESSAGE
 #endif
 
-#define FASTLED_VERSION 3002005
+#define FASTLED_VERSION 3002006
 #ifndef FASTLED_INTERNAL
 #  ifdef FASTLED_HAS_PRAGMA_MESSAGE
-#    pragma message "FastLED version 3.002.005"
+#    pragma message "FastLED version 3.002.006"
 #  else
-#    warning FastLED version 3.002.005  (Not really a warning, just telling you here.)
+#    warning FastLED version 3.002.006  (Not really a warning, just telling you here.)
 #  endif
 #endif
 
