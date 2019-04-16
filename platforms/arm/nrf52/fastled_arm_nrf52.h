9c5f5696bbf829a91e1ab2f15fc62162ce0ffc40
Henry Gabryjelski <henrygab@users.noreply.github.com>
2019-04-15 23:13:18 -0700

    Add nRF52 FastPIN support.

---
+++ platforms/arm/nrf52/fastled_arm_nrf52.h
@@ -0,0 +1,11 @@
+#ifndef __INC_FASTLED_ARM_NRF52_H
+#define __INC_FASTLED_ARM_NRF52_H
+
+// at this point, more of a TODO list
+
+#include "fastpin_arm_nrf52.h"
+// #include "fastspi_arm_nrf52.h"
+// #include "clockless_arm_nrf52.h"
+
+#endif // #ifndef __INC_FASTLED_ARM_NRF52_H
+
