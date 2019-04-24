23c3273caaf6cee4bf8f622bf35436a6da80d9ff
Henry Gabryjelski <henrygab@users.noreply.github.com>
2019-04-24 09:04:59 -0700

    Add nRF52 FastSPI support.
    This is part 2 of 3 to get full nRF52 support.
    These parts are:
    1. FastPIN
    2. FastSPI
    3. Clockless

--- platforms/arm/nrf52/fastled_arm_nrf52.h
+++ platforms/arm/nrf52/fastled_arm_nrf52.h
@@ -2,7 +2,7 @@
 #define __INC_FASTLED_ARM_NRF52_H
 
 #include "fastpin_arm_nrf52.h"
-// #include "fastspi_arm_nrf52.h"
+#include "fastspi_arm_nrf52.h"
 // #include "clockless_arm_nrf52.h"
 
 #endif // #ifndef __INC_FASTLED_ARM_NRF52_H
--- platforms/arm/nrf52/led_sysdefs_arm_nrf52.h
+++ platforms/arm/nrf52/led_sysdefs_arm_nrf52.h
@@ -1,23 +1,27 @@
 #ifndef __LED_SYSDEFS_ARM_NRF52
 #define __LED_SYSDEFS_ARM_NRF52
 
-
 #define FASTLED_ARM
 
 #ifndef F_CPU
     #define F_CPU 64000000 // the NRF52 series has a 64MHz CPU
 #endif
 
-// #define FASTLED_HAS_MILLIS
-#define FASTLED_USE_PROGMEM 0 // nRF52 series have flat memory model
-#define FASTLED_FORCE_SOFTWARE_SPI
+#if !defined(FASTLED_USE_PROGMEM)
+    #define FASTLED_USE_PROGMEM 0 // nRF52 series have flat memory model
+#endif
+
+#if !defined(FASTLED_ALLOW_INTERRUPTS)
+    #define FASTLED_ALLOW_INTERRUPTS 1
+#endif
 
 #include <nrfx.h>
+#include <drivers\include\nrfx_spim.h>
 
 typedef __I  uint32_t RoReg;
 typedef __IO uint32_t RwReg;
 
 #define cli()  __disable_irq();
-#define sei() __enable_irq();
+#define sei()  __enable_irq();
 
 #endif
