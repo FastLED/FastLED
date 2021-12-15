367c356dbbb5abe1e2b7b43343c7919430516548
Jueff <61515422+Jueff@users.noreply.github.com>
2021-12-15 21:11:52 +0100

    Add some defaults to avoid build failure on old platform versions e.g. 1.0.4

--- src/platforms/esp/32/fastled_esp32.h
+++ src/platforms/esp/32/fastled_esp32.h
@@ -1,5 +1,14 @@
 #pragma once
 
+// macros and platform define are not available in older platform versions
+// so set some defaults to avoid build errors
+#ifndef ESP_IDF_VERSION_VAL
+#define ESP_IDF_VERSION_VAL(major, minor, patch) ((major << 16) | (minor << 8) | (patch))
+#ifndef CONFIG_IDF_TARGET_ESP32
+#define CONFIG_IDF_TARGET_ESP32 1
+#endif
+#endif
+
 #include "fastpin_esp32.h"
 
 #ifdef FASTLED_ALL_PINS_HARDWARE_SPI
