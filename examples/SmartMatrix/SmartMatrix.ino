611fa55f105c3388d13668fed7541b6ef9c3e9b8
Tochinet <tochinet@gmail.com>
2022-01-31 17:38:14 +0100

    Update SmartMatrix.ino

--- examples/SmartMatrix/SmartMatrix.ino
+++ examples/SmartMatrix/SmartMatrix.ino
@@ -1,5 +1,7 @@
 /* This example demos a rectangular LED matrix with moving noise. 
-   It requires the SmartMatrix library in addition to FastLED. This is only available on Teensyy boards at the moment.
+   It requires the SmartMatrix library in addition to FastLED. 
+   This SmartMatrix library is only available on Teensy boards at the moment.
+   It can be found at https://github.com/pixelmatix/SmartMatrix
 */
 #include <SmartMatrix.h>
 #include <FastLED.h>
