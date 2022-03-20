614b2d843b4a69f064396873447e9b7c317d4f68
David Madison <dmadison@users.noreply.github.com>
2022-03-20 01:04:07 -0400

    Update led_sysdefs documentation (NS)
    Overlooked the documentation for these namespace directives the first time around (499f96f)

--- src/led_sysdefs.h
+++ src/led_sysdefs.h
@@ -59,8 +59,11 @@
 #endif
 
 #ifndef FASTLED_NAMESPACE_BEGIN
+/// Start of the FastLED namespace
 #define FASTLED_NAMESPACE_BEGIN
+/// End of the FastLED namespace
 #define FASTLED_NAMESPACE_END
+/// "Using" directive for the namespace
 #define FASTLED_USING_NAMESPACE
 #endif
 
