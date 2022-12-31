1176e862b6f54ce25101106ed191d2b77ded796f
NP v/d Spek <nielsnl68@gmail.com>
2022-12-31 14:40:31 +0100

    ,

--- src/cpp_compat.h
+++ src/cpp_compat.h
@@ -23,13 +23,12 @@
 
 #if __cplusplus < 201703L
 #define REGISTER register
- warning oepsy this should be called
 #else
   #ifdef REGISTER
     #undef REGISTER
   #endif
 #define REGISTER
-  warning Okay, this is good
+
 #endif
 
 #endif
