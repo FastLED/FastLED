9ae74e8360c4c26b579c8dcd448d38f3b7c72c94
Shawn Silverman <shawn@pobox.com>
2023-07-16 00:20:33 -0700

    Add a way to specify external buffers for COctoWS2811Controller

--- src/platforms/arm/mxrt1062/octows2811_controller.h
+++ src/platforms/arm/mxrt1062/octows2811_controller.h
@@ -9,9 +9,10 @@ FASTLED_NAMESPACE_BEGIN
 
 template<EOrder RGB_ORDER = GRB, uint8_t CHIP = WS2811_800kHz>
 class COctoWS2811Controller : public CPixelLEDController<RGB_ORDER, 8, 0xFF> {
+  void *framebuffer;
+  void *drawbuffer;
+  bool wantNullDrawBuffer;
   OctoWS2811 *pocto = nullptr;
-  uint8_t *drawbuffer = nullptr;
-  uint8_t *framebuffer = nullptr;
 
   bool tryToAllocate = true;
 
@@ -20,10 +21,14 @@ class COctoWS2811Controller : public CPixelLEDController<RGB_ORDER, 8, 0xFF> {
       // Allocate the draw buffer second in case the malloc fails,
       // because it can technically be NULL. In other words,
       // prioritize allocating the frame buffer.
-      framebuffer = static_cast<uint8_t *>(malloc(nLeds * 8 * 3));
+      if (framebuffer == nullptr) {
+        framebuffer = malloc(nLeds * 8 * 3);
+      }
 
       if (framebuffer != nullptr) {
-        drawbuffer = static_cast<uint8_t *>(malloc(nLeds * 8 * 3));
+        if (!wantNullDrawBuffer && drawbuffer == nullptr) {
+          drawbuffer = malloc(nLeds * 8 * 3);
+        }
         // Note: A NULL draw buffer is okay
 
         // byte ordering is handled in show by the pixel controller
@@ -45,7 +50,12 @@ class COctoWS2811Controller : public CPixelLEDController<RGB_ORDER, 8, 0xFF> {
     }
   }
 public:
-  COctoWS2811Controller() = default;
+  // If the frame buffer isn't NULL then the draw buffer won't be
+  // internally allocated.
+  COctoWS2811Controller(void *framebuf = nullptr, void *drawbuf = nullptr)
+      : framebuffer(framebuf),
+        drawbuffer(drawbuf),
+        wantNullDrawBuffer(framebuf != nullptr) {}
 
   virtual int size() { return CLEDController::size() * 8; }
 
