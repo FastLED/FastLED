f66586b8dfeb87fb5eac46da4225eb366a313a1f
zackees <z@zackees.com>
2024-08-17 13:23:53 -0700

    use actual hardware pins depending on build type

--- examples/_bugs/1653_S3_Apa102_Hardware_Pins/1653_S3_Apa102_Hardware_Pins.ino
+++ examples/_bugs/1653_S3_Apa102_Hardware_Pins/1653_S3_Apa102_Hardware_Pins.ino
@@ -4,8 +4,54 @@
 #define FASTLED_ALL_PINS_HARDWARE_SPI
 #include <FastLED.h>
 
-#define LED_DI 40  // Data pin for the APA102 LED
-#define LED_CI 39  // Clock pin for the APA102 LED
+#if defined(__AVR_ATmega328P__)
+# define DATA_PIN 11
+# define CLOCK_PIN 13
+#elif defined(__AVR_ATmega2560__)
+# define DATA_PIN 51
+# define CLOCK_PIN 52
+#elif defined(__AVR_ATmega32U4__)
+# define DATA_PIN 17
+# define CLOCK_PIN 21
+#elif defined(__AVR_ATmega1284P__)
+# define DATA_PIN 11
+# define CLOCK_PIN 13
+#elif defined(__AVR_ATmega1280__)
+# define DATA_PIN 51
+# define CLOCK_PIN 52
+// teensy 3.0
+#elif defined(__MK20DX128__)
+# define DATA_PIN 2
+# define CLOCK_PIN 14
+// teensy 3.1
+#elif defined(__MK20DX256__)
+# define DATA_PIN 2
+# define CLOCK_PIN 14
+// teensy 3.2
+#elif defined(__MK64FX512__)
+# define DATA_PIN 2
+# define CLOCK_PIN 14
+// teensy 4.1
+#elif defined(__IMXRT1062__)
+# define DATA_PIN 2
+# define CLOCK_PIN 14
+// esp32
+#elif defined(ESP32)
+# define DATA_PIN 23
+# define CLOCK_PIN 18
+// esp8266
+#elif defined(ESP8266)
+# define DATA_PIN 13
+# define CLOCK_PIN 14
+// esp32-c3
+#elif defined(ESP32C3)
+# define DATA_PIN 23
+# define CLOCK_PIN 18
+#else
+#define DATA_PIN 40  // Data pin for the APA102 LED
+#define CLOCK_PIN 39  // Clock pin for the APA102 LED
+#endif
+
 #define NUM_LEDS 1 // The number of LEDs in your APA102 strip
 
 // Define the array of leds
@@ -14,7 +60,7 @@ CRGB leds[NUM_LEDS];
 void setup()
 {
     // Initialize the LED stripThe sketch is:
-    FastLED.addLeds<APA102, LED_DI, LED_CI, BGR>(leds, NUM_LEDS);
+    FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
     // set master brightness control
     FastLED.setBrightness(64);
 }
