cc4536da3bf2942811944b7f135ab0dba8205aa6
zackees <z@zackees.com>
2024-08-16 18:41:32 -0700

    Fixes #1623
    Merged in fix from d1291e2
    
    This code fixes compilation errors allowing the c6 family to compile. However
    correct behavior has not been fully tested yet.

--- src/platforms/esp/32/clockless_rmt_esp32.cpp
+++ src/platforms/esp/32/clockless_rmt_esp32.cpp
@@ -345,7 +345,7 @@ void IRAM_ATTR ESP32RMTController::tx_start()
     RMT.chnconf0[mRMT_channel].conf_update_chn = 1;
     RMT.chnconf0[mRMT_channel].tx_start_chn = 1;
     #else
-        RMT.chnconf0[mRMT_channel].mem_rd_rst_n = 1;
+    RMT.chnconf0[mRMT_channel].mem_rd_rst_n = 1;
     RMT.chnconf0[mRMT_channel].mem_rd_rst_n = 0;
     RMT.chnconf0[mRMT_channel].apb_mem_rst_n = 1;
     RMT.chnconf0[mRMT_channel].apb_mem_rst_n = 0;
@@ -357,6 +357,19 @@ void IRAM_ATTR ESP32RMTController::tx_start()
     RMT.chnconf0[mRMT_channel].conf_update_n = 1;
     RMT.chnconf0[mRMT_channel].tx_start_n = 1;
     #endif
+#elif CONFIG_IDF_TARGET_ESP32C6
+    // rmt_ll_tx_reset_pointer(&RMT, mRMT_channel)
+    RMT.chnconf0[mRMT_channel].mem_rd_rst_chn = 1;
+    RMT.chnconf0[mRMT_channel].mem_rd_rst_chn = 0;
+    RMT.chnconf0[mRMT_channel].apb_mem_rst_chn = 1;
+    RMT.chnconf0[mRMT_channel].apb_mem_rst_chn = 0;
+    // rmt_ll_clear_tx_end_interrupt(&RMT, mRMT_channel)
+    RMT.int_clr.val = (1 << (mRMT_channel));
+    // rmt_ll_enable_tx_end_interrupt(&RMT, mRMT_channel, true)
+    RMT.int_ena.val |= (1 << mRMT_channel);
+    // rmt_ll_tx_start(&RMT, mRMT_channel)
+    RMT.chnconf0[mRMT_channel].conf_update_chn = 1;
+    RMT.chnconf0[mRMT_channel].tx_start_chn = 1;
 #elif CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32
     // rmt_ll_tx_reset_pointer(&RMT, mRMT_channel)
     RMT.conf_ch[mRMT_channel].conf1.mem_rd_rst = 1;
@@ -368,6 +381,19 @@ void IRAM_ATTR ESP32RMTController::tx_start()
     RMT.int_ena.val |= (1 << (mRMT_channel * 3));
     // rmt_ll_tx_start(&RMT, mRMT_channel)
     RMT.conf_ch[mRMT_channel].conf1.tx_start = 1;
+#elif CONFIG_IDF_TARGET_ESP32C6
+    // rmt_ll_tx_reset_pointer(&RMT, mRMT_channel)
+    RMT.chnconf0[mRMT_channel].mem_rd_rst_chn = 1;
+    RMT.chnconf0[mRMT_channel].mem_rd_rst_chn = 0;
+    RMT.chnconf0[mRMT_channel].apb_mem_rst_chn = 1;
+    RMT.chnconf0[mRMT_channel].apb_mem_rst_chn = 0;
+    // rmt_ll_clear_tx_end_interrupt(&RMT, mRMT_channel)
+    RMT.int_clr.val = (1 << (mRMT_channel));
+    // rmt_ll_enable_tx_end_interrupt(&RMT, mRMT_channel, true)
+    RMT.int_ena.val |= (1 << mRMT_channel);
+    // rmt_ll_tx_start(&RMT, mRMT_channel)
+    RMT.chnconf0[mRMT_channel].conf_update_chn = 1;
+    RMT.chnconf0[mRMT_channel].tx_start_chn = 1;
 #else
     #error Not yet implemented for unknown ESP32 target
 #endif
@@ -426,6 +452,17 @@ void IRAM_ATTR ESP32RMTController::doneOnChannel(rmt_channel_t channel, void * a
     RMT.chnconf0[channel].apb_mem_rst_n = 1;
     RMT.chnconf0[channel].apb_mem_rst_n = 0;
     #endif
+#elif CONFIG_IDF_TARGET_ESP32C6
+    // rmt_ll_enable_tx_end_interrupt(&RMT, channel)
+    RMT.int_ena.val &= ~(1 << channel);
+    // rmt_ll_tx_stop(&RMT, channel)
+    RMT.chnconf0[channel].tx_stop_chn = 1;
+    RMT.chnconf0[channel].conf_update_chn = 1;
+    // rmt_ll_tx_reset_pointer(&RMT, channel)
+    RMT.chnconf0[channel].mem_rd_rst_chn = 1;
+    RMT.chnconf0[channel].mem_rd_rst_chn = 0;
+    RMT.chnconf0[channel].apb_mem_rst_chn = 1;
+    RMT.chnconf0[channel].apb_mem_rst_chn = 0;
 #elif CONFIG_IDF_TARGET_ESP32S2
     // rmt_ll_enable_tx_end_interrupt(&RMT, channel)
     RMT.int_ena.val &= ~(1 << (channel * 3));
@@ -487,6 +524,9 @@ void IRAM_ATTR ESP32RMTController::interruptHandler(void *arg)
         #elif CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32H2
         int tx_done_bit = channel;
         int tx_next_bit = channel + 8;
+        #elif CONFIG_IDF_TARGET_ESP32C6
+        int tx_done_bit = channel; // TODO correct?
+        int tx_next_bit = channel + 8; // TODO correct?
         #elif CONFIG_IDF_TARGET_ESP32
         int tx_done_bit = channel * 3;
         int tx_next_bit = channel + 24;
