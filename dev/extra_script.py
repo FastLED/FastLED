b1fd3ed668c4a7679ae547b4b4407f6a8cd08bce
zackees <z@zackees.com>
2024-08-23 17:24:05 -0700

    update

--- dev/extra_script.py
+++ dev/extra_script.py
@@ -1,6 +1,25 @@
 Import("env")
 
-def before_build(source, target, env):
-    print("Hello")
+# Does nothing right now. I was trying to make
+# FastLED use a symlink to the code folder so
+# that changes to the code would be reflected
+# in the repo.
 
-env.AddPreAction("buildprog", before_build)
+from pathlib import Path
+import warnings
+
+# PIO_DIR = Path(env.PioPlatform())
+
+PROJECT_ROOT = Path(str(env.subst("$PROJECT_DIR"))).resolve()
+LIBDEPS = PROJECT_ROOT / ".pio" / "libdeps"
+
+def find_newest_libdeps() -> Path:
+    target = LIBDEPS / env.subst("$PIOENV")
+    if not target.exists():
+        raise FileNotFoundError(f"Target libdeps not found: {target}")
+    return target
+
+try:
+    print(str(find_newest_libdeps()))
+except Exception as e:
+    warnings.warn(str(e))
