9660a21369ad616caa4f8baecba5c44489f688cb
Zachary Vorhies <zachvorhies@protonmail.com>
2024-08-27 23:27:08 -0700

    move rmt specific encoding to the rmt class

--- src/platforms/esp/32/idf4_clockless_rmt_esp32.h
+++ src/platforms/esp/32/idf4_clockless_rmt_esp32.h
@@ -61,64 +61,16 @@ public:
     virtual uint16_t getMaxRefreshRate() const { return 400; }
 
 protected:
-
-    // -- Load pixel data
-    //    This method loads all of the pixel data into a separate buffer for use by
-    //    by the RMT driver. Copying does two important jobs: it fixes the color
-    //    order for the pixels, and it performs the scaling/adjusting ahead of time.
-    //    It also packs the bytes into 32 bit chunks with the right bit order.
-    void loadPixelData(PixelController<RGB_ORDER> & pixels)
-    {
-        // -- Make sure the buffer is allocated
-        int size_in_bytes = pixels.size() * 3;
-        uint8_t * pData = mRMTController.getPixelBuffer(size_in_bytes);
-
-        // -- This might be faster
-        while (pixels.has(1)) {
-            *pData++ = pixels.loadAndScale0();
-            *pData++ = pixels.loadAndScale1();
-            *pData++ = pixels.loadAndScale2();
-            pixels.advanceData();
-            pixels.stepDithering();
-        }
-    }
-
     // -- Show pixels
     //    This is the main entry point for the controller.
     virtual void showPixels(PixelController<RGB_ORDER> & pixels)
     {
         if (FASTLED_RMT_BUILTIN_DRIVER) {
-            convertAllPixelData(pixels);
+            mRMTController.loadAllPixelsToRmtSymbolData(pixels);
         } else {
-            loadPixelData(pixels);
+            mRMTController.loadPixelDataForStreamEncoding(pixels);
         }
-
         mRMTController.showPixels();
-    }
-
-    // -- Convert all pixels to RMT pulses
-    //    This function is only used when the user chooses to use the
-    //    built-in RMT driver, which needs all of the RMT pulses
-    //    up-front.
-    void convertAllPixelData(PixelController<RGB_ORDER> & pixels)
-    {
-        // -- Make sure the data buffer is allocated
-        mRMTController.initPulseBuffer(pixels.size() * 3);
-
-        // -- Cycle through the R,G, and B values in the right order,
-        //    storing the pulses in the big buffer
-
-        uint32_t byteval;
-        while (pixels.has(1)) {
-            byteval = pixels.loadAndScale0();
-            mRMTController.ingest(byteval);
-            byteval = pixels.loadAndScale1();
-            mRMTController.ingest(byteval);
-            byteval = pixels.loadAndScale2();
-            mRMTController.ingest(byteval);
-            pixels.advanceData();
-            pixels.stepDithering();
-        }
     }
 };
 
