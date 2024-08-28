100e44360f2ebe520e0b992d2e5677c73f5258ec
zackees <z@zackees.com>
2024-08-28 12:51:57 -0700

    Add build configuration for rpipicow platform

--- ci/ci-compile.py
+++ ci/ci-compile.py
@@ -87,6 +87,7 @@ CUSTOM_PROJECT_OPTIONS = {
     "esp32-s3-devkitc-1": f"platform={ESP32_IDF_5_1}",
     "esp32-h2-devkitm-1": f"platform={ESP32_IDF_5_1_LATEST}",
     "adafruit_feather_nrf52840_sense": "platform=nordicnrf52",
+    "rpipicow": "platform=https://github.com/maxgerhardt/platform-raspberrypi.git",
 }
 
 ERROR_HAPPENED = False
