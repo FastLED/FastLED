d94e6e389ebd4be762a4403c19cf1b8a48ab1457
zackees <z@zackees.com>
2024-08-28 17:50:46 -0700

    nit

--- ci/ci-compile.py
+++ ci/ci-compile.py
@@ -88,8 +88,8 @@ CUSTOM_PROJECT_OPTIONS = {
     "esp32-h2-devkitm-1": [f"platform={ESP32_IDF_5_1_LATEST}"],
     "adafruit_feather_nrf52840_sense": ["platform=nordicnrf52"],
     "rpico2": [
-        "platform=https://github.com/earlephilhower/arduino-pico.git",
-        #"platform=https://github.com/maxgerhardt/platform-raspberrypi.git",
+        #"platform=https://github.com/earlephilhower/arduino-pico.git",
+        "platform=https://github.com/maxgerhardt/platform-raspberrypi.git",
         "framework=framework-arduinopico@https://github.com/earlephilhower/arduino-pico.git",
         "board_build.core=earlephilhower",
         "board_build.filesystem_size=0.5m"
