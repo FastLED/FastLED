3fa7dcfd7143f1b22d26862b351dc469eb18f6ab
zackees <z@zackees.com>
2024-08-30 16:35:44 -0700

    Add support for Cortex M0 platform in get_sys_ticks

--- src/platforms/arm/common/_get_sys_ticks.h
+++ src/platforms/arm/common/_get_sys_ticks.h
@@ -1,13 +1,43 @@
 
 #ifndef __GET_SYS_TICKS_H__
 #define __GET_SYS_TICKS_H__
-// #include <core_cm0plus.h>  // This seems to be missing?
+
+// only include this for the cortex m0 platform
+#ifdef ___ARM_ARCH_6M__
+
+// Stripped down version of <core_mo0.h> for the SysTick structure. This is needed
+// by some code for the cortex m0
 
 #include <stdint.h>
+
+// Define the SysTick base address
+#define SCS_BASE            (0xE000E000UL)                            /*!< System Control Space Base Address */
+#define SysTick_BASE        (SCS_BASE +  0x0010UL)                    /*!< SysTick Base Address */
+#define SysTick             ((SysTick_Type   *)     SysTick_BASE  )   /*!< SysTick configuration struct */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Define the SysTick structure
+typedef struct {
+    volatile uint32_t CTRL;
+    volatile uint32_t LOAD;
+    volatile uint32_t VAL;
+    volatile const uint32_t CALIB;
+} SysTick_Type;
+
+// Define the SysTick
+#define SysTick ((SysTick_Type *)SysTick_BASE)
 
 inline uint32_t _get_sys_ticks() {
     return SysTick->VAL;
 }
 
+#ifdef __cplusplus
+}
+#endif
 
-#endif  // __ARM_ARCH_6M__
+#endif  // ___ARM_ARCH_6M__
+
+#endif  // __GET_SYS_TICKS_H__
