f0d699305d1dff9026a9f3883c91dee555c5e2c3
zackees <z@zackees.com>
2024-09-01 16:18:09 -0700

    C++ fixes for esp32 rmt led_strip component

--- src/platforms/esp/32/led_strip/led_strip_rmt_encoder.cpp
+++ src/platforms/esp/32/led_strip/led_strip_rmt_encoder.cpp
@@ -7,6 +7,10 @@
 
 #if FASTLED_ESP32_COMPONENT_LED_STRIP_BUILT_IN
 
+template<typename EnumT>
+EnumT enum_or(EnumT a, EnumT b) {
+    return static_cast<EnumT>(static_cast<int>(a) | static_cast<int>(b));
+}
 
 
 #ifdef __cplusplus
@@ -29,13 +33,15 @@ typedef struct {
     rmt_symbol_word_t reset_code;
 } rmt_led_strip_encoder_t;
 
+
+
 static size_t rmt_encode_led_strip(rmt_encoder_t *encoder, rmt_channel_handle_t channel, const void *primary_data, size_t data_size, rmt_encode_state_t *ret_state)
 {
     rmt_led_strip_encoder_t *led_encoder = __containerof(encoder, rmt_led_strip_encoder_t, base);
     rmt_encoder_handle_t bytes_encoder = led_encoder->bytes_encoder;
     rmt_encoder_handle_t copy_encoder = led_encoder->copy_encoder;
-    rmt_encode_state_t session_state = 0;
-    rmt_encode_state_t state = 0;
+    rmt_encode_state_t session_state = RMT_ENCODING_RESET;
+    rmt_encode_state_t state = RMT_ENCODING_RESET;
     size_t encoded_symbols = 0;
     switch (led_encoder->state) {
     case 0: // send RGB data
@@ -44,7 +50,7 @@ static size_t rmt_encode_led_strip(rmt_encoder_t *encoder, rmt_channel_handle_t 
             led_encoder->state = 1; // switch to next state when current encoding session finished
         }
         if (session_state & RMT_ENCODING_MEM_FULL) {
-            state |= RMT_ENCODING_MEM_FULL;
+            state = enum_or(state, RMT_ENCODING_MEM_FULL);
             goto out; // yield if there's no free space for encoding artifacts
         }
     // fall-through
@@ -53,10 +59,12 @@ static size_t rmt_encode_led_strip(rmt_encoder_t *encoder, rmt_channel_handle_t 
                                                 sizeof(led_encoder->reset_code), &session_state);
         if (session_state & RMT_ENCODING_COMPLETE) {
             led_encoder->state = 0; // back to the initial encoding session
-            state |= RMT_ENCODING_COMPLETE;
+            //state |= RMT_ENCODING_COMPLETE;
+            state = enum_or(state, RMT_ENCODING_COMPLETE);
         }
         if (session_state & RMT_ENCODING_MEM_FULL) {
-            state |= RMT_ENCODING_MEM_FULL;
+            //state |= RMT_ENCODING_MEM_FULL;
+            state = enum_or(state, RMT_ENCODING_MEM_FULL);
             goto out; // yield if there's no free space for encoding artifacts
         }
     }
@@ -89,7 +97,7 @@ esp_err_t rmt_new_led_strip_encoder(const led_strip_encoder_config_t *config, rm
     rmt_led_strip_encoder_t *led_encoder = NULL;
     ESP_GOTO_ON_FALSE(config && ret_encoder, ESP_ERR_INVALID_ARG, err, TAG, "invalid argument");
     ESP_GOTO_ON_FALSE(config->led_model < LED_MODEL_INVALID, ESP_ERR_INVALID_ARG, err, TAG, "invalid led model");
-    led_encoder = calloc(1, sizeof(rmt_led_strip_encoder_t));
+    led_encoder = static_cast<rmt_led_strip_encoder_t*>(calloc(1, sizeof(rmt_led_strip_encoder_t)));
     ESP_GOTO_ON_FALSE(led_encoder, ESP_ERR_NO_MEM, err, TAG, "no mem for led strip encoder");
     led_encoder->base.encode = rmt_encode_led_strip;
     led_encoder->base.del = rmt_del_led_strip_encoder;
