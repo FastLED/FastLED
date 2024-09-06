bc3eced7a9d29b81dab89d0a060b368b3afe4f69
zackees <z@zackees.com>
2024-09-06 13:56:21 -0700

    esp32: silence deprecation warning by binding to new cpu_hal_get_cycle_count

--- src/platforms/esp/32/clock_cycles.h
+++ src/platforms/esp/32/clock_cycles.h
@@ -4,12 +4,20 @@
 #include <stdint.h>
 #include "hal/cpu_hal.h"
 
+#include "esp_idf_version.h"
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+#define __cpu_hal_get_cycle_count esp_cpu_get_cycle_count
+#else
+#define __cpu_hal_get_cycle_count cpu_hal_get_cycle_count
+#endif  // ESP_IDF_VERSION
+
+
 __attribute__ ((always_inline)) inline static uint32_t __clock_cycles() {
   uint32_t cyc;
 #ifdef FASTLED_XTENSA
   __asm__ __volatile__ ("rsr %0,ccount":"=a" (cyc));
 #else
-  cyc = cpu_hal_get_cycle_count();
+  cyc = __cpu_hal_get_cycle_count();
 #endif
   return cyc;
 }
