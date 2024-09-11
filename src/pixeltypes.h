2e6049f2eb2a5fe4e154f368890826716c7e654b
zackees <z@zackees.com>
2024-09-10 19:25:26 -0700

    Add CHSV struct for HSV pixel representation

--- src/pixeltypes.h
+++ src/pixeltypes.h
@@ -7,6 +7,7 @@
 #include "lib8tion.h"
 #include "color.h"
 #include "eorder.h"
+#include "chsv.h"
 
 /// @file pixeltypes.h
 /// Definitions for pixel color data structs
@@ -14,7 +15,6 @@
 FASTLED_NAMESPACE_BEGIN
 
 struct CRGB;
-struct CHSV;
 
 /// @defgroup PixelTypes Pixel Data Types (CRGB/CHSV)
 /// @brief Structs that hold pixel color data
@@ -24,98 +24,7 @@ struct CHSV;
 /// to avoid circular dependencies.
 extern void hsv2rgb_rainbow( const CHSV& hsv, CRGB& rgb);
 
-/// Representation of an HSV pixel (hue, saturation, value (aka brightness)).
-struct CHSV {
-    union {
-        struct {
-            union {
-                /// Color hue.
-                /// This is an 8-bit value representing an angle around
-                /// the color wheel. Where 0 is 0°, and 255 is 358°.
-                uint8_t hue;
-                uint8_t h;  ///< @copydoc hue
-            };
-            union {
-                /// Color saturation.
-                /// This is an 8-bit value representing a percentage.
-                uint8_t saturation;
-                uint8_t sat;  ///< @copydoc saturation
-                uint8_t s;    ///< @copydoc saturation
-            };
-            union {
-                /// Color value (brightness).
-                /// This is an 8-bit value representing a percentage.
-                uint8_t value;
-                uint8_t val;  ///< @copydoc value
-                uint8_t v;    ///< @copydoc value
-            };
-        };
-        /// Access the hue, saturation, and value data as an array.
-        /// Where:
-        /// * `raw[0]` is the hue
-        /// * `raw[1]` is the saturation
-        /// * `raw[2]` is the value
-        uint8_t raw[3];
-    };
 
-    /// Array access operator to index into the CHSV object
-    /// @param x the index to retrieve (0-2)
-    /// @returns the CHSV::raw value for the given index
-    inline uint8_t& operator[] (uint8_t x) __attribute__((always_inline))
-    {
-        return raw[x];
-    }
-
-    /// @copydoc operator[]
-    inline const uint8_t& operator[] (uint8_t x) const __attribute__((always_inline))
-    {
-        return raw[x];
-    }
-
-    /// Default constructor
-    /// @warning Default values are UNITIALIZED!
-    inline CHSV() __attribute__((always_inline)) = default;
-
-    /// Allow construction from hue, saturation, and value
-    /// @param ih input hue
-    /// @param is input saturation
-    /// @param iv input value
-    inline CHSV( uint8_t ih, uint8_t is, uint8_t iv) __attribute__((always_inline))
-        : h(ih), s(is), v(iv)
-    {
-    }
-
-    /// Allow copy construction
-    inline CHSV(const CHSV& rhs) __attribute__((always_inline)) = default;
-
-    /// Allow copy construction
-    inline CHSV& operator= (const CHSV& rhs) __attribute__((always_inline)) = default;
-
-    /// Assign new HSV values
-    /// @param ih input hue
-    /// @param is input saturation
-    /// @param iv input value
-    /// @returns reference to the CHSV object
-    inline CHSV& setHSV(uint8_t ih, uint8_t is, uint8_t iv) __attribute__((always_inline))
-    {
-        h = ih;
-        s = is;
-        v = iv;
-        return *this;
-    }
-};
-
-/// Pre-defined hue values for CHSV objects
-typedef enum {
-    HUE_RED = 0,       ///< Red (0°)
-    HUE_ORANGE = 32,   ///< Orange (45°)
-    HUE_YELLOW = 64,   ///< Yellow (90°)
-    HUE_GREEN = 96,    ///< Green (135°)
-    HUE_AQUA = 128,    ///< Aqua (180°)
-    HUE_BLUE = 160,    ///< Blue (225°)
-    HUE_PURPLE = 192,  ///< Purple (270°)
-    HUE_PINK = 224     ///< Pink (315°)
-} HSVHue;
 
 /// Representation of an RGB pixel (Red, Green, Blue)
 struct CRGB {
@@ -946,7 +855,7 @@ inline CRGB operator%( const CRGB& p1, uint8_t d)
 }
 
 
-
+FASTLED_NAMESPACE_END
 
 ///@}
 
