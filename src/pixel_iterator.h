69654f5e625c29ad7363088358f376c33e6354d1
zackees <z@zackees.com>
2024-09-11 13:43:55 -0700

    Remove default implementation of loadAndScale_APA102_HD

--- src/pixel_iterator.h
+++ src/pixel_iterator.h
@@ -53,7 +53,7 @@ class PixelIterator {
     virtual void loadAndScale_APA102_HD(
         uint8_t *b0_out, uint8_t *b1_out,
         uint8_t *b2_out, // Output RGB values in order of RGB_ORDER
-        uint8_t *brightness_out);
+        uint8_t *brightness_out) = 0;
 
     // stepDithering
     virtual void stepDithering() = 0;
