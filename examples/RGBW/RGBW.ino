9e4fbe8bb89e74dd0e9c08ae8ea019ee735eb7c3
zackees <z@zackees.com>
2024-09-11 21:17:24 -0700

    try to fix esp32 for RGBW example

--- examples/RGBW/RGBW.ino
+++ examples/RGBW/RGBW.ino
@@ -13,7 +13,7 @@
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
 // ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
 // Clock pin only needed for SPI based chipsets when not using hardware SPI
-#define DATA_PIN 9
+#define DATA_PIN 3
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
