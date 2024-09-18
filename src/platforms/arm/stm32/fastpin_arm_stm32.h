e99ab9c621aeafb78d3600704f6f671a1f3978d6
zackees <z@zackees.com>
2024-09-18 16:33:21 -0700

    clarify legacy pin definitions vs old pin definitions

--- src/platforms/arm/stm32/fastpin_arm_stm32.h
+++ src/platforms/arm/stm32/fastpin_arm_stm32.h
@@ -1,11 +1,12 @@
 #ifndef __FASTPIN_ARM_STM32_H
 #define __FASTPIN_ARM_STM32_H
 
-#if defined(ARDUINO_BLUEPILL_F103C8)
-#include "fastpin_arm_stm_bluepill.h"
+#if defined(ARDUINO_BLUEPILL_F103C8) && 0
+// from https://github.com/13rac1/FastLED-STM32
+#include "fastpin_arm_stm_new.h"
 #else
-// Everything else.
-#include "fastpin_arm_stm_generic.h"
+// Legacy fastled pin definitions
+#include "fastpin_arm_stm_legacy.h"
 #endif
 
 #endif // __INC_FASTPIN_ARM_STM32
