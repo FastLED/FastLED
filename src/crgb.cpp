ab6b1905083e67320c1ae0bf9f840367e2a828fb
zackees <z@zackees.com>
2024-09-24 15:48:09 -0700

    Optimize CRGB::computeAdjustment for AVR size

--- src/crgb.cpp
+++ src/crgb.cpp
@@ -11,6 +11,8 @@ CRGB CRGB::computeAdjustment(uint8_t scale, const CRGB & colorCorrection, const 
                     uint8_t cc = colorCorrection.raw[i];
                     uint8_t ct = colorTemperature.raw[i];
                     if(cc > 0 && ct > 0) {
+                        // Optimized for AVR size. This function is only called very infrequently so size
+                        // matters more than speed.
                         uint32_t work = (((uint16_t)cc)+1);
                         work *= (((uint16_t)ct)+1);
                         work *= scale;
