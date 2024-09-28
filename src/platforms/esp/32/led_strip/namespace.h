5d5bd584fb35a6dae6422a92f2a1f0e0ff309a84
zackees <z@zackees.com>
2024-09-28 15:40:08 -0700

    Update LED strip namespace to fastled_rmt51_strip

--- src/platforms/esp/32/led_strip/namespace.h
+++ src/platforms/esp/32/led_strip/namespace.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define LED_STRIP_NAMESPACE_BEGIN namespace led_strip {
+#define LED_STRIP_NAMESPACE_BEGIN namespace fastled_rmt51_strip {
 
 #define LED_STRIP_NAMESPACE_END }
 
-#define USING_NAMESPACE_LED_STRIP using namespace led_strip;
+#define USING_NAMESPACE_LED_STRIP using namespace fastled_rmt51_strip;
