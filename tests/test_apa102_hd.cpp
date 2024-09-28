8fadfb703656d5ac40e517da0487f33ecf4a7bbe
Zachary Vorhies <zachvorhies@protonmail.com>
2024-09-27 19:12:08 -0700

    Apa102 global (#1736)
    * Add tasks.json and optimize five_bit_hd_gamma.cpp
    
    * update
    
    * Add randomized testing for compute_power function

--- tests/test_apa102_hd.cpp
+++ tests/test_apa102_hd.cpp
@@ -6,87 +6,98 @@
 #include "doctest.h"
 #include "FastLED.h"
 #include "five_bit_hd_gamma.h"
+#include "assert.h"
+#include "math.h"
+#include <ctime>
+#include <cstdlib>
+
+
+#define CHECK_NEAR(a, b, c) CHECK_LT(abs(a - b), c)
+
+
+// Testing allows upto 25% error between power output of WS2812 and APA102 in HD mode.
+// This probably happens on the high end of the brightness scale.
+const static float TOLERANCE = 0.25;
+
+
+
+struct Power {
+  float power;
+  float power_5bit;
+};
+
+
+uint16_t mymap(uint32_t x, uint32_t in_min, uint32_t in_max, uint32_t out_min, uint32_t out_max) {
+  return static_cast<uint16_t>(
+     (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+  );
+}
 
 TEST_CASE("FASTLED_APA102_USES_HD_GLOBAL_BRIGHTNESS is 1") {
   CHECK_EQ(FASTLED_HD_COLOR_MIXING, 1);
   CHECK_EQ(FASTLED_APA102_USES_HD_GLOBAL_BRIGHTNESS, 1);
 }
 
+float power_rgb(CRGB color) {
+  float out = uint16_t(color.r) + uint16_t(color.g) + uint16_t(color.b);
+  return out / (255 * 3);
+}
+
+float computer_power_5bit(CRGB color, uint8_t five_bit_brightness) {
+  assert(five_bit_brightness <= 31);
+  float rgb_pow = power_rgb(color);
+  float brightness_pow = five_bit_brightness / 31.0f;
+  return rgb_pow * brightness_pow;
+}
+
+
+
+Power compute_power(uint8_t brightness8, CRGB color) {
+  uint16_t r16 = mymap(color.r, 0, 255, 0, 0xffff);
+  uint16_t g16 = mymap(color.g, 0, 255, 0, 0xffff);
+  uint16_t b16 = mymap(color.b, 0, 255, 0, 0xffff);
+  uint8_t power_5bit;
+  CRGB out_colors;
+  five_bit_bitshift(r16, g16, b16, brightness8, &out_colors, &power_5bit);
+  float power = computer_power_5bit(out_colors, power_5bit);
+  CRGB color_rgb8 = color;
+  color_rgb8.r = scale8(color_rgb8.r, brightness8);
+  color_rgb8.g = scale8(color_rgb8.g, brightness8);
+  color_rgb8.b = scale8(color_rgb8.b, brightness8);
+  float power_rgb8 = power_rgb(color_rgb8);
+  return {power, power_rgb8};
+}
+
+
 TEST_CASE("five_bit_hd_gamma_bitshift functionality") {
   enum {
     kBrightness = 255,
   };
-  uint8_t r8, g8, b8, power_5bit;
-  
-  SUBCASE("Full brightness, no scaling") {
-    CRGB colors(255, 255, 255);
-    CRGB colors_scale(255, 255, 255);
-    CRGB out_colors;
-    five_bit_hd_gamma_bitshift(colors, colors_scale, kBrightness, &out_colors, &power_5bit);
-    CHECK_EQ(out_colors.r, 255);
-    CHECK_EQ(out_colors.g, 255);
-    CHECK_EQ(out_colors.b, 255);
-    CHECK_EQ(power_5bit, 31);
-  }
 
-  SUBCASE("Full brightness, half scaling") {
-    CRGB colors(255, 255, 255);
-    CRGB colors_scale(128, 128, 128);
-    CRGB out_colors;
-    five_bit_hd_gamma_bitshift(colors, colors_scale, kBrightness, &out_colors, &power_5bit);
-    CHECK_LT(out_colors.r, 255);
-    CHECK_EQ(out_colors.g, out_colors.r);
-    CHECK_EQ(out_colors.b, out_colors.r);
-    CHECK_EQ(power_5bit, 31);
-  }
+  SUBCASE("Randomized Test") {
+    srand(time(NULL));  // Seed the random number generator
+    const int NUM_TESTS = 1000000;
 
-  SUBCASE("Different colors, no scaling") {
-    CRGB colors(255, 128, 64);
-    CRGB colors_scale(255, 255, 255);
-    CRGB out_colors;
-    five_bit_hd_gamma_bitshift(colors, colors_scale, kBrightness, &out_colors, &power_5bit);
-    CHECK_EQ(out_colors.r, 255);
-    CHECK_GT(out_colors.g, 0);
-    CHECK_LT(out_colors.g, 255);
-    CHECK_GT(out_colors.b, 0);
-    CHECK_LT(out_colors.b, out_colors.g);
-    CHECK_EQ(power_5bit, 31);
-  }
+    bool fail = false;
+    
+    for (int i = 0; i < NUM_TESTS; i++) {
+      uint8_t brightness = rand() % 256;  // Random brightness (0-255)
+      uint8_t r = rand() % 256;  // Random red value (0-255)
+      uint8_t g = rand() % 256;  // Random green value (0-255)
+      uint8_t b = rand() % 256;  // Random blue value (0-255)
+      
+      CRGB color(r, g, b);
+      Power result = compute_power(brightness, color);
+      float diff = abs(result.power - result.power_5bit);
 
-  SUBCASE("Some different values") {
-    CRGB colors(65, 64, 64);
-    CRGB colors_scale(255, 255, 255);
-    CRGB out_colors;
-    five_bit_hd_gamma_bitshift(colors, colors_scale, kBrightness, &out_colors, &power_5bit);
-    CHECK_EQ(out_colors.r, 170);
-    CHECK_EQ(out_colors.g, 165);
-    CHECK_EQ(out_colors.b, 165);
-    CHECK_EQ(power_5bit, 3);
+      if (diff > .04) {
+        std::cout << "diff=" << diff << " for brightness=" << (int)brightness << ", color=(" << (int)r << "," << (int)g << "," << (int)b << ")" << std::endl;
+        fail = true;
+      }
+    }
+    if (fail) {
+      FAIL("Some tests failed");
+    }
   }
-
-  SUBCASE("Global brightness half") {
-    const uint8_t brightness = 128;
-    CRGB colors(65, 64, 64);
-    CRGB colors_scale(255, 255, 255);
-    CRGB out_colors;
-    five_bit_hd_gamma_bitshift(colors, colors_scale, brightness, &out_colors, &power_5bit);
-    CHECK_EQ(out_colors.r, 85);
-    CHECK_EQ(out_colors.g, 83);
-    CHECK_EQ(out_colors.b, 83);
-    CHECK_EQ(power_5bit, 3);
-  }
-
-  SUBCASE("Global brightness low end") {
-    const uint8_t brightness = 8;
-    CRGB colors(65, 64, 64);
-    CRGB colors_scale(255, 255, 255);
-    CRGB out_colors;
-    five_bit_hd_gamma_bitshift(colors, colors_scale, brightness, &out_colors, &power_5bit);
-    CHECK_EQ(out_colors.r, 9);
-    CHECK_EQ(out_colors.g, 9);
-    CHECK_EQ(out_colors.b, 9);
-    CHECK_EQ(power_5bit, 1);
-  }
-
 }
 
