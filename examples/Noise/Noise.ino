3542bad508388fe66e7c4c52fefc8e2d39e28b01
zackees <z@zackees.com>
2024-09-28 11:57:33 -0700

    Update matrix width and height for Tiny 4x4 device

--- examples/Noise/Noise.ino
+++ examples/Noise/Noise.ino
@@ -9,8 +9,14 @@
 //
 
 // Params for width and height
+#if defined(__AVR_ATtinyxy4__)
+// Tiny 4x4 matrix for this memory constrained device.
+const uint8_t kMatrixWidth = 8;
+const uint8_t kMatrixHeight = 8;
+#else
 const uint8_t kMatrixWidth = 16;
 const uint8_t kMatrixHeight = 16;
+#endif
 
 #define MAX_DIMENSION ((kMatrixWidth>kMatrixHeight) ? kMatrixWidth : kMatrixHeight)
 #define NUM_LEDS (kMatrixWidth * kMatrixHeight)
