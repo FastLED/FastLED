741c39cc0a2897f84206e08064484afe0bb1263b
zackees <z@zackees.com>
2024-09-29 09:53:12 -0700

    switch on DMA usage by default

--- src/platforms/esp/32/led_strip/configure_led.h
+++ src/platforms/esp/32/led_strip/configure_led.h
@@ -6,21 +6,26 @@
 
 LED_STRIP_NAMESPACE_BEGIN
 
-// #ifndef FASTLED_RMT_MEMBLOCK_SYMBOLS
-// #define FASTLED_RMT_MEMBLOCK_SYMBOLS 64
-// #endif
-
 #ifndef FASTLED_RMT_WITH_DMA
+#define FASTLED_RMT_WITH_DMA 1
+#else
 #define FASTLED_RMT_WITH_DMA 0
 #endif
 
+#ifndef FASTLED_RMT_MEMBLOCK_SYMBOLS
+#if FASTLED_RMT_WITH_DMA
+#define FASTLED_RMT_MEMBLOCK_SYMBOLS 1024
+#else
+#define FASTLED_RMT_MEMBLOCK_SYMBOLS 0  // Let the library decide
+#endif  // FASTLED_RMT_WITH_DMA
+#endif  // FASTLED_RMT_MEMBLOCK_SYMBOLS
 
 struct config_led_t {
     int pin;
     uint32_t max_leds;
     led_model_t chipset;
     led_pixel_format_t rgbw;
-    size_t mem_block_symbols = 0;  // Let the library decide
+    size_t mem_block_symbols = FASTLED_RMT_MEMBLOCK_SYMBOLS;
     bool with_dma = FASTLED_RMT_WITH_DMA;
     uint8_t* pixel_buf = nullptr;
 };
