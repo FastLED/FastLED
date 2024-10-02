fe7ed9a3643d1067f60d6c8ddda7c783d38a7617
zackees <z@zackees.com>
2024-10-01 20:36:52 -0700

    Add Cylon animation effect using FastLED library

---
+++ src/fx/cylon.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+/// @brief   An animation that moves a single LED back and forth (Larson Scanner effect)
+
+
+#include "FastLED.h"
+
+struct CylonData {
+    CRGB* leds = nullptr;
+    uint16_t num_leds = 0;
+    uint8_t hue = 0;
+    uint8_t fade_amount = 250;
+    uint8_t delay_ms = 10;
+    bool reverse = false;
+    int16_t position = 0;
+
+    // constructor
+    CylonData(CRGB* leds, uint16_t num_leds, uint8_t fade_amount = 250, uint8_t delay_ms = 10)
+        : leds(leds), num_leds(num_leds), fade_amount(fade_amount), delay_ms(delay_ms) {}
+};
+
+
+void CylonLoop(CylonData& self) {
+    if (self.leds == nullptr || self.num_leds == 0) {
+        return;
+    }
+
+    // Set the current LED to the current hue
+    self.leds[self.position] = CHSV(self.hue++, 255, 255);
+
+
+
+    // Fade all LEDs
+    for(int i = 0; i < self.num_leds; i++) {
+        self.leds[i].nscale8(self.fade_amount);
+    }
+
+    // Move the position
+    if (self.reverse) {
+        self.position--;
+        if (self.position < 0) {
+            self.position = 1;
+            self.reverse = false;
+        }
+    } else {
+        self.position++;
+        if (self.position >= self.num_leds) {
+            self.position = self.num_leds - 2;
+            self.reverse = true;
+        }
+    }
+}
