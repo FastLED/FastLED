7f4defbb05228c074dce741b7916f2b87a023a38
zackees <z@zackees.com>
2024-10-01 23:26:09 -0700

    Add math utility functions and noise generator struct

--- src/fx/noisewave.hpp
+++ src/fx/noisewave.hpp
@@ -1,25 +1,12 @@
 #pragma once
 
 #include "FastLED.h"
+#include "noisegen.h"
 
-struct NoiseGenerator {
-    int32_t iteration_scale;
-    unsigned long time_multiplier;
 
-    NoiseGenerator() : iteration_scale(10), time_multiplier(10) {}
-    NoiseGenerator(int32_t itScale, int32_t timeMul) : iteration_scale(itScale), time_multiplier(timeMul) {}
+#include "namespace.h"
 
-    uint8_t Value(int32_t i, unsigned long time_ms) const {
-        uint32_t input = iteration_scale * i + time_ms * time_multiplier;
-        uint16_t v1 = inoise16(input);
-        return uint8_t(v1 >> 8);
-    }
-
-    int LedValue(int32_t i, unsigned long time_ms) const {
-        int val = Value(i, time_ms);
-        return max(0, val - 128) * 2;
-    }
-};
+FASTLED_NAMESPACE_BEGIN
 
 struct NoiseWaveData {
     CRGB* leds = nullptr;
@@ -50,3 +37,4 @@ void NoiseWaveLoop(NoiseWaveData& self) {
     }
 }
 
+FASTLED_NAMESPACE_END
