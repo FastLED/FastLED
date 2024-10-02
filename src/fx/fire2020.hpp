afdd9ebcd7c03e243b8fe0726a1ed11f0362f4b9
zackees <z@zackees.com>
2024-10-01 20:11:49 -0700

    update

--- src/fx/fire2020.hpp
+++ src/fx/fire2020.hpp
@@ -1,10 +1,47 @@
 /// @brief   Simple one-dimensional fire animation function
+// Fire2012 by Mark Kriegsman, July 2012
+// as part of "Five Elements" shown here: http://youtu.be/knWiGsmgycY
+//// 
+// This basic one-dimensional 'fire' simulation works roughly as follows:
+// There's a underlying array of 'heat' cells, that model the temperature
+// at each point along the line.  Every cycle through the simulation, 
+// four steps are performed:
+//  1) All cells cool down a little bit, losing heat to the air
+//  2) The heat from each cell drifts 'up' and diffuses a little
+//  3) Sometimes randomly new 'sparks' of heat are added at the bottom
+//  4) The heat from each cell is rendered as a color into the leds array
+//     The heat-to-color mapping uses a black-body radiation approximation.
+//
+// Temperature is in arbitrary units from 0 (cold black) to 255 (white hot).
+//
+// This simulation scales it self a bit depending on NUM_LEDS; it should look
+// "OK" on anywhere from 20 to 100 LEDs without too much tweaking. 
+//
+// I recommend running this simulation at anywhere from 30-100 frames per second,
+// meaning an interframe delay of about 10-35 milliseconds.
+//
+// Looks best on a high-density LED setup (60+ pixels/meter).
+//
+//
+// There are two main parameters you can play with to control the look and
+// feel of your fire: COOLING (used in step 1 above), and SPARKING (used
+// in step 3 above).
+//
+// COOLING: How much does the air cool as it rises?
+// Less cooling = taller flames.  More cooling = shorter flames.
+// Default 50, suggested range 20-100 
+
+// SPARKING: What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire.  Lower chance = more flickery fire.
+// Default 120, suggested range 50-200.
+
 
 #ifndef FIRE2020_HPP
 #define FIRE2020_HPP
 
 #include "crgb.h"
 #include "FastLED.h"
+
 
 struct Fire2020Data {
     CRGB* leds = nullptr;
@@ -27,12 +64,12 @@ void Fire2020Loop(Fire2020Data& self)
     }
 
     // Step 1.  Cool down every cell a little
-    for (int i = 0; i < self.num_leds; i++) {
+    for (uint16_t i = 0; i < self.num_leds; i++) {
         self.heat[i] = qsub8(self.heat[i], random8(0, ((self.cooling * 10) / self.num_leds) + 2));
     }
   
     // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-    for (int k = self.num_leds - 1; k >= 2; k--) {
+    for (uint16_t k = self.num_leds - 1; k >= 2; k--) {
         self.heat[k] = (self.heat[k - 1] + self.heat[k - 2] + self.heat[k - 2]) / 3;
     }
     
