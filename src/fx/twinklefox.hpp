afca766455ca7fff62e8b43059e5e27d7718b5db
zackees <z@zackees.com>
2024-10-01 21:43:41 -0700

    Add parameters for twinkle speed, density, and color palettes

--- src/fx/twinklefox.hpp
+++ src/fx/twinklefox.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+/// @file    TwinkleFox.hpp
+/// @brief   Twinkling "holiday" lights that fade in and out.
+/// @example TwinkleFox.ino
+
 //  TwinkleFOX: Twinkling 'holiday' lights that fade in and out.
 //  Colors are chosen from a palette; a few palettes are provided.
 //
@@ -65,10 +69,28 @@
 #include "FastLED.h"
 
 // TwinkleFox effect parameters
+// Overall twinkle speed.
+// 0 (VERY slow) to 8 (VERY fast).  
+// 4, 5, and 6 are recommended, default is 4.
 #define TWINKLE_SPEED 4
+
+// Overall twinkle density.
+// 0 (NONE lit) to 8 (ALL lit at once).  
+// Default is 5.
 #define TWINKLE_DENSITY 5
+
+// How often to change color palettes.
 #define SECONDS_PER_PALETTE 30
+
+// If AUTO_SELECT_BACKGROUND_COLOR is set to 1,
+// then for any palette where the first two entries 
+// are the same, a dimmed version of that color will
+// automatically be used as the background color.
 #define AUTO_SELECT_BACKGROUND_COLOR 0
+
+// If COOL_LIKE_INCANDESCENT is set to 1, colors will 
+// fade out slighted 'reddened', similar to how
+// incandescent bulbs change color as they get dim down.
 #define COOL_LIKE_INCANDESCENT 1
 
 struct TwinkleFoxData {
@@ -88,6 +110,11 @@ struct TwinkleFoxData {
           coolLikeIncandescent(COOL_LIKE_INCANDESCENT), autoSelectBackgroundColor(AUTO_SELECT_BACKGROUND_COLOR) {}
 };
 
+// Background color for 'unlit' pixels
+// Can be set to CRGB::Black if desired.
+// Example of dim incandescent fairy light background color
+// CRGB gBackgroundColor = CRGB(CRGB::FairyLight).nscale8_video(16);
+
 // Function declarations
 void chooseNextColorPalette(CRGBPalette16& pal);
 CRGB computeOneTwinkle(TwinkleFoxData& self, uint32_t ms, uint8_t salt);
@@ -95,6 +122,10 @@ uint8_t attackDecayWave8(uint8_t i);
 void coolLikeIncandescent(CRGB& c, uint8_t phase);
 
 void TwinkleFoxLoop(TwinkleFoxData& self) {
+    // "PRNG16" is the pseudorandom number generator
+    // It MUST be reset to the same starting value each time
+    // this function is called, so that the sequence of 'random'
+    // numbers that it generates is (paradoxically) stable.
     uint16_t PRNG16 = 11337;
     uint32_t clock32 = millis();
 
@@ -180,6 +211,8 @@ void coolLikeIncandescent(CRGB& c, uint8_t phase) {
 
 // Color palettes
 // Color palette definitions
+// A mostly red palette with green accents and white trim.
+// "CRGB::Gray" is used as white to keep the brightness more uniform.
 const TProgmemRGBPalette16 RedGreenWhite_p FL_PROGMEM = {
     CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, 
     CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red, 
