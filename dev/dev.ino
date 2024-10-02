95de2a56c6ed8fef60e6f73761b40ee614126755
zackees <z@zackees.com>
2024-10-01 23:09:59 -0700

    Add NoiseWave effect with NoiseWaveLoop function

--- dev/dev.ino
+++ dev/dev.ino
@@ -1,7 +1,5 @@
-
-
 #include <FastLED.h>
-
+#include "fx/noisewave.hpp"
 
 #define LED_PIN     2
 #define COLOR_ORDER BRG
@@ -9,64 +7,7 @@
 #define NUM_LEDS    100
 
 CRGB leds[NUM_LEDS];
-
-struct NoiseGenerator {
-  NoiseGenerator() {
-    iteration_scale = 10;
-    time_multiplier = 10;
-  }
-  NoiseGenerator (int32_t itScale, int32_t timeMul) : iteration_scale(itScale), time_multiplier(timeMul) {
-  }
-  // i is the position of the LED
-  uint8_t Value(int32_t i, unsigned long time_ms) const;
-  int LedValue(int32_t i, unsigned long time_ms) const;
-  int32_t iteration_scale;
-  unsigned long time_multiplier;
-};
-
-uint8_t NoiseGenerator::Value(int32_t i, unsigned long time_ms) const {
-  uint32_t input = iteration_scale*i + time_ms * time_multiplier;
-  uint16_t v1 = inoise16(input);
-  return uint8_t(v1 >> 8);
-}
-
-int NoiseGenerator::LedValue(int32_t i, unsigned long time_ms) const {
-  int val = Value(i, time_ms);
-  return max(0, val - 128) * 2;
-}
-
-
-void setup_noisewave();
-int noisewave_loop(bool clear, bool sensor_active_top, bool sensor_active_bottom);
-
-
-//#define VIS_DURATION 1000 * 60 * 10  // 10 minutes
-#define VIS_DURATION 1000 * 5   // 5 seconds
-
-
-void setup_noisewave() {
-
-}
-
-int noisewave_loop(bool clear, bool sensor_active_top, bool sensor_active_bottom) {
-  unsigned long start_t = millis();
-  unsigned long time_now = start_t;
-  NoiseGenerator noiseGeneratorRed (500, 14);
-  NoiseGenerator noiseGeneratorBlue (500, 10);
-  for (int32_t i = 0; i < NUM_LEDS; ++i) {
-    int r = noiseGeneratorRed.LedValue(i, time_now);
-    int b = noiseGeneratorBlue.LedValue(i, time_now + 100000) >> 1;
-    int g = 0;
-    leds[i].r = r;
-    leds[i].g = g;
-    leds[i].b = b;
-  }
-  return 0;
-}
-
-
-
-
+NoiseWaveData noiseWaveData(leds, NUM_LEDS);
 
 void setup() {
   delay(3000); // sanity delay
@@ -78,7 +19,8 @@ void setup() {
 
 void loop()
 {
-  noisewave_loop(false, false, false);
+  NoiseWaveLoop(noiseWaveData);
+  FastLED.show();
   FastLED.delay(1000 / 60);
 }
 
--- src/fx/noisewave.hpp
+++ src/fx/noisewave.hpp
@@ -1,84 +1,52 @@
+#pragma once
 
-
-#include <FastLED.h>
-
-
-#define LED_PIN     2
-#define COLOR_ORDER BRG
-#define CHIPSET     WS2811
-#define NUM_LEDS    100
-
-CRGB leds[NUM_LEDS];
+#include "FastLED.h"
 
 struct NoiseGenerator {
-  NoiseGenerator() {
-    iteration_scale = 10;
-    time_multiplier = 10;
-  }
-  NoiseGenerator (int32_t itScale, int32_t timeMul) : iteration_scale(itScale), time_multiplier(timeMul) {
-  }
-  // i is the position of the LED
-  uint8_t Value(int32_t i, unsigned long time_ms) const;
-  int LedValue(int32_t i, unsigned long time_ms) const;
-  int32_t iteration_scale;
-  unsigned long time_multiplier;
+    int32_t iteration_scale;
+    unsigned long time_multiplier;
+
+    NoiseGenerator() : iteration_scale(10), time_multiplier(10) {}
+    NoiseGenerator(int32_t itScale, int32_t timeMul) : iteration_scale(itScale), time_multiplier(timeMul) {}
+
+    uint8_t Value(int32_t i, unsigned long time_ms) const {
+        uint32_t input = iteration_scale * i + time_ms * time_multiplier;
+        uint16_t v1 = inoise16(input);
+        return uint8_t(v1 >> 8);
+    }
+
+    int LedValue(int32_t i, unsigned long time_ms) const {
+        int val = Value(i, time_ms);
+        return max(0, val - 128) * 2;
+    }
 };
 
-uint8_t NoiseGenerator::Value(int32_t i, unsigned long time_ms) const {
-  uint32_t input = iteration_scale*i + time_ms * time_multiplier;
-  uint16_t v1 = inoise16(input);
-  return uint8_t(v1 >> 8);
-}
+struct NoiseWaveData {
+    CRGB* leds = nullptr;
+    uint16_t num_leds = 0;
+    NoiseGenerator noiseGeneratorRed;
+    NoiseGenerator noiseGeneratorBlue;
+    unsigned long start_time = 0;
 
-int NoiseGenerator::LedValue(int32_t i, unsigned long time_ms) const {
-  int val = Value(i, time_ms);
-  return max(0, val - 128) * 2;
-}
+    NoiseWaveData(CRGB* leds, uint16_t num_leds)
+        : leds(leds), num_leds(num_leds), 
+          noiseGeneratorRed(500, 14), noiseGeneratorBlue(500, 10) {
+        start_time = millis();
+    }
+};
 
+void NoiseWaveLoop(NoiseWaveData& self) {
+    if (self.leds == nullptr || self.num_leds == 0) {
+        return;
+    }
 
-void setup_noisewave();
-int noisewave_loop(bool clear, bool sensor_active_top, bool sensor_active_bottom);
+    unsigned long time_now = millis() - self.start_time;
 
-
-//#define VIS_DURATION 1000 * 60 * 10  // 10 minutes
-#define VIS_DURATION 1000 * 5   // 5 seconds
-
-
-void setup_noisewave() {
-
-}
-
-int noisewave_loop(bool clear, bool sensor_active_top, bool sensor_active_bottom) {
-  unsigned long start_t = millis();
-  unsigned long time_now = start_t;
-  NoiseGenerator noiseGeneratorRed (500, 14);
-  NoiseGenerator noiseGeneratorBlue (500, 10);
-  for (int32_t i = 0; i < NUM_LEDS; ++i) {
-    int r = noiseGeneratorRed.LedValue(i, time_now);
-    int b = noiseGeneratorBlue.LedValue(i, time_now + 100000) >> 1;
-    int g = 0;
-    leds[i].r = r;
-    leds[i].g = g;
-    leds[i].b = b;
-  }
-  return 0;
-}
-
-
-
-
-
-void setup() {
-  delay(3000); // sanity delay
-  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
-    .setCorrection(TypicalLEDStrip)
-    .setRgbw();
-  FastLED.setBrightness(128);
-}
-
-void loop()
-{
-  noisewave_loop(false, false, false);
-  FastLED.delay(1000 / 60);
+    for (int32_t i = 0; i < self.num_leds; ++i) {
+        int r = self.noiseGeneratorRed.LedValue(i, time_now);
+        int b = self.noiseGeneratorBlue.LedValue(i, time_now + 100000) >> 1;
+        int g = 0;
+        self.leds[i] = CRGB(r, g, b);
+    }
 }
 
