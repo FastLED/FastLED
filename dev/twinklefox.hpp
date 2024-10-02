59bfbfc0b2c9171da71e605ab49d4d7597ae5d42
zackees <z@zackees.com>
2024-10-01 21:51:19 -0700

    Add TwinkleFox library and update LED initialization

--- dev/twinklefox.hpp
+++ dev/twinklefox.hpp
@@ -4,7 +4,7 @@
 #define NUM_LEDS      100
 #define LED_TYPE   WS2811
 #define COLOR_ORDER   GRB
-#define DATA_PIN        3
+#define DATA_PIN        2
 #define VOLTS          12
 #define MAX_MA       4000
 
