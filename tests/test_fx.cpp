d8c3679302dfad60433df11eac2002d189bf04a6
zackees <z@zackees.com>
2024-10-03 13:52:07 -0700

    Implement millis() function using std::chrono

--- tests/test_fx.cpp
+++ tests/test_fx.cpp
@@ -3,13 +3,16 @@
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
+#include <stdint.h>
+
 #include "doctest.h"
 #include "fx/cylon.hpp"
-//#include "fx/pride2015.hpp"
-//#include "fx/noisewave.hpp"
+#include "fx/pride2015.hpp"
+#include "fx/noisewave.hpp"
 #include "fx/_fx2d.h"
 #include "fx/_fxstrip.h"
 #include "fx/_xmap.h"
+#include <chrono>
 
 #define COMPILE_ANIMARTRIX 0
 
@@ -21,6 +24,12 @@
 // To satisfy the linker, we must also define uint16_t XY( uint8_t, uint8_t);
 // This should go away someday and only use functions supplied by the user.
 uint16_t XY( uint8_t, uint8_t) { return 0; }
+
+// Implement millis() function using std::chrono
+uint32_t millis() {
+    using namespace std::chrono;
+    return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
 
 TEST_CASE("Compile Test") {
     // Suceessful compilation test
