28aa343c2294c19f2f6a22fa2e1a4c2939bc80cb
zackees <z@zackees.com>
2024-10-03 16:14:34 -0700

    Update MAX_DIMENSION constant to 22 and optimize memory allocation

--- src/fx/2d/noisepalette.hpp
+++ src/fx/2d/noisepalette.hpp
@@ -14,7 +14,7 @@
 #include "lib8tion/random8.h"
 #include "noise.h"
 
-#define MAX_DIMENSION 16
+#define MAX_DIMENSION 22
 
 FASTLED_NAMESPACE_BEGIN
 
@@ -31,15 +31,15 @@ class NoisePalette : public FxGrid {
         z = random16();
 
         // Allocate memory for the noise array
-        noise = new uint8_t *[width];
-        for (int i = 0; i < width; ++i) {
-            noise[i] = new uint8_t[height];
+        noise = new uint8_t *[MAX_DIMENSION];
+        for (int i = 0; i < MAX_DIMENSION; ++i) {
+            noise[i] = new uint8_t[MAX_DIMENSION];
         }
     }
 
     ~NoisePalette() {
         // Free the allocated memory
-        for (int i = 0; i < width; ++i) {
+        for (int i = 0; i < MAX_DIMENSION; ++i) {
             delete[] noise[i];
         }
         delete[] noise;
@@ -66,8 +66,8 @@ class NoisePalette : public FxGrid {
                 // array for our brightness, and the flipped value from (j,i)
                 // for our pixel's index into the color palette.
 
-                uint8_t index = noise[j][i];
-                uint8_t bri = noise[i][j];
+                uint8_t index = noise[i][j];
+                uint8_t bri = noise[j][i];
 
                 // if this palette is a 'loop', add a slowly-changing base value
                 if (colorLoop) {
@@ -101,47 +101,47 @@ class NoisePalette : public FxGrid {
     uint8_t colorLoop = 1;
 
     void fillnoise8() {
-    // If we're runing at a low "speed", some 8-bit artifacts become visible
-    // from frame-to-frame.  In order to reduce this, we can do some fast
-    // data-smoothing. The amount of data smoothing we're doing depends on
-    // "speed".
-    uint8_t dataSmoothing = 0;
-    if (speed < 50) {
-        dataSmoothing = 200 - (speed * 4);
-    }
-
-    for (int i = 0; i < MAX_DIMENSION; i++) {
-        int ioffset = scale * i;
-        for (int j = 0; j < MAX_DIMENSION; j++) {
-            int joffset = scale * j;
-
-            uint8_t data = inoise8(x + ioffset, y + joffset, z);
-
-            // The range of the inoise8 function is roughly 16-238.
-            // These two operations expand those values out to roughly 0..255
-            // You can comment them out if you want the raw noise data.
-            data = qsub8(data, 16);
-            data = qadd8(data, scale8(data, 39));
-
-            if (dataSmoothing) {
-                uint8_t olddata = noise[i][j];
-                uint8_t newdata = scale8(olddata, dataSmoothing) +
-                                  scale8(data, 256 - dataSmoothing);
-                data = newdata;
-            }
-
-            noise[i][j] = data;
+        // If we're running at a low "speed", some 8-bit artifacts become visible
+        // from frame-to-frame.  In order to reduce this, we can do some fast
+        // data-smoothing. The amount of data smoothing we're doing depends on
+        // "speed".
+        uint8_t dataSmoothing = 0;
+        if (speed < 50) {
+            dataSmoothing = 200 - (speed * 4);
         }
+
+        for (int i = 0; i < MAX_DIMENSION; i++) {
+            int ioffset = scale * i;
+            for (int j = 0; j < MAX_DIMENSION; j++) {
+                int joffset = scale * j;
+
+                uint8_t data = inoise8(x + ioffset, y + joffset, z);
+
+                // The range of the inoise8 function is roughly 16-238.
+                // These two operations expand those values out to roughly 0..255
+                // You can comment them out if you want the raw noise data.
+                data = qsub8(data, 16);
+                data = qadd8(data, scale8(data, 39));
+
+                if (dataSmoothing) {
+                    uint8_t olddata = noise[i][j];
+                    uint8_t newdata = scale8(olddata, dataSmoothing) +
+                                      scale8(data, 256 - dataSmoothing);
+                    data = newdata;
+                }
+
+                noise[i][j] = data;
+            }
+        }
+
+        z += speed;
+
+        // apply slow drift to X and Y, just for visual variation.
+        x += speed / 8;
+        y -= speed / 16;
     }
 
-    z += speed;
-
-    // apply slow drift to X and Y, just for visual variation.
-    x += speed / 8;
-    y -= speed / 16;
-}
-
-    uint16_t XY(uint8_t x, uint8_t y) {
+    uint16_t XY(uint8_t x, uint8_t y) const {
         return mXyMap.mapToIndex(x, y);
     }
 
