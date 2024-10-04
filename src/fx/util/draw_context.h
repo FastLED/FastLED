9836d97631dadc904b872c89b10690b81ff7d556
Zachary Vorhies <zachvorhies@protonmail.com>
2024-10-04 16:47:44 -0700

    move some code around

---
+++ src/fx/util/draw_context.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+#include "namespace.h"
+#include "crgb.h"
+
+FASTLED_NAMESPACE_BEGIN
+
+// Abstract base class for effects on a strip/grid of LEDs.
+
+struct DrawContext {
+    uint32_t now;
+    CRGB* leds;
+    uint16_t frame_time = 0;
+    float speed = 1.0f;
+    uint8_t* alpha_channel = nullptr;
+    DrawContext(
+        uint32_t now,
+        CRGB* leds,
+        uint16_t frame_time = 0,
+        float speed = 1.0f,
+        uint8_t* alpha_channel = nullptr
+    ): now(now),
+        leds(leds),
+        frame_time(frame_time),
+        speed(speed),
+        alpha_channel(alpha_channel) {}
+};
+
+FASTLED_NAMESPACE_END
+
---
+++ src/fx/util/transition.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <stdint.h>
+#include "namespace.h"
+
+FASTLED_NAMESPACE_BEGIN
+
+class Transition {
+public:
+    Transition() : mStart(0), mDuration(0), mNotStarted(true) {}
+    ~Transition() {}
+
+    uint8_t getProgress(uint32_t now) {
+        if (mNotStarted) {
+            return 0;
+        }
+        if (now < mStart) {
+            return 0;
+        } else if (now >= mStart + mDuration) {
+            return 255;
+        } else {
+            return ((now - mStart) * 255) / mDuration;
+        }
+    }
+
+    void start(uint32_t now, uint32_t duration) {
+        mNotStarted = false;
+        mStart = now;
+        mDuration = duration;
+    }
+
+    bool isTransitioning(uint32_t now) {
+        if (mNotStarted) {
+            return false;
+        }   
+        return now >= mStart && now < mStart + mDuration;
+    }
+
+private:
+    uint32_t mStart;
+    uint32_t mDuration;
+    bool mNotStarted;
+};
+
+
+FASTLED_NAMESPACE_END
+
