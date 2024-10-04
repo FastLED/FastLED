dbdf0152b9f5f44bca0fc4c224edc0887c83f9c8
Zachary Vorhies <zachvorhies@protonmail.com>
2024-10-03 21:13:30 -0700

    initial commit (#1745)
    * initial commit
    
    * Add FASTLED_NAMESPACE_BEGIN and FASTLED_NAMESPACE_END
    
    * Update begin method to include chip select parameter

---
+++ src/fx/storage/sd.cpp
@@ -0,0 +1,8 @@
+#include "fx/storage/sd.h"
+
+#if FASTLED_HAS_SD
+#include "sd_std.hpp"
+#endif
+
+
+
---
+++ src/fx/storage/sd.h
@@ -0,0 +1,44 @@
+#pragma once
+
+
+#include <stdint.h>
+#include <stddef.h>
+#include "filehandle.h"
+#include "namespace.h"
+
+
+#if __has_include(<SdFat.h>)
+#define FASTLED_HAS_SD 1
+#else
+#define FASTLED_HAS_SD 0
+#endif
+
+FASTLED_NAMESPACE_BEGIN
+
+namespace storage
+{
+
+class SdCardSpi {
+  public:
+    SdCardSpi(int clock, int data) : mClockPin(clock), mDataPin(data) {}
+    SdCardSpi() {}  // Use default pins for spi.
+    bool begin(int chipSelect);
+    //  End use of card
+    void end();
+
+    // Not implemented yet.
+    // bool restart();
+
+    FileHandle* open(const char *name, uint8_t oflag);
+    void close(FileHandle *file);
+    bool isOpen() const { return mChipSelect >= 0; }
+
+  private:
+    int mClockPin = -1;
+    int mDataPin = -1;
+    int mChipSelect = -1;
+};
+
+} // namespace storage
+
+FASTLED_NAMESPACE_END
