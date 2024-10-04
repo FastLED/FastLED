cf67735f0d6922581449a38ec70cb3443b4a6d1c
Zachary Vorhies <zachvorhies@protonmail.com>
2024-10-03 17:42:36 -0700

    Np (#1744)
    Refactor noise playground to be an fx and point everything to that

--- dev/noisepalette.hpp
+++ dev/noisepalette.hpp
@@ -1,33 +1,17 @@
-/// @file    NoisePlusPalette.ino
-/// @brief   Demonstrates how to mix noise generation with color palettes on a 2D LED matrix
-/// @example NoisePlusPalette.ino
-
-#ifndef COMPILE_NOISEPLUSPALETTE
-#if defined(__AVR_ATtinyxy4__)
-// too large for ATtiny1604, so skip this.
-#define COMPILE_NOISEPLUSPALETTE 0
-#else
-#define COMPILE_NOISEPLUSPALETTE 1
-#endif
-#endif  // COMPILE_NOISEPLUSPALETTE
-
-
-#if COMPILE_NOISEPLUSPALETTE
-
+#include "fx/2d/noisepalette.hpp"
 #include <FastLED.h>
 
-#define LED_PIN     2
-#define BRIGHTNESS  96
-#define LED_TYPE    WS2811
+#include <iostream>
+
+#define LED_PIN 2
 #define COLOR_ORDER GRB
+#define CHIPSET WS2811
 
-// Params for width and height
-const uint8_t kMatrixWidth  = 22;
-const uint8_t kMatrixHeight = 22;
+#define MATRIX_WIDTH 22
+#define MATRIX_HEIGHT 22
+#define GRID_SERPENTINE 1
 
-// Param for different pixel layouts
-const bool    kMatrixSerpentineLayout = true;
-
+#define NUM_LEDS (MATRIX_WIDTH * MATRIX_HEIGHT)
 
 // This example combines two features of FastLED to produce a remarkable range of
 // effects from a relatively small amount of code.  This example combines FastLED's 
@@ -54,254 +38,41 @@ const bool    kMatrixSerpentineLayout = true;
 // used, as well as some 'hand-defined' ones, and some proceedurally generated
 // palettes.
 
-
-#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
-#define MAX_DIMENSION ((kMatrixWidth>kMatrixHeight) ? kMatrixWidth : kMatrixHeight)
-
-// The leds
-CRGB leds[kMatrixWidth * kMatrixHeight];
-
-// The 16 bit version of our coordinates
-static uint16_t x;
-static uint16_t y;
-static uint16_t z;
+// Scale determines how far apart the pixels in our noise matrix are.  Try
+// changing these values around to see how it affects the motion of the display.  The
+// higher the value of scale, the more "zoomed out" the noise iwll be.  A value
+// of 1 will be so zoomed in, you'll mostly see solid colors.
+#define SCALE 20
 
 // We're using the x/y dimensions to map to the x/y pixels on the matrix.  We'll
 // use the z-axis for "time".  speed determines how fast time moves forward.  Try
 // 1 for a very slow moving effect, or 60 for something that ends up looking like
 // water.
-uint16_t speed = 20; // speed is set dynamically once we've started up
+#define SPEED 30
 
-// Scale determines how far apart the pixels in our noise matrix are.  Try
-// changing these values around to see how it affects the motion of the display.  The
-// higher the value of scale, the more "zoomed out" the noise iwll be.  A value
-// of 1 will be so zoomed in, you'll mostly see solid colors.
-uint16_t scale = 30; // scale is set dynamically once we've started up
-
-// This is the array that we keep our computed noise values in
-uint8_t noise[MAX_DIMENSION][MAX_DIMENSION];
-
-CRGBPalette16 currentPalette( PartyColors_p );
-uint8_t       colorLoop = 1;
+CRGB leds[NUM_LEDS];
+XYMap xyMap(MATRIX_WIDTH, MATRIX_HEIGHT, GRID_SERPENTINE);
+NoisePalette noisePalette(leds, xyMap);
 
 void setup() {
-  delay(3000);
-  FastLED.addLeds<LED_TYPE,LED_PIN,COLOR_ORDER>(leds,NUM_LEDS);
-  FastLED.setBrightness(BRIGHTNESS);
-
-  // Initialize our coordinates to some random values
-  x = random16();
-  y = random16();
-  z = random16();
-}
-
-
-
-// Fill the x/y array of 8-bit noise values using the inoise8 function.
-void fillnoise8() {
-  // If we're runing at a low "speed", some 8-bit artifacts become visible
-  // from frame-to-frame.  In order to reduce this, we can do some fast data-smoothing.
-  // The amount of data smoothing we're doing depends on "speed".
-  uint8_t dataSmoothing = 0;
-  if( speed < 50) {
-    dataSmoothing = 200 - (speed * 4);
-  }
-  
-  for(int i = 0; i < MAX_DIMENSION; i++) {
-    int ioffset = scale * i;
-    for(int j = 0; j < MAX_DIMENSION; j++) {
-      int joffset = scale * j;
-      
-      uint8_t data = inoise8(x + ioffset,y + joffset,z);
-
-      // The range of the inoise8 function is roughly 16-238.
-      // These two operations expand those values out to roughly 0..255
-      // You can comment them out if you want the raw noise data.
-      data = qsub8(data,16);
-      data = qadd8(data,scale8(data,39));
-
-      if( dataSmoothing ) {
-        uint8_t olddata = noise[i][j];
-        uint8_t newdata = scale8( olddata, dataSmoothing) + scale8( data, 256 - dataSmoothing);
-        data = newdata;
-      }
-      
-      noise[i][j] = data;
-    }
-  }
-  
-  z += speed;
-  
-  // apply slow drift to X and Y, just for visual variation.
-  x += speed / 8;
-  y -= speed / 16;
-}
-
-uint16_t XY( uint8_t x, uint8_t y);
-void ChangePaletteAndSettingsPeriodically();
-void SetupRandomPalette();
-void SetupBlackAndWhiteStripedPalette();
-void SetupPurpleAndGreenPalette();
-
-void mapNoiseToLEDsUsingPalette()
-{
-  static uint8_t ihue=0;
-  
-  for(int i = 0; i < kMatrixWidth; i++) {
-    for(int j = 0; j < kMatrixHeight; j++) {
-      // We use the value at the (i,j) coordinate in the noise
-      // array for our brightness, and the flipped value from (j,i)
-      // for our pixel's index into the color palette.
-
-      uint8_t index = noise[j][i];
-      uint8_t bri =   noise[i][j];
-
-      // if this palette is a 'loop', add a slowly-changing base value
-      if( colorLoop) { 
-        index += ihue;
-      }
-
-      // brighten up, as the color palette itself often contains the 
-      // light/dark dynamic range desired
-      if( bri > 127 ) {
-        bri = 255;
-      } else {
-        bri = dim8_raw( bri * 2);
-      }
-
-      CRGB color = ColorFromPalette( currentPalette, index, bri);
-      leds[XY(i,j)] = color;
-    }
-  }
-  
-  ihue+=1;
+    delay(1000); // sanity delay
+    FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+        .setCorrection(TypicalLEDStrip);
+    FastLED.setBrightness(96);
+    noisePalette.lazyInit();
+    noisePalette.setSpeed(SPEED);
+    noisePalette.setScale(SCALE);
 }
 
 void loop() {
-  // Periodically choose a new palette, speed, and scale
-  ChangePaletteAndSettingsPeriodically();
-
-  // generate noise data
-  fillnoise8();
-  
-  // convert the noise data to colors in the LED array
-  // using the current palette
-  mapNoiseToLEDsUsingPalette();
-
-  FastLED.show();
-  // delay(10);
-}
-
-
-
-
-
-// There are several different palettes of colors demonstrated here.
-//
-// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
-// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
-//
-// Additionally, you can manually define your own color palettes, or you can write
-// code that creates color palettes on the fly.
-
-// 1 = 5 sec per palette
-// 2 = 10 sec per palette
-// etc
-#define HOLD_PALETTES_X_TIMES_AS_LONG 1
-
-
-
-void ChangePaletteAndSettingsPeriodically()
-{
-  uint8_t secondHand = ((millis() / 1000) / HOLD_PALETTES_X_TIMES_AS_LONG) % 60;
-  static uint8_t lastSecond = 99;
-  
-  if( lastSecond != secondHand) {
-    lastSecond = secondHand;
-    if( secondHand ==  0)  { currentPalette = RainbowColors_p;         speed = 20; scale = 30; colorLoop = 1; }
-    if( secondHand ==  5)  { SetupPurpleAndGreenPalette();             speed = 10; scale = 50; colorLoop = 1; }
-    if( secondHand == 10)  { SetupBlackAndWhiteStripedPalette();       speed = 20; scale = 30; colorLoop = 1; }
-    if( secondHand == 15)  { currentPalette = ForestColors_p;          speed =  8; scale =120; colorLoop = 0; }
-    if( secondHand == 20)  { currentPalette = CloudColors_p;           speed =  4; scale = 30; colorLoop = 0; }
-    if( secondHand == 25)  { currentPalette = LavaColors_p;            speed =  8; scale = 50; colorLoop = 0; }
-    if( secondHand == 30)  { currentPalette = OceanColors_p;           speed = 20; scale = 90; colorLoop = 0; }
-    if( secondHand == 35)  { currentPalette = PartyColors_p;           speed = 20; scale = 30; colorLoop = 1; }
-    if( secondHand == 40)  { SetupRandomPalette();                     speed = 20; scale = 20; colorLoop = 1; }
-    if( secondHand == 45)  { SetupRandomPalette();                     speed = 50; scale = 50; colorLoop = 1; }
-    if( secondHand == 50)  { SetupRandomPalette();                     speed = 90; scale = 90; colorLoop = 1; }
-    if( secondHand == 55)  { currentPalette = RainbowStripeColors_p;   speed = 30; scale = 20; colorLoop = 1; }
-  }
-}
-
-// This function generates a random palette that's a gradient
-// between four different colors.  The first is a dim hue, the second is 
-// a bright hue, the third is a bright pastel, and the last is 
-// another bright hue.  This gives some visual bright/dark variation
-// which is more interesting than just a gradient of different hues.
-void SetupRandomPalette()
-{
-  currentPalette = CRGBPalette16( 
-                      CHSV( random8(), 255, 32), 
-                      CHSV( random8(), 255, 255), 
-                      CHSV( random8(), 128, 255), 
-                      CHSV( random8(), 255, 255)); 
-}
-
-// This function sets up a palette of black and white stripes,
-// using code.  Since the palette is effectively an array of
-// sixteen CRGB colors, the various fill_* functions can be used
-// to set them up.
-void SetupBlackAndWhiteStripedPalette()
-{
-  // 'black out' all 16 palette entries...
-  fill_solid( currentPalette, 16, CRGB::Black);
-  // and set every fourth one to white.
-  currentPalette[0] = CRGB::White;
-  currentPalette[4] = CRGB::White;
-  currentPalette[8] = CRGB::White;
-  currentPalette[12] = CRGB::White;
-
-}
-
-// This function sets up a palette of purple and green stripes.
-void SetupPurpleAndGreenPalette()
-{
-  CRGB purple = CHSV( HUE_PURPLE, 255, 255);
-  CRGB green  = CHSV( HUE_GREEN, 255, 255);
-  CRGB black  = CRGB::Black;
-  
-  currentPalette = CRGBPalette16( 
-    green,  green,  black,  black,
-    purple, purple, black,  black,
-    green,  green,  black,  black,
-    purple, purple, black,  black );
-}
-
-
-//
-// Mark's xy coordinate mapping code.  See the XYMatrix for more information on it.
-//
-uint16_t XY( uint8_t x, uint8_t y)
-{
-  uint16_t i;
-  if( kMatrixSerpentineLayout == false) {
-    i = (y * kMatrixWidth) + x;
-  }
-  if( kMatrixSerpentineLayout == true) {
-    if( y & 0x01) {
-      // Odd rows run backwards
-      uint8_t reverseX = (kMatrixWidth - 1) - x;
-      i = (y * kMatrixWidth) + reverseX;
-    } else {
-      // Even rows run forwards
-      i = (y * kMatrixWidth) + x;
+    EVERY_N_MILLISECONDS(1000) {
+        uint8_t n = noisePalette.getPalettePresetCount();
+        int idx = noisePalette.getPalettePreset();
+        idx = (idx + 1) % n;
+        noisePalette.setPalettePreset(idx);
+        std::cout << "Changed to palette " << idx << std::endl;
     }
-  }
-  return i;
+
+    noisePalette.draw();
+    FastLED.delay(1000 / 60);
 }
-
-#else
-void setup() {}
-void loop() {}
-#endif // COMPILE_NOISEPLUSPALETTE
-
---
+++ src/fx/2d/noisepalette.hpp
@@ -0,0 +1,266 @@
+/// @file    NoisePlusPalette.hpp
+/// @brief   Demonstrates how to mix noise generation with color palettes on a
+/// 2D LED matrix
+/// @example NoisePlusPalette.hpp
+
+#pragma once
+
+#include <stdint.h>
+
+#include "FastLED.h"
+#include "fx/_fx2d.h"
+#include "fx/_xymap.h"
+#include "lib8tion/random8.h"
+#include "noise.h"
+#include "ptr.h"
+
+FASTLED_NAMESPACE_BEGIN
+
+class NoisePalette : public FxGrid {
+  public:
+    NoisePalette(CRGB *leds, XYMap xyMap)
+        : FxGrid(xyMap), leds(leds), scale(scale), speed(speed), colorLoop(1) {
+        width = xyMap.getWidth();
+        height = xyMap.getHeight();
+
+        // Initialize our coordinates to some random values
+        x = random16();
+        y = random16();
+        z = random16();
+
+        setPalettePreset(0);
+
+        // Allocate memory for the noise array using scoped_ptr
+        noise = scoped_ptr<uint8_t>(new uint8_t[width * height]);
+    }
+
+    // No need for a destructor, scoped_ptr will handle memory deallocation
+
+    void lazyInit() override {}
+
+    void draw() override {
+        fillnoise8();
+        mapNoiseToLEDsUsingPalette();
+    }
+
+    const char *fxName() const override { return "NoisePalette"; }
+
+    void mapNoiseToLEDsUsingPalette();
+
+    uint8_t changeToRandomPalette();
+
+    // There are 12 palette indexes but they don't have names. Use this to set
+    // which one you want.
+    uint8_t getPalettePresetCount() const { return 12; }
+    uint8_t getPalettePreset() const { return currentPaletteIndex; }
+    void setPalettePreset(int paletteIndex);
+    void setPalette(const CRGBPalette16 &palette, uint16_t speed,
+                    uint16_t scale, bool colorLoop) {
+        currentPalette = palette;
+        this->speed = speed;
+        this->scale = scale;
+        this->colorLoop = colorLoop;
+    }
+    void setSpeed(uint16_t speed) { this->speed = speed; }
+    void setScale(uint16_t scale) { this->scale = scale; }
+
+  private:
+    CRGB *leds;
+    uint16_t x, y, z;
+    uint16_t width, height;
+    uint16_t speed = 0;
+    uint16_t scale = 0;
+    scoped_ptr<uint8_t> noise;
+    CRGBPalette16 currentPalette = PartyColors_p;
+    bool colorLoop = 0;
+    int currentPaletteIndex = 0;
+
+    void fillnoise8();
+
+    uint16_t XY(uint8_t x, uint8_t y) const { return mXyMap.mapToIndex(x, y); }
+
+    void SetupRandomPalette() {
+        CRGBPalette16 newPalette;
+        do {
+            newPalette = CRGBPalette16(
+                CHSV(random8(), 255, 32), CHSV(random8(), 255, 255),
+                CHSV(random8(), 128, 255), CHSV(random8(), 255, 255));
+        } while (newPalette == currentPalette);
+        currentPalette = newPalette;
+    }
+
+    void SetupBlackAndWhiteStripedPalette() {
+        fill_solid(currentPalette, 16, CRGB::Black);
+        currentPalette[0] = CRGB::White;
+        currentPalette[4] = CRGB::White;
+        currentPalette[8] = CRGB::White;
+        currentPalette[12] = CRGB::White;
+    }
+
+    void SetupPurpleAndGreenPalette() {
+        CRGB purple = CHSV(HUE_PURPLE, 255, 255);
+        CRGB green = CHSV(HUE_GREEN, 255, 255);
+        CRGB black = CRGB::Black;
+
+        currentPalette = CRGBPalette16(
+            green, green, black, black, purple, purple, black, black, green,
+            green, black, black, purple, purple, black, black);
+    }
+};
+
+inline void NoisePalette::setPalettePreset(int paletteIndex) {
+    currentPaletteIndex = paletteIndex % 12; // Ensure the index wraps around
+    switch (currentPaletteIndex) {
+    case 0:
+        currentPalette = RainbowColors_p;
+        speed = 20;
+        scale = 30;
+        colorLoop = 1;
+        break;
+    case 1:
+        SetupPurpleAndGreenPalette();
+        speed = 10;
+        scale = 50;
+        colorLoop = 1;
+        break;
+    case 2:
+        SetupBlackAndWhiteStripedPalette();
+        speed = 20;
+        scale = 30;
+        colorLoop = 1;
+        break;
+    case 3:
+        currentPalette = ForestColors_p;
+        speed = 8;
+        scale = 120;
+        colorLoop = 0;
+        break;
+    case 4:
+        currentPalette = CloudColors_p;
+        speed = 4;
+        scale = 30;
+        colorLoop = 0;
+        break;
+    case 5:
+        currentPalette = LavaColors_p;
+        speed = 8;
+        scale = 50;
+        colorLoop = 0;
+        break;
+    case 6:
+        currentPalette = OceanColors_p;
+        speed = 20;
+        scale = 90;
+        colorLoop = 0;
+        break;
+    case 7:
+        currentPalette = PartyColors_p;
+        speed = 20;
+        scale = 30;
+        colorLoop = 1;
+        break;
+    case 8:
+    case 9:
+    case 10:
+        SetupRandomPalette();
+        speed = 20 + (currentPaletteIndex - 8) * 5;
+        scale = 20 + (currentPaletteIndex - 8) * 5;
+        colorLoop = 1;
+        break;
+    case 11:
+        currentPalette = RainbowStripeColors_p;
+        speed = 2;
+        scale = 20;
+        colorLoop = 1;
+        break;
+    }
+}
+
+inline void NoisePalette::mapNoiseToLEDsUsingPalette() {
+    static uint8_t ihue = 0;
+
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            // We use the value at the (i,j) coordinate in the noise
+            // array for our brightness, and the flipped value from (j,i)
+            // for our pixel's index into the color palette.
+
+            uint8_t index = noise.get()[i * height + j];
+            uint8_t bri = noise.get()[j * width + i];
+
+            // if this palette is a 'loop', add a slowly-changing base value
+            if (colorLoop) {
+                index += ihue;
+            }
+
+            // brighten up, as the color palette itself often contains the
+            // light/dark dynamic range desired
+            if (bri > 127) {
+                bri = 255;
+            } else {
+                bri = dim8_raw(bri * 2);
+            }
+
+            CRGB color = ColorFromPalette(currentPalette, index, bri);
+            leds[XY(i, j)] = color;
+        }
+    }
+
+    ihue += 1;
+}
+
+inline void NoisePalette::fillnoise8() {
+    // If we're running at a low "speed", some 8-bit artifacts become
+    // visible from frame-to-frame.  In order to reduce this, we can do some
+    // fast data-smoothing. The amount of data smoothing we're doing depends
+    // on "speed".
+    uint8_t dataSmoothing = 0;
+    if (speed < 50) {
+        dataSmoothing = 200 - (speed * 4);
+    }
+
+    for (int i = 0; i < width; i++) {
+        int ioffset = scale * i;
+        for (int j = 0; j < height; j++) {
+            int joffset = scale * j;
+
+            uint8_t data = inoise8(x + ioffset, y + joffset, z);
+
+            // The range of the inoise8 function is roughly 16-238.
+            // These two operations expand those values out to roughly
+            // 0..255 You can comment them out if you want the raw noise
+            // data.
+            data = qsub8(data, 16);
+            data = qadd8(data, scale8(data, 39));
+
+            if (dataSmoothing) {
+                uint8_t olddata = noise.get()[i * height + j];
+                uint8_t newdata = scale8(olddata, dataSmoothing) +
+                                  scale8(data, 256 - dataSmoothing);
+                data = newdata;
+            }
+
+            noise.get()[i * height + j] = data;
+        }
+    }
+
+    z += speed;
+
+    // apply slow drift to X and Y, just for visual variation.
+    x += speed / 8;
+    y -= speed / 16;
+}
+
+inline uint8_t NoisePalette::changeToRandomPalette() {
+    while (true) {
+        uint8_t new_idx = random8() % 12;
+        if (new_idx == currentPaletteIndex) {
+            continue;
+        }
+        currentPaletteIndex = new_idx;
+        setPalettePreset(currentPaletteIndex);
+        return currentPaletteIndex;
+    }
+}
+
+FASTLED_NAMESPACE_END
