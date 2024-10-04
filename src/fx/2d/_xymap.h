3800b87d9b06ff43e479797269ab63e2aefe8189
zackees <z@zackees.com>
2024-10-03 17:59:33 -0700

    force inline

--- src/fx/2d/_xymap.h
+++ src/fx/2d/_xymap.h
@@ -3,8 +3,9 @@
 #include <stdint.h>
 #include <string.h>
 #include "ptr.h"
+#include "force_inline.h"
 
-uint16_t xy_serpentine(uint16_t x, uint16_t y, uint16_t width, uint16_t height) {
+FASTLED_FORCE_INLINE uint16_t xy_serpentine(uint16_t x, uint16_t y, uint16_t width, uint16_t height) {
     if (y & 1) // Even or odd row?
         // reverse every second line for a serpentine lled layout
         return (y + 1) * width - 1 - x;
@@ -12,7 +13,7 @@ uint16_t xy_serpentine(uint16_t x, uint16_t y, uint16_t width, uint16_t height) 
         return y * width + x;
 }
 
-uint16_t xy_line_by_line(uint16_t x, uint16_t y, uint16_t width, uint16_t height) {
+FASTLED_FORCE_INLINE uint16_t xy_line_by_line(uint16_t x, uint16_t y, uint16_t width, uint16_t height) {
     return y * width + x;
 }
 
