832ca1b5e5cc1f55ed5e26872a90763306f0a41f
zackees <z@zackees.com>
2024-10-05 15:03:47 -0700

    Refactor FxEngine header includes and formatting

--- src/fx/fx_engine.h
+++ src/fx/fx_engine.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <stdint.h>
-#include "namespace.h"
 #include "crgb.h"
-#include "fx/fx.h"
-#include "ptr.h"
 #include "fixed_vector.h"
+#include "fx/fx.h"
+#include "namespace.h"
+#include "ptr.h"
+#include <stdint.h>
 #include <string.h>
 
 #ifndef FASTLED_FX_ENGINE_MAX_FX
@@ -16,20 +16,20 @@ FASTLED_NAMESPACE_BEGIN
 
 struct Layer;
 typedef RefPtr<Layer> LayerPtr;
-struct Layer: public Referent {
+struct Layer : public Referent {
     scoped_array<CRGB> surface;
     scoped_array<uint8_t> surface_alpha;
     // RefPtr<Fx> fx;
 };
 
 class FxEngine {
-public:
+  public:
     FxEngine(uint16_t numLeds);
     ~FxEngine();
 
-    bool addFx(Fx* effect);
-    //void startTransition(uint32_t now, uint32_t duration);
-    void draw(uint32_t now, CRGB* outputBuffer);
+    bool addFx(Fx *effect);
+    // void startTransition(uint32_t now, uint32_t duration);
+    void draw(uint32_t now, CRGB *outputBuffer);
     bool nextFx(uint32_t now, uint32_t duration) {
         uint16_t next_index = (mCurrentIndex + 1) % mEffects.size();
         return setNextFx(next_index, now, duration);
@@ -40,7 +40,8 @@ public:
             return false;
         }
         if (mIsTransitioning) {
-            // If already transitioning, complete the current transition immediately
+            // If already transitioning, complete the current transition
+            // immediately
             mEffects[mCurrentIndex]->pause();
             mCurrentIndex = mNextIndex;
             mIsTransitioning = false;
@@ -52,9 +53,9 @@ public:
         return true;
     }
 
-private:
+  private:
     uint16_t mNumLeds;
-    FixedVector<Fx*, FASTLED_FX_ENGINE_MAX_FX> mEffects;
+    FixedVector<Fx *, FASTLED_FX_ENGINE_MAX_FX> mEffects;
     LayerPtr mLayers[2];
     bool mIsTransitioning;
     bool mWasTransitioning;
@@ -62,21 +63,22 @@ private:
     uint16_t mNextIndex;
     Transition mTransition;
     void startTransition(uint32_t now, uint32_t duration);
-
 };
 
-inline FxEngine::FxEngine(uint16_t numLeds) 
-    : mNumLeds(numLeds), mIsTransitioning(false), mCurrentIndex(0), mNextIndex(0) {
+inline FxEngine::FxEngine(uint16_t numLeds)
+    : mNumLeds(numLeds), mIsTransitioning(false), mCurrentIndex(0),
+      mNextIndex(0) {
     mLayers[0] = LayerPtr::FromHeap(new Layer());
     mLayers[1] = LayerPtr::FromHeap(new Layer());
-    // TODO: When there is only Fx in the list then don't allocate memory for the second layer
+    // TODO: When there is only Fx in the list then don't allocate memory for
+    // the second layer
     mLayers[0]->surface.reset(new CRGB[numLeds]);
     mLayers[1]->surface.reset(new CRGB[numLeds]);
 }
 
 inline FxEngine::~FxEngine() {}
 
-inline bool FxEngine::addFx(Fx* effect) {
+inline bool FxEngine::addFx(Fx *effect) {
     if (mEffects.size() >= FASTLED_FX_ENGINE_MAX_FX) {
         return false;
     }
@@ -84,35 +86,37 @@ inline bool FxEngine::addFx(Fx* effect) {
     return true;
 }
 
-inline void FxEngine::draw(uint32_t now, CRGB* finalBuffer) {
+inline void FxEngine::draw(uint32_t now, CRGB *finalBuffer) {
     if (!mEffects.empty()) {
         Fx::DrawContext context = {now, mLayers[0]->surface.get()};
         mEffects[mCurrentIndex]->draw(context);
-        
-        if (mIsTransitioning && mEffects.size() > 1) {
-            context = {now, mLayers[1]->surface.get()};
-            mEffects[mNextIndex]->draw(context);
-            
-            uint8_t progress = mTransition.getProgress(now);
-            uint8_t inverse_progress = 255 - progress;
 
-            for (uint16_t i = 0; i < mNumLeds; i++) {
-                CRGB p0 = mLayers[0]->surface[i].nscale8(inverse_progress);
-                CRGB p1 = mLayers[1]->surface[i].nscale8(progress);
-                finalBuffer[i] = p0 + p1;
-            }
+        if (!mIsTransitioning || mEffects.size() < 2) {
+            memcpy(finalBuffer, mLayers[0]->surface.get(),
+                   sizeof(CRGB) * mNumLeds);
+            return;
+        }
 
-            if (progress == 255) {
-                // Transition complete, update current index
-                mEffects[mCurrentIndex]->pause();
-                mCurrentIndex = mNextIndex;
-                mIsTransitioning = false;
-            }
-        } else {
-            memcpy(finalBuffer, mLayers[0]->surface.get(), sizeof(CRGB) * mNumLeds);
+        context = {now, mLayers[1]->surface.get()};
+        mEffects[mNextIndex]->draw(context);
+
+        uint8_t progress = mTransition.getProgress(now);
+        uint8_t inverse_progress = 255 - progress;
+
+        for (uint16_t i = 0; i < mNumLeds; i++) {
+            CRGB p0 = mLayers[0]->surface[i].nscale8(inverse_progress);
+            CRGB p1 = mLayers[1]->surface[i].nscale8(progress);
+            finalBuffer[i] = p0 + p1;
+        }
+
+        if (progress == 255) {
+            // Transition complete, update current index
+            mEffects[mCurrentIndex]->pause();
+            mCurrentIndex = mNextIndex;
+            mIsTransitioning = false;
         }
     }
 }
 
-FASTLED_NAMESPACE_END
 
+FASTLED_NAMESPACE_END
