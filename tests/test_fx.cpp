976debad2218d57288e968d9cad0701a79781a81
zackees <z@zackees.com>
2024-10-06 13:41:22 -0700

    Update test_fx.cpp includes and XY function definition

--- tests/test_fx.cpp
+++ tests/test_fx.cpp
@@ -7,21 +7,19 @@
 
 #include "doctest.h"
 #include "fx/1d/cylon.hpp"
-#include "fx/1d/pride2015.hpp"  // needs XY defined or linker error.
+#include "fx/1d/demoreel100.hpp"
 #include "fx/1d/noisewave.hpp"
-#include "fx/2d/noisepalette.hpp"
-#include "fx/2d/animartrix.hpp"
-#include "fx/2d/scale_up.hpp"
 #include "fx/1d/pacifica.hpp"
-
+#include "fx/1d/pride2015.hpp" // needs XY defined or linker error.
+#include "fx/1d/twinklefox.hpp"
+#include "fx/2d/animartrix.hpp"
+#include "fx/2d/noisepalette.hpp"
+#include "fx/2d/scale_up.hpp"
 
 // To satisfy the linker, we must also define uint16_t XY( uint8_t, uint8_t);
 // This should go away someday and only use functions supplied by the user.
-uint16_t XY( uint8_t, uint8_t) { return 0; }
-
-
+uint16_t XY(uint8_t, uint8_t) { return 0; }
 
 TEST_CASE("Compile Test") {
     // Suceessful compilation test
 }
-
