12315d3dc169676c195ad13776de7b5b16e440c7
zackees <z@zackees.com>
2024-10-07 01:43:30 -0700

    Add RedSquare effect for 2D LED grids

--- tests/test_fx.cpp
+++ tests/test_fx.cpp
@@ -15,6 +15,7 @@
 #include "fx/2d/animartrix.hpp"
 #include "fx/2d/noisepalette.hpp"
 #include "fx/2d/scale_up.hpp"
+#include "fx/2d/redsquare.hpp"
 
 // To satisfy the linker, we must also define uint16_t XY( uint8_t, uint8_t);
 // This should go away someday and only use functions supplied by the user.
