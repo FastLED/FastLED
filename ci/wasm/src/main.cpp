d9d7e4689dc1390574bf7316d117dbd68fa8a52c
zackees <z@zackees.com>
2024-10-10 00:21:16 -0700

    docs: update comments for wasm build preview of FastLED

--- ci/wasm/src/main.cpp
+++ ci/wasm/src/main.cpp
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <emscripten/emscripten.h> // Include Emscripten headers
 
-// You can run this in node like the following
+// This is a very early preview of a the wasm build of FastLED.
+// Right now this demo only works in node.js, but the goal is to make it work in the browser, too.
+// 
+// To do this demo you are going to grab a copy of the emscripten build. Just look at the wasm builds on the front
+// page of fastled and you'll find it in one of the build steps. Small too.
+// Then make sure you have node installed.
+//
+// Open up node.
 //  $ node
 //  > const foo = await import('./fastled.js')
 //  > foo.default()
 //  > console.log(await foo.default()[0])
 //  ---> prints out "Hello from FastLED"
+//  ## On node using require(...)
 //  Or alternatively, you can run this:
 //  > let fastled = require('./fastled');
 //  > fastled();
