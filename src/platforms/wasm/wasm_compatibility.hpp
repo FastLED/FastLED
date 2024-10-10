6865d6f76cd99a5a37f0b35318e060d663b9c44f
zackees <z@zackees.com>
2024-10-10 16:26:16 -0700

    refactor: reorganize wasm compatibility header file

--- src/platforms/wasm/wasm_compatibility.hpp
+++ src/platforms/wasm/wasm_compatibility.hpp
@@ -23,11 +23,15 @@ extern "C" {
 
     // Replacement for 'millis' in WebAssembly context
     uint32_t millis() {
+        #if 0
         if (g_animation_running) {
             return real_millis() - g_time_offset;
         } else {
             return g_pause_time;
         }
+        #else
+        real_millis();
+        #endif
     }
 
     // Replacement for 'micros' in WebAssembly context
@@ -40,20 +44,21 @@ extern "C" {
         std::this_thread::sleep_for(std::chrono::milliseconds(ms));
     }
 
+
     // Function to pause the animation and set the pause time
     void fastled_pause_timer() {
-        g_animation_running = false;
-        g_pause_time = millis();
+        //g_animation_running = false;
+        //g_pause_time = millis();
     }
 
     // Function to resume the animation
     void fastled_resume_timer() {
-        if (!g_animation_running) {
-            uint32_t current_time = real_millis();
-            uint32_t elapsed_pause_time = current_time - g_pause_time;
-            g_time_offset += elapsed_pause_time;
-            g_animation_running = true;
-        }
+        //if (!g_animation_running) {
+        //    uint32_t current_time = real_millis();
+        //    uint32_t elapsed_pause_time = current_time - g_pause_time;
+        //    g_time_offset += elapsed_pause_time;
+        //    g_animation_running = true;
+        //}
     }
 
     // Function to check if animation is running
