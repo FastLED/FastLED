27386011efbbb23a330e18e97e50dcfdad7bb398
zackees <z@zackees.com>
2024-10-09 18:26:29 -0700

    refactor(ci/wasm): improve wasm build configuration

--- ci/wasm/wasm.py
+++ ci/wasm/wasm.py
@@ -1,3 +1,4 @@
+import os
 
 Import("env", "projenv")
 
@@ -7,27 +8,39 @@ Import("env", "projenv")
 # this is kinda hacky but let's just replace the default toolchain
 # with emscripten. the right way to do this would be to create a
 # SCons toolchain file for emscripten, and platformio platform for
-# WebAssembly, but this is easier for nowprojenv.Replace(CC="emcc", CXX="em++", LINK="em++", AR="emar", RANLIB="emranlib")
+# WebAssembly, but this is easier for now
+
+projenv.Replace(CC="emcc", CXX="em++", LINK="em++", AR="emar", RANLIB="emranlib")
+
 env.Replace(CC="emcc", CXX="em++", LINK="em++", AR="emar", RANLIB="emranlib")
 
 wasmflags = [
-    "-s", "EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap']",
-    "-s", "ALLOW_MEMORY_GROWTH=1",
-    "-s", "EXPORTED_FUNCTIONS=['_malloc', '_free', '_main']",
-    "-s", "INITIAL_MEMORY=1073741824",
-    "-s", "STACK_SIZE=536870912",
-    "-s", "ENVIRONMENT=web",
+    "--oformat=js",
+    "-s",
+    "EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap']",
+    "-s",
+    "ALLOW_MEMORY_GROWTH=1",
+    "-s",
+    "EXPORTED_FUNCTIONS=['_malloc', '_free', '_main']",
+    "-s",
+    "INITIAL_MEMORY=1073741824",
+    "-s",
+    "STACK_SIZE=536870912"
 ]
 
-# Set output files
-output_dir = env.subst('$BUILD_DIR')
-output_base = f"{output_dir}/output"
-wasmflags += [
-    "-s",
-    "MODULARIZE=1",
-    "-o", f"{output_base}.html"]
+export_name = env.GetProjectOption("custom_wasm_export_name", "")
+if export_name:
+    wasmflags += [
+        "-s",
+        "MODULARIZE=1",
+        "-s",
+        f"EXPORT_NAME='{export_name}'",
+        "-o",
+        f"{env.subst('$BUILD_DIR')}/{export_name}.js",
+    ]
 
 env.Append(LINKFLAGS=wasmflags)
+
 
 # Pass flags to the other Project Dependencies (libraries)
 for lb in env.GetLibBuilders():
