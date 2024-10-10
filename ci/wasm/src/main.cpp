47f739584d0be347803437b3b983044d97e93844
zackees <z@zackees.com>
2024-10-09 19:25:51 -0700

    feat(ci): add fastled.js for WebAssembly integration

--- ci/wasm/src/main.cpp
+++ ci/wasm/src/main.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <emscripten/emscripten.h> // Include Emscripten headers
 
-
+// You can run this in node like the following
+//  $ node
+//  > const foo = await import('./fastled.js')
+//  > foo.default()
+//  > console.log(await foo.default()[0])
+//  ---> prints out "Hello from FastLED"
+//  Or alternatively, you can run this:
+//  > let fastled = require('./fastled');
+//  > fastled();
 EMSCRIPTEN_KEEPALIVE extern "C" int main() {
    printf("Hello from FastLED\r\n");
    return 0;
