06694dd410e36ab64ed7d0115c774128be060c10
zackees <z@zackees.com>
2024-10-09 17:33:01 -0700

    refactor: update main function definition for C++ compatibility

--- ci/wasm/src/main.cpp
+++ ci/wasm/src/main.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <emscripten/emscripten.h> // Include Emscripten headers
 
-EMSCRIPTEN_KEEPALIVE extern "C" int main() {
+#ifdef __cplusplus
+#define EXTERN extern "C"
+#else
+#define EXTERN
+#endif
+
+EMSCRIPTEN_KEEPALIVE EXTERN "C" int main() {
    printf("Hello from FastLED\r\n");
    return 0;
 }
