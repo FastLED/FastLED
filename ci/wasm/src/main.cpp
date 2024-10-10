f3f41701cfcf764d66bdfdc14d2e3dab196e454e
zackees <z@zackees.com>
2024-10-09 19:00:29 -0700

    refactor: simplify main function for Emscripten integration

--- ci/wasm/src/main.cpp
+++ ci/wasm/src/main.cpp
@@ -1,26 +1,8 @@
-#include <emscripten.h>
-#include <emscripten/html5.h>
 #include <stdio.h>
+#include <emscripten/emscripten.h> // Include Emscripten headers
 
-// Our "main loop" function. This callback receives the current time as
-// reported by the browser, and the user data we provide in the call to
-// emscripten_request_animation_frame_loop().
-bool one_iter(double time, void* userData) {
-  // Can render to the screen here, etc.
-  puts("FastLED!");
-  // Return true to keep the loop running.
-  return true;
-}
 
-int main() {
-#ifdef __EMSCRIPTEN__
-  // Receives a function to call and some user data to provide it.
-  emscripten_request_animation_frame_loop(one_iter, 0);
-#else
-  while (1) {
-    one_iter();
-    // Delay to keep frame rate constant (using SDL).
-    SDL_Delay(time_to_next_frame());
-  }
-#endif
+EMSCRIPTEN_KEEPALIVE extern "C" int main() {
+   printf("Hello from FastLED\r\n");
+   return 0;
 }
