aae534417bbdce7ffd7e6bb387bbdcaeb065fec1
Zachary Vorhies <zachvorhies@protonmail.com>
2024-10-11 11:41:50 -0700

    add a way to push messages into fastled.js

--- src/platforms/stub/wasm/message_queue.cpp
+++ src/platforms/stub/wasm/message_queue.cpp
@@ -6,6 +6,8 @@
 FASTLED_NAMESPACE_BEGIN
 
 static std::deque<std::string> message_queue;
+static const size_t MAX_QUEUE_SIZE = 100;
+static size_t missed_messages_count = 0;
 
 bool js_messages_available() {
     return !message_queue.empty();
@@ -21,8 +23,24 @@ bool js_message_pop_front(std::string* message) {
 }
 
 bool js_message_push_back(const std::string& message) {
+    if (message_queue.size() >= MAX_QUEUE_SIZE) {
+        message_queue.pop_front();
+        missed_messages_count++;
+    }
     message_queue.push_back(message);
     return true;
+}
+
+size_t js_get_missed_messages_count() {
+    return missed_messages_count;
+}
+
+size_t js_get_message_count() {
+    return message_queue.size();
+}
+
+size_t js_get_max_message_count() {
+    return MAX_QUEUE_SIZE;
 }
 
 FASTLED_NAMESPACE_END
--- src/platforms/stub/wasm/message_queue.h
+++ src/platforms/stub/wasm/message_queue.h
@@ -25,4 +25,22 @@ bool js_message_pop_front(std::string* message);
  */
 bool js_message_push_back(const std::string& message);
 
+/**
+ * @brief Get the count of messages that were missed due to queue overflow.
+ * @return The number of missed messages.
+ */
+size_t js_get_missed_messages_count();
+
+/**
+ * @brief Get the current number of messages in the queue.
+ * @return The number of messages currently in the queue.
+ */
+size_t js_get_message_count();
+
+/**
+ * @brief Get the maximum number of messages allowed in the queue.
+ * @return The maximum number of messages allowed in the queue.
+ */
+size_t js_get_max_message_count();
+
 FASTLED_NAMESPACE_END
