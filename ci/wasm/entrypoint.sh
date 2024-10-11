876bf2169b43bf570dfa93a13e02c288a9c5da6f
Zachary Vorhies <zachvorhies@protonmail.com>
2024-10-11 12:55:34 -0700

    inject export headers in the wasm compiler so the client doesn't need to include this

--- ci/wasm/entrypoint.sh
+++ ci/wasm/entrypoint.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 set -e
+
+# Function to insert the header
+insert_header() {
+    local file="$1"
+    # Remove any existing include of _exports.hpp
+    sed -i '/#include "platforms\/stub\/wasm\/_exports.hpp"/d' "$file"
+    # Add the include at the beginning of the file
+    sed -i '1i#include "platforms/stub/wasm/_exports.hpp"' "$file"
+    echo "Processed: $file"
+}
+
+# Copy the contents of the hosts mapped directory to the container
 cp -r /mapped/* /js/
+
+# Remove the .pio directory copy, if it exists because this could contain build
+# artifacts from a previous build
 rm -rf /js/.pio
 cp -r /wasm/* /js/
 cd /js
-for ino_file in *.ino; do
-    if [ -f "$ino_file" ]; then
-        echo -e "\n#include \"platforms/stub/wasm/led_sysdefs_wasm.hpp\"" >> "$ino_file"
-    fi
+
+# Find all .ino, .h, .hpp, and .cpp files recursively and process them
+find . -type f \( -name "*.ino" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) | while read -r file; do
+    insert_header "$file"
 done
+
 pio run
 rm -rf /mapped/fastled_js/*
 mkdir -p /mapped/fastled_js
