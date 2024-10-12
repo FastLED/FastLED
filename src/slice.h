518112479c5c7c57519ee2ef6c0890cee6898dcd
zackees <z@zackees.com>
2024-10-11 23:12:58 -0700

    feat: add Slice template class for array manipulation

---
+++ src/slice.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <stdint.h>
+
+template<typename T>
+class Slice {
+public:
+    Slice(T* data, size_t size) : mData(data), mSize(size) {}
+
+    T& operator[](size_t index) {
+        // No bounds checking in embedded environment
+        return mData[index];
+    }
+
+    const T& operator[](size_t index) const {
+        // No bounds checking in embedded environment
+        return mData[index];
+    }
+
+    T* begin() const {
+        return mData;
+    }
+
+    T* end() const {
+        return mData + mSize;
+    }
+
+    size_t length() const {
+        return mSize;
+    }
+
+    const T* data() const {
+        return mData;
+    }
+
+    T* data() {
+        return mData;
+    }
+
+    Slice<T> slice(size_t start, size_t end) const {
+        // No bounds checking in embedded environment
+        return Slice<T>(mData + start, end - start);
+    }
+
+    Slice<T> slice(size_t start) const {
+        // No bounds checking in embedded environment
+        return Slice<T>(mData + start, mSize - start);
+    }
+
+private:
+    T* mData;
+    size_t mSize;
+};
