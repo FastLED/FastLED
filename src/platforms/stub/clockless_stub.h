c496eeed1491c70a429b3fdc8f3d39dc965f30df
zackees <z@zackees.com>
2024-10-12 18:15:28 -0700

    feat: implement clockless controller for Emscripten and generic builds

--- src/platforms/stub/clockless_stub.h
+++ src/platforms/stub/clockless_stub.h
@@ -1,25 +1,9 @@
-#ifndef __INC_CLOCKLESS_STUB_H
-#define __INC_CLOCKLESS_STUB_H
+#pragma once
 
-#include "namespace.h"
-
-FASTLED_NAMESPACE_BEGIN
-
-#if defined(FASTLED_STUB_IMPL)
-
-#define FASTLED_HAS_CLOCKLESS 1
-
-template <int DATA_PIN, int T1, int T2, int T3, EOrder RGB_ORDER = RGB, int XTRA0 = 0, bool FLIP = false, int WAIT_TIME = 0>
-class ClocklessController : public CPixelLEDController<RGB_ORDER> {
-public:
-	virtual void init() { }
-
-protected:
-	virtual void showPixels(PixelController<RGB_ORDER> & pixels) { }
-};
-
-#endif // defined(FASTLED_STUB_IMPL)
-
-FASTLED_NAMESPACE_END
-
-#endif // __INC_CLOCKLESS_STUB_H
+#if defined(__EMSCRIPTEN__)
+#include "platforms/stub/wasm/clockless.h"
+#elif defined(FASTLED_STUB_IMPL)
+#include "platforms/stub/clockless_stub_generic.h"
+#else
+#error "This file should only be included for stub or emscripten builds"
+#endif
---
+++ src/platforms/stub/wasm/clockless.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef __EMSCRIPTEN__
+#error "This file should only be included in an Emscripten build"
+#endif
+
+#include "namespace.h"
+#include "eorder.h"
+
+FASTLED_NAMESPACE_BEGIN
+
+
+#define FASTLED_HAS_CLOCKLESS 1
+
+template <int DATA_PIN, int T1, int T2, int T3, EOrder RGB_ORDER = RGB, int XTRA0 = 0, bool FLIP = false, int WAIT_TIME = 0>
+class ClocklessController : public CPixelLEDController<RGB_ORDER> {
+public:
+	virtual void init() { }
+
+protected:
+	virtual void showPixels(PixelController<RGB_ORDER> & pixels) { }
+};
+
+FASTLED_NAMESPACE_END
