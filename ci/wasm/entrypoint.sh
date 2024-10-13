43483291692b93f64aa363477a043acf6bfe351a
zackees <z@zackees.com>
2024-10-12 17:13:31 -0700

    enable instant compile for the docker container

--- ci/wasm/entrypoint.sh
+++ ci/wasm/entrypoint.sh
@@ -20,38 +20,42 @@ mkdir -p /js/src
 cp -r /mapped/* /js/src
 
 
-
-# If there is an ino file in the src directory, then rename it to main.cpp. Add
-# a special case if there already is a main.cpp file, in this case we will
-# rename it to main2.hpp, then generate the main.cpp file and then include the main2.hpp
-# file in the main.cpp file.
-if [ -f /js/src/*.ino ]; then
-    # Check if main.cpp already exists
-    if [ -f /js/src/main.cpp ]; then
-        # special case, main.cpp exists, so we want to rename it to main2.hpp
-        mv /js/src/main.cpp /js/src/main2.hpp
+include_deps() {
+    # If there is an ino file in the src directory, then rename it to main.cpp. Add
+    # a special case if there already is a main.cpp file, in this case we will
+    # rename it to main2.hpp, then generate the main.cpp file and then include the main2.hpp
+    # file in the main.cpp file.
+    if [ -f /js/src/*.ino ]; then
+        # Check if main.cpp already exists
+        if [ -f /js/src/main.cpp ]; then
+            # special case, main.cpp exists, so we want to rename it to main2.hpp
+            mv /js/src/main.cpp /js/src/main2.hpp
+        fi
+        mv /js/src/*.ino /js/src/main.cpp
+        # If main2.hpp exists (because it was renamed, then append it to main.cpp)
+        if [ -f /js/src/main2.hpp ]; then
+            # the main2.hpp file was created, so include it.
+            echo '#include "main2.hpp"' >> /js/src/main.cpp
+        fi
     fi
-    mv /js/src/*.ino /js/src/main.cpp
-    # If main2.hpp exists (because it was renamed, then append it to main.cpp)
-    if [ -f /js/src/main2.hpp ]; then
-        # the main2.hpp file was created, so include it.
-        echo '#include "main2.hpp"' >> /js/src/main.cpp
-    fi
-fi
 
-
-
+    # Find all .ino, .h, .hpp, and .cpp files recursively and process them
+    find src -type f \( -name "*.ino" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) | while read -r file; do
+        insert_header "$file"
+    done
+}
 
 # Remove the .pio directory copy, if it exists because this could contain build
 # artifacts from a previous build
 cd /js
 
 
-# Find all .ino, .h, .hpp, and .cpp files recursively and process them
-find src -type f \( -name "*.ino" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp" \) | while read -r file; do
-    insert_header "$file"
-done
-
+# We only want to run this compile pre-step once. Leave a breadcrumb to indicate
+# that we have already done this.
+if [ ! -f /js/fixed ]; then
+    include_deps
+    touch /js/fixed
+fi
 
 #############################################
 compile
@@ -60,6 +64,7 @@ compile
 
 # Ensure the directory exists
 mkdir -p /mapped/fastled_js
+
 
 # Copy (overwrite if exists) the files
 cp -f ./.pio/build/*/fastled.js /mapped/fastled_js/fastled.js
