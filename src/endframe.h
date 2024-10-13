1079e05b318e6a60c5d09a1c0a5a62c67f24fa27
zackees <z@zackees.com>
2024-10-13 00:30:24 -0700

    feat: add EndFrame and EndFrameListener classes for frame management

---
+++ src/endframe.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "singleton.h"
+#include "fixed_vector.h"
+
+class EndFrameListener {
+public:
+    virtual void onEndFrame() = 0;
+};
+
+class EndFrame : public Singleton<EndFrame> {
+public:
+    static EndFrame* getInstance() {
+        #ifdef __AVR__
+        return nullptr;  // strip out for avr.
+        #else
+        return &instance();
+        #endif
+    }
+
+    void addListener(EndFrameListener* listener) {
+        #ifdef __AVR__
+        (void)listener;
+        #else
+        mListeners.push_back(listener);
+        #endif
+    }
+
+    void callListeners() {
+        #ifdef __AVR__
+        return;
+        #else
+        for (auto listener : mListeners) {
+            listener->onEndFrame();
+        }
+        #endif
+    }
+
+private:
+    #ifndef __AVR__
+    typedef FixedVector<EndFrameListener*, 8> ListenerList;
+    ListenerList mListeners;
+    #endif
+
+    // Make constructor private to enforce singleton pattern
+    EndFrame() = default;
+    friend class Singleton<EndFrame>;
+};
