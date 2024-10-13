18f71413bf7f36174747cda70973519f9cc30597
zackees <z@zackees.com>
2024-10-13 10:29:11 -0700

    feat: implement MessageQueue class for Emscripten builds

--- src/platforms/stub/wasm/message_queue.h
+++ src/platforms/stub/wasm/message_queue.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifndef __EMSCRIPTEN__
+#error "This file should only be included in an Emscripten build"
+#endif
+
 #include <string>
 #include <deque>
 #include "namespace.h"
@@ -9,67 +13,14 @@ FASTLED_NAMESPACE_BEGIN
 
 class MessageQueue {
 public:
-    MessageQueue(size_t max_size = 100) : MAX_QUEUE_SIZE(max_size), missed_messages_count(0) {}
+    MessageQueue(size_t max_size = 100);
 
-    /**
-     * @brief Check if there are any messages available in the queue.
-     * @return true if there are messages, false otherwise.
-     */
-    bool messages_available() const {
-        return !message_queue.empty();
-    }
-
-    /**
-     * @brief Retrieve and remove the first message from the queue.
-     * @param message Pointer to a string where the message will be stored.
-     * @return true if a message was retrieved, false if the queue was empty.
-     */
-    bool message_pop_front(std::string* message) {
-        if (message_queue.empty()) {
-            return false;
-        }
-        *message = message_queue.front();
-        message_queue.pop_front();
-        return true;
-    }
-
-    /**
-     * @brief Add a new message to the back of the queue.
-     * @param message The message to be added.
-     * @return true if the message was successfully added, false otherwise.
-     */
-    EMSCRIPTEN_KEEPALIVE bool message_push_back(const char* msg) {
-        if (message_queue.size() >= MAX_QUEUE_SIZE) {
-            message_queue.pop_front();
-            missed_messages_count++;
-        }
-        message_queue.push_back(msg);
-        return true;
-    }
-
-    /**
-     * @brief Get the count of messages that were missed due to queue overflow.
-     * @return The number of missed messages.
-     */
-    size_t get_missed_messages_count() const {
-        return missed_messages_count;
-    }
-
-    /**
-     * @brief Get the current number of messages in the queue.
-     * @return The number of messages currently in the queue.
-     */
-    size_t get_message_count() const {
-        return message_queue.size();
-    }
-
-    /**
-     * @brief Get the maximum number of messages allowed in the queue.
-     * @return The maximum number of messages allowed in the queue.
-     */
-    size_t get_max_message_count() const {
-        return MAX_QUEUE_SIZE;
-    }
+    bool messages_available() const;
+    bool message_pop_front(std::string* message);
+    bool message_push_back(const char* msg);
+    size_t get_missed_messages_count() const;
+    size_t get_message_count() const;
+    size_t get_max_message_count() const;
 
 private:
     std::deque<std::string> message_queue;
