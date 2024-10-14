9611792999387e95f1c7fb376d4e736a620c4cff
zackees <z@zackees.com>
2024-10-13 21:11:32 -0700

    fix

--- src/singleton.h
+++ src/singleton.h
@@ -1,19 +1,18 @@
 #pragma once
-#include <new>
 
 // A templated singleton class, parameterized by the type of the singleton and an optional integer.
 template<typename T, int N = 0>
 class Singleton {
 public:
     static T& instance() {
-        static char buffer[sizeof(T)];
+        static char storage[sizeof(T)];
         static bool initialized = false;
         if (!initialized) {
-            // Use inplace new so that the destructor doesn't get called when the program exits.
-            new (buffer) T();
+            // Construct the object in-place using placement new
+            new (storage) T();
             initialized = true;
         }
-        return *reinterpret_cast<T*>(buffer);
+        return *reinterpret_cast<T*>(storage);
     }
     static T* instancePtr() {
         return &instance();
