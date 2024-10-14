f6d0b1f0c0c649ed64b77f896aa9460bbb60de74
zackees <z@zackees.com>
2024-10-13 18:07:27 -0700

    docs: update comments for extern_post_message function

--- src/platforms/stub/wasm/exports.h
+++ src/platforms/stub/wasm/exports.h
@@ -12,6 +12,9 @@
 
 EMSCRIPTEN_KEEPALIVE extern "C" int extern_setup();
 EMSCRIPTEN_KEEPALIVE extern "C" int extern_loop();
-// Posts a message from JavaScript to C++.
+// Posts a message from JavaScript to C++. This is as raw as you can get, with no
+// reciever control, which has to be handled by the user. It's recommended that
+// that you send all strings as encoded json strings and then have your code
+// on the backend decode the json string and figure out who the message is for.
 EMSCRIPTEN_KEEPALIVE extern "C" bool extern_post_message(const char* jstStr);
 
