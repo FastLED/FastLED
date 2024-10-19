235f038315c65b110de376655d1c4d2ad4a57f7c
zackees <z@zackees.com>
2024-10-19 13:37:57 -0700

    nit - comment

--- src/engine_events.cpp
+++ src/engine_events.cpp
@@ -15,7 +15,9 @@ EngineEvents::Listener::~Listener() {
     if (has_listener) {
         // Warning, the listener should be removed by the subclass. If we are here
         // then the subclass did not remove the listener and we are now in a partial
-        // state of destruction and the results may be undefined.
+        // state of destruction and the results may be undefined for multithreaded
+        // applications. However, for single threaded (the only option as of 2024, October)
+        // this will be ok.
         ptr->_removeListener(this);
     }
     #endif
