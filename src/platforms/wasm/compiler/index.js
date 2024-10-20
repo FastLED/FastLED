8d411ed2d843bf92ee41d3f6c4f0449c07415209
zackees <z@zackees.com>
2024-10-19 23:07:43 -0700

    style: comment out console.log statements for performance

--- src/platforms/wasm/compiler/index.js
+++ src/platforms/wasm/compiler/index.js
@@ -74,8 +74,8 @@ let previousUiState = {};
 
 
 globalThis.FastLED_onFrame = function (frameInfo, frameData, uiUpdateCallback) {
-    console.log(frameInfo);
-    console.log(frameData);
+    //console.log(frameInfo);
+    //console.log(frameData);
     updateCanvas(frameData.getFirstPixelData_Uint8());
 
     const changes = {};
