ac08313199e4c346cea359690f73f7f45021bb6b
zackees <z@zackees.com>
2024-10-23 15:57:21 -0700

    fix: correct conditional compilation for native wstring support

--- src/str.h
+++ src/str.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifndef FASTLED_HAS_NATIVE_WSTRING
-#if __has_include("wstring.h") || __has_include(<wstring.h>)
+#if __has_include("wstring.h") || __has_include(<wstring.h>) || defined(__AVR__)
 #define FASTLED_HAS_NATIVE_WSTRING 1
 #else
 #define FASTLED_HAS_NATIVE_WSTRING 0
