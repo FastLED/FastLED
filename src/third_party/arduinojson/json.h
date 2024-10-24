b00313041e82c02208ad793e8c097c6eb6cb31d7
zackees <z@zackees.com>
2024-10-23 18:33:11 -0700

    refactor: simplify ArduinoJson configuration in json.h

--- src/third_party/arduinojson/json.h
+++ src/third_party/arduinojson/json.h
@@ -1,22 +1,3 @@
 #pragma once
 
-// constrict the ArduinoJson library to only the features we need. Otherwise
-// we get compiler errors on the avr platforms and others.
-#define ARDUINOJSON_ENABLE_STD_STREAM 1
-#define ARDUINOJSON_ENABLE_STRING_VIEW 1
-#define ARDUINOJSON_ENABLE_STD_STRING 1
-#define ARDUINOJSON_ENABLE_ARDUINO_STRING 0
-#define ARDUINOJSON_ENABLE_ARDUINO_STREAM 0
-#define ARDUINOJSON_ENABLE_ARDUINO_PRINT 0
-
-#if defined(ARDUINO)
-#define __RESTORE_ARDUINO ARDUINO
-#undef ARDUINO
-#endif
-
 #include "json.hpp"
-
-#if defined(__RESTORE_ARDUINO)
-#define ARDUINO __RESTORE_ARDUINO
-#undef __RESTORE_ARDUINO
-#endif
