12e9c1b3cff2912f0bb088fb21f4aa8e79b0efbe
zackees <z@zackees.com>
2024-10-23 18:42:24 -0700

    fix: constrain ArduinoJson library features for compatibility

--- src/third_party/arduinojson/json.h
+++ src/third_party/arduinojson/json.h
@@ -1,3 +1,22 @@
 #pragma once
 
+// constrict the ArduinoJson library to only the features we need. Otherwise
+// we get compiler errors on the avr platforms and others.
+#define ARDUINOJSON_ENABLE_STD_STREAM 0
+#define ARDUINOJSON_ENABLE_STRING_VIEW 0
+#define ARDUINOJSON_ENABLE_STD_STRING 0
+#define ARDUINOJSON_ENABLE_ARDUINO_STRING 0
+#define ARDUINOJSON_ENABLE_ARDUINO_STREAM 0
+#define ARDUINOJSON_ENABLE_ARDUINO_PRINT 0
+
+#if defined(ARDUINO)
+#define __RESTORE_ARDUINO ARDUINO
+#undef ARDUINO
+#endif
+
 #include "json.hpp"
+
+#if defined(__RESTORE_ARDUINO)
+#define ARDUINO __RESTORE_ARDUINO
+#undef __RESTORE_ARDUINO
+#endif
