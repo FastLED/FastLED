37e69142b13c1b65666c7a12d10dae8c86e0284c
zackees <z@zackees.com>
2024-10-26 17:28:48 -0700

    feat: add EngineListener class with event handling methods

--- src/platforms/wasm/ui/events.cpp
+++ src/platforms/wasm/ui/events.cpp
@@ -19,6 +19,32 @@
 
 FASTLED_NAMESPACE_BEGIN
 
+// Note: Having trouble getting this into a cpp file.
+void EngineListener::Init() {
+    Singleton<EngineListener>::instance();
+}
+
+EngineListener::EngineListener() {
+    EngineEvents::addListener(this);
+}
+EngineListener::~EngineListener() {
+    EngineEvents::removeListener(this);
+}
+
+void EngineListener::onEndFrame() {
+    ActiveStripData& active_strips = ActiveStripData::Instance();
+    jsOnFrame(active_strips);
+}
+
+void EngineListener::onStripAdded(CLEDController* strip, uint32_t num_leds) {
+    int id = StripIdMap::addOrGetId(strip);
+    jsOnStripAdded(id, num_leds);
+}
+
+void EngineListener::onCanvasUiSet(CLEDController* strip, const ScreenMap& screenmap) {
+    int controller_id = StripIdMap::addOrGetId(strip);
+    jsSetCanvasSize(controller_id, screenmap);
+}
 
 
 FASTLED_NAMESPACE_END
--- src/platforms/wasm/ui/events.h
+++ src/platforms/wasm/ui/events.h
@@ -1,26 +1,13 @@
 
 
-#include <map>
-
-#include <emscripten.h>
-#include <emscripten/emscripten.h> // Include Emscripten headers
-#include <emscripten/html5.h>
-#include <emscripten/bind.h>
-#include <emscripten/val.h>
-#include "platforms/wasm/js.h"
-
-#include "platforms/wasm/active_strip_data.h"
+#include <stdint.h>
 #include "engine_events.h"
-#include "platforms/wasm/strip_id_map.h"
-#include "platforms/wasm/js.h"
-
-
 #include "namespace.h"
 
 FASTLED_NAMESPACE_BEGIN
 
 class CLEDController;
-
+class ScreenMap;
 
 class EngineListener: public EngineEvents::Listener {
 public:
@@ -28,31 +15,12 @@ public:
     static void Init();
 
 private:
-    void onEndFrame() override {
-        ActiveStripData& active_strips = ActiveStripData::Instance();
-        jsOnFrame(active_strips);
-    }
-    void onStripAdded(CLEDController* strip, uint32_t num_leds) override {
-        int id = StripIdMap::addOrGetId(strip);
-        jsOnStripAdded(id, num_leds);
-    }
-
-    void onCanvasUiSet(CLEDController* strip, const ScreenMap& screenmap) override {
-        int controller_id = StripIdMap::addOrGetId(strip);
-        jsSetCanvasSize(controller_id, screenmap);
-    }
-
-    EngineListener() {
-        EngineEvents::addListener(this);
-    }
-    ~EngineListener() {
-        EngineEvents::removeListener(this);
-    }
+    void onEndFrame() override;
+    void onStripAdded(CLEDController* strip, uint32_t num_leds) override;
+    void onCanvasUiSet(CLEDController* strip, const ScreenMap& screenmap) override;
+    EngineListener();
+    ~EngineListener();
 };
 
-// Note: Having trouble getting this into a cpp file.
-inline void EngineListener::Init() {
-    Singleton<EngineListener>::instance();
-}
 
 FASTLED_NAMESPACE_END
