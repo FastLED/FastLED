a3c7fd2278cf985cf871553edf36d4e3fa5ab0ec
zackees <z@zackees.com>
2024-10-26 17:06:50 -0700

    refactor: remove unused ui_canvas_size.hpp header file

--- src/platforms/wasm/ui/link.cpp
+++ src/platforms/wasm/ui/link.cpp
@@ -1,7 +1,6 @@
 // this will get stripped out but it's good to ensure that the code that we have is compiling as expected.
 
 #ifdef __EMSCRIPTEN__
-#include "ui_canvas_size.hpp"
 #include "engine_events.hpp"
 #include "setup_and_loop.hpp"
 #include "ui_number_field.hpp"
