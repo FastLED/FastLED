aeb4ff336bac50df49b3494bcf3ac206c2c567c8
zackees <z@zackees.com>
2024-10-29 19:08:14 -0700

    add option to determine if namespace is being used

--- src/namespace.h
+++ src/namespace.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #ifndef FASTLED_NAMESPACE
+#define FASTLED_IS_USING_NAMESPACE 0
 /// Start of the FastLED namespace
 #define FASTLED_NAMESPACE_BEGIN
 /// End of the FastLED namespace
@@ -8,6 +9,7 @@
 /// "Using" directive for the namespace
 #define FASTLED_USING_NAMESPACE
 #else
+#define FASTLED_IS_USING_NAMESPACE 1
 #define FASTLED_NAMESPACE_BEGIN namespace FASTLED_NAMESPACE {
 #define FASTLED_NAMESPACE_END }
 #define FASTLED_USING_NAMESPACE using namespace FASTLED_NAMESPACE;
