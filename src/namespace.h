29e95f83f3e7001e4c74533064fded87eb011821
zackees <z@zackees.com>
2024-10-29 20:18:15 -0700

    improve namespace and fix issues for tests

--- src/namespace.h
+++ src/namespace.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#ifdef FASTLED_FORCE_NAMESPACE
+#if defined(FASTLED_FORCE_NAMESPACE ) && !defined(FASTLED_IS_USING_NAMESPACE) && !defined(FASTLED_NAMESPACE)
 #define FASTLED_NAMESPACE fl
+#define FASTLED_IS_USING_NAMESPACE 1
 #endif
 
 #ifndef FASTLED_NAMESPACE
@@ -16,5 +17,12 @@
 #define FASTLED_IS_USING_NAMESPACE 1
 #define FASTLED_NAMESPACE_BEGIN namespace FASTLED_NAMESPACE {
 #define FASTLED_NAMESPACE_END }
+
+// We need to create an empty instance of the namespace before we can
+// declare that we are using it.
+FASTLED_NAMESPACE_BEGIN
+FASTLED_NAMESPACE_END
+
 #define FASTLED_USING_NAMESPACE using namespace FASTLED_NAMESPACE;
 #endif
+
