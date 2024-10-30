4e86b7c12d69beb97c5e24e012d237029cb480af
zackees <z@zackees.com>
2024-10-29 22:51:46 -0700

    chore: remove unused FastLED configuration defines from examples

--- examples/RGBWEmulated/RGBWEmulated.ino
+++ examples/RGBWEmulated/RGBWEmulated.ino
@@ -1,9 +1,4 @@
 
-
-#define FASTLED_RMT_BUILTIN_DRIVER 0
-#define FASTLED_EXPERIMENTAL_ESP32_RGBW_ENABLED 0
-#define FASTLED_EXPERIMENTAL_ESP32_RGBW_MODE kRGBWExactColors
-
 #include <FastLED.h>
 
 // How many leds in your strip?
