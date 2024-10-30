68807b866bb29d05af2dea6e9101a0e355664b08
zackees <z@zackees.com>
2024-10-29 20:09:10 -0700

    fix fs usage of namespace

--- src/fx/storage/fs.hpp
+++ src/fx/storage/fs.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
 #include "fs.h"
+#include "namespace.h"
 
 #ifdef __EMSCRIPTEN__
 #include "platforms/wasm/fs_wasm.h"
 #elif __has_include(<SD.h>)
 #include "fs_sdcard_arduino.hpp"
 #else
+FASTLED_NAMESPACE_BEGIN
 inline FsImplPtr make_filesystem(int cs_pin) {
     return FsImplPtr::Null();
 }
+FASTLED_NAMESPACE_END
 #endif
 
 
