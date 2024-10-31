018f445833465e2b174b6346669514dab6fc2c78
Nicholas Asch <nsasch@users.noreply.github.com>
2024-10-31 10:29:21 -0700

    Fix fill_rainbow and operator() for CPixelView with reverse direction (#1756)
    Co-authored-by: Nicholas Asch <git@pilotsworld.net>

--- src/pixelset.h
+++ src/pixelset.h
@@ -109,7 +109,7 @@ public:
     /// result in a reverse ordering for many functions (useful for mirroring).
     /// @param start the first element from this set for the new subset
     /// @param end the last element for the new subset
-    inline CPixelView operator()(int start, int end) { return CPixelView(leds, start, end); }
+    inline CPixelView operator()(int start, int end) { if(dir & 0x80) { return CPixelView(leds+len+1, -len-start-1, -len-end-1); } else { return CPixelView(leds, start, end); } }
 
     // Access an inclusive subset of the LEDs in this set, starting from the first.
     // @param end the last element for the new subset
@@ -242,7 +242,7 @@ public:
         if(dir >= 0) {
             FUNCTION_FILL_RAINBOW(leds,len,initialhue,deltahue);
         } else {
-            FUNCTION_FILL_RAINBOW(leds+len+1,-len,initialhue,deltahue);
+            FUNCTION_FILL_RAINBOW(leds + len + 1, -len, initialhue - deltahue * (len+1), -deltahue);
         }
         return *this;
     }
