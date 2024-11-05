be90ee8b12e27e5d7e268d441e2199317e10afa3
zackees <z@zackees.com>
2024-11-04 17:58:29 -0800

    feat: enhance run.py to support multiple modes of operation

--- src/platforms/wasm/compiler/run.py
+++ src/platforms/wasm/compiler/run.py
@@ -3,12 +3,17 @@ import subprocess
 import sys
 from typing import Tuple
 
+_CHOICES = [
+    "compile",
+    "server"
+]
+
 def _parse_args() -> Tuple[argparse.Namespace, list[str]]:
     parser = argparse.ArgumentParser(description="Run compile.py with additional arguments")
+    parser.add_argument("mode", help="Which mode does this script run in", choices=_CHOICES)
     return parser.parse_known_args()
 
-def main():
-    args, unknown_args = _parse_args()
+def _run_compile(unknown_args) -> None:
 
     # Construct the command to call compile.py with unknown arguments
     command = [sys.executable, 'compile.py'] + unknown_args
@@ -20,6 +25,12 @@ def main():
     print(result.stdout)
     if result.stderr:
         print(result.stderr, file=sys.stderr)
+
+def main():
+    args, unknown_args = _parse_args()
+
+    if args.mode == "compile":
+        _run_compile(unknown_args)
 
 if __name__ == "__main__":
     main()
