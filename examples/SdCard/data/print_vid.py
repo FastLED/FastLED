f092ff1cec93e3b122ce2c79d3317099ec0aae7e
zackees <z@zackees.com>
2024-11-09 15:51:15 -0800

    fix: increase the byte size limit for video data printing

--- examples/SdCard/data/print_vid.py
+++ examples/SdCard/data/print_vid.py
@@ -10,7 +10,7 @@ with open(VIDEO_DAT, "rb") as f:
     # print out the first 1000 bytes of the file as a tuple of RGB values
     size = os.path.getsize(VIDEO_DAT)
     # size is 1000 or min of 1000 and file size
-    size = min(size, 1000)
+    size = min(size, 5000)
     for i in range(size):
         r = f.read(1)
         g = f.read(1)
