ecdb7b12af873b3e5e1eb2e9d0024098d2a8416b
zackees <z@zackees.com>
2024-11-09 20:40:48 -0800

    refactor(video): restructure Video and VideoImpl classes

--- src/fx/video.h
+++ src/fx/video.h
@@ -1,44 +1,18 @@
 #pragma once
 
-#include "fx/detail/data_stream.h"
-#include "fx/frame.h"
+#include <stdint.h>
+
 #include "namespace.h"
-#include "fx/video/frame_interpolator.h"
+#include "ref.h"
+
 
 FASTLED_NAMESPACE_BEGIN
 
+// Forward declare dependencies.
 FASTLED_SMART_REF(VideoImpl);
 FASTLED_SMART_REF(ByteStream);
-FASTLED_SMART_REF(DataStream);
 FASTLED_SMART_REF(FileHandle);
-FASTLED_SMART_REF(FrameInterpolator);
-FASTLED_SMART_REF(Frame);
-
-class VideoImpl : public Referent {
-public:
-    // frameHistoryCount is the number of frames to keep in the buffer after draw. This
-    // allows for time based effects like syncing video speed to audio triggers.
-    VideoImpl(size_t pixelsPerFrame, float fpsVideo, size_t frameHistoryCount = 0);
-    ~VideoImpl();
-    // Api
-    void begin(FileHandleRef h);
-    void beginStream(ByteStreamRef s);
-    bool draw(uint32_t now, CRGB* leds, uint8_t* alpha = nullptr);
-    void end();
-    bool rewind();
-
-    // internal use
-    bool draw(uint32_t now, Frame* frame);
-    bool full() const;
-    FrameRef popOldest();
-    void pushNewest(FrameRef frame);
-
-private:
-    void updateBufferIfNecessary(uint32_t now);
-    uint32_t mPixelsPerFrame = 0;
-    DataStreamRef mStream;
-    FrameInterpolatorRef mInterpolator;
-};
+struct CRGB;
 
 class Video {
 public:
@@ -46,6 +20,7 @@ public:
     // allows for time based effects like syncing video speed to audio triggers.
     Video();  // Please use FileSytem to construct a Video.
     ~Video();
+    Video(const Video&);
     // Api
     void begin(FileHandleRef h, size_t pixelsPerFrame, float fps = 30.0f, size_t frameHistoryCount = 0);
     void beginStream(ByteStreamRef s, size_t pixelsPerFrame, float fps = 30.0f, size_t frameHistoryCount = 0);
--- src/fx/video/video.cpp
+++ src/fx/video/video.cpp
@@ -1,5 +1,10 @@
 #include "fx/video.h"
 
+#include "fx/video/frame_interpolator.h"
+#include "fx/detail/data_stream.h"
+#include "fx/frame.h"
+#include "crgb.h"
+
 #ifdef __EMSCRIPTEN__
 #define DEBUG_IO_STREAM 1
 #else
@@ -17,6 +22,37 @@ using namespace std;
 #include "namespace.h"
 
 FASTLED_NAMESPACE_BEGIN
+
+FASTLED_SMART_REF(DataStream);
+FASTLED_SMART_REF(FrameInterpolator);
+FASTLED_SMART_REF(Frame);
+
+
+class VideoImpl : public Referent {
+public:
+    // frameHistoryCount is the number of frames to keep in the buffer after draw. This
+    // allows for time based effects like syncing video speed to audio triggers.
+    VideoImpl(size_t pixelsPerFrame, float fpsVideo, size_t frameHistoryCount = 0);
+    ~VideoImpl();
+    // Api
+    void begin(FileHandleRef h);
+    void beginStream(ByteStreamRef s);
+    bool draw(uint32_t now, CRGB* leds, uint8_t* alpha = nullptr);
+    void end();
+    bool rewind();
+
+    // internal use
+    bool draw(uint32_t now, Frame* frame);
+    bool full() const;
+    FrameRef popOldest();
+    void pushNewest(FrameRef frame);
+
+private:
+    void updateBufferIfNecessary(uint32_t now);
+    uint32_t mPixelsPerFrame = 0;
+    DataStreamRef mStream;
+    FrameInterpolatorRef mInterpolator;
+};
 
 VideoImpl::VideoImpl(size_t pixelsPerFrame, float fpsVideo, size_t nFramesInBuffer)
     : mPixelsPerFrame(pixelsPerFrame),
@@ -126,6 +162,7 @@ bool VideoImpl::rewind() {
 
 Video::Video() = default;
 Video::~Video() = default;
+Video::Video(const Video&) = default;
 
 void Video::begin(FileHandleRef h, size_t pixelsPerFrame, float fps, size_t frameHistoryCount) {
     mImpl.reset();
