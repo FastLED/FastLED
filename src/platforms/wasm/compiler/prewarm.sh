4bc62a33d048936f575f627c5e27020690a927af
zackees <z@zackees.com>
2024-11-12 12:34:15 -0800

    turn off pre-warming for local compiles

--- src/platforms/wasm/compiler/prewarm.sh
+++ src/platforms/wasm/compiler/prewarm.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# if os env has PREWARM == "true"
+if [ "$PREWARM" == "true" ]; then
+  echo "Prewarming..."
+else
+  exit 0
+fi
+
 mkdir -p /mapped && cp -r /js/fastled/examples/wasm /mapped/wasm
 python /js/run.py compile --debug && rm -rf /mapped && rm -rf /js/.pio/build/wasm/src/wasm.ino.o || echo "failed to delete wasm.ino.o"
 
