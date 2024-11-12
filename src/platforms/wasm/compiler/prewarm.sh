e2663dc9103e363d4b4d991ec4d58e14944d96f1
zackees <z@zackees.com>
2024-11-11 16:06:57 -0800

    feat: add multiple compile modes for wasm prewarm script

--- src/platforms/wasm/compiler/prewarm.sh
+++ src/platforms/wasm/compiler/prewarm.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 mkdir -p /mapped && cp -r /js/fastled/examples/wasm /mapped/wasm
-python /js/run.py compile && rm -rf /mapped && rm -rf /js/.pio/build/wasm/src/wasm.ino.o || echo "failed to delete wasm.ino.o"
+python /js/run.py compile --debug && rm -rf /mapped && rm -rf /js/.pio/build/wasm/src/wasm.ino.o || echo "failed to delete wasm.ino.o"
+
+mkdir -p /mapped && cp -r /js/fastled/examples/wasm /mapped/wasm
+python /js/run.py compile --quick && rm -rf /mapped && rm -rf /js/.pio/build/wasm/src/wasm.ino.o || echo "failed to delete wasm.ino.o"
+
+mkdir -p /mapped && cp -r /js/fastled/examples/wasm /mapped/wasm
+python /js/run.py compile --release && rm -rf /mapped && rm -rf /js/.pio/build/wasm/src/wasm.ino.o || echo "failed to delete wasm.ino.o"
