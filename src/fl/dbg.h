543f27ba07e9deab7db9b76a39e06c36ad2bb067
zackees <z@zackees.com>
2024-11-19 04:19:08 -0800

    refactor: remove dummy DBG class and related test

--- src/fl/dbg.h
+++ src/fl/dbg.h
@@ -1,31 +1,12 @@
 #pragma once
-#include <ostream>
 
-namespace fl {
-
-#ifndef FASTLED_HAS_DPRINT
-// make a dummy class for DBG. It will be used like this:
-// example: FASTLED_DPRINT() << "Hello World" << endl;
-
-
-class DBG {
-public:
-    // Constructor does nothing
-    DBG() {}
-    
-    // Dummy stream operator that returns *this to allow chaining
-    template<typename T>
-    DBG& operator<<(const T&) { return *this; }
-    
-    // Special handling for endl-like manipulators
-    DBG& operator<<(std::ostream& (*manip)(std::ostream&)) { return *this; }
-};
-
-// Define the print macro
-#define FASTLED_DPRINT() fl::DBG()
-
+#ifdef __EMSCRIPTEN__
+#include "platforms/wasm/dbg_defs.h"
 #endif
 
 
-}  // namespace fl
+#ifndef FASTLED_HAS_DBG
+// FLDPRINT is a macro that can be defined to enable debug printing.
+#define FASTLED_DBG(X)((void)X)
 
+#endif
---
+++ src/platforms/wasm/dbg_defs.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define FASTLED_HAS_DBG 1
+// todo move.
+#include <iostream>
+using std::endl;
+using std::cout;
+#define FASTLED_DBG(X) cout << X
