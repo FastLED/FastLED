0821fbaab063423ea1d0176b3ca6df9333959e84
zackees <z@zackees.com>
2024-11-20 13:14:24 -0800

    style: improve formatting and organization in yvez_i2c.h

--- src/platforms/esp/32/yvez_i2c.h
+++ src/platforms/esp/32/yvez_i2c.h
@@ -2,29 +2,27 @@
 
 #include <stdint.h>
 
-#include "fixed_vector.h"
 #include "crgb.h"
+#include "fixed_vector.h"
 
 #include "namespace.h"
 
 FASTLED_NAMESPACE_BEGIN
 
-class YvezI2C
-{
-public:
-
+class YvezI2C {
+  public:
     typedef FixedVector<uint8_t, 6> Pins;
     static Pins DefaultPins() {
-    #ifdef CONFIG_IDF_TARGET_ESP32S3
-      uint8_t pins[6] = {9, 10,12,8,18,17};
-    #else
-      uint8_t pins[6] = {14, 12, 13, 25, 33, 32};
-    #endif
-      return Pins(pins);
+#ifdef CONFIG_IDF_TARGET_ESP32S3
+        return Pins({9, 10, 12, 8, 18, 17});
+#else
+        return Pins({14, 12, 13, 25, 33, 32});
+#endif
     }
- 
+
     YvezI2C(const YvezI2C &) = delete;
-    YvezI2C(const CRGB* leds, int clock_pin, int latch_pin, const Pins& pins = DefaultPins());  // FastLED bit timings. See embedded python script in chipsets.h for how to calculate these.
+    YvezI2C(const CRGB *leds, int clock_pin, int latch_pin,
+            const Pins &pins = DefaultPins());
 
     ~YvezI2C();
     void showPixels();
@@ -35,10 +33,7 @@ public:
     YvezI2C(YvezI2C &&) = delete;
     YvezI2C &operator=(const YvezI2C &) = delete;
 
-private:
+  private:
 };
 
-
 FASTLED_NAMESPACE_END
-
-
