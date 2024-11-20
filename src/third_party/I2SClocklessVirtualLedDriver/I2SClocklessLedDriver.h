f35757dec487bb8744b419a097884fdd19c4d8d6
zackees <z@zackees.com>
2024-11-20 13:49:19 -0800

    refactor: reorganize I2SClocklessLedDriver files for ESP32 support

--- src/third_party/I2SClocklessVirtualLedDriver/I2SClocklessLedDriver.h
+++ src/third_party/I2SClocklessVirtualLedDriver/I2SClocklessLedDriver.h
@@ -1,3 +1,34 @@
 
+
+#ifndef ESP32
+#error "This library is for ESP32 only"
+#endif
+
+#include "esp32-hal.h"
+
+
 // All of our FastLED specific hacks will go in here.
-#include "I2SClocklessLedDriver.hpp"
+#ifndef USE_FASTLED
+#define USE_FASTLED 1
+#endif
+
+
+#include "platforms/esp/esp_version.h"
+
+
+#if CONFIG_IDF_TARGET_ESP32S3
+#error "ESP32-S3 is not supported by this library"
+#elif CONFIG_IDF_TARGET_ESP32C3
+#error "ESP32-C3 is not supported by this library"
+#elif CONFIG_IDF_TARGET_ESP32H2
+#error "ESP32-H2 is not supported by this library"
+#elif CONFIG_IDF_TARGET_ESP32C6
+#error "ESP32-C6 is not supported by this library"
+#endif
+
+// esp32 dev
+#if CONFIG_IDF_TARGET_ESP32
+#include "espdev/I2SClocklessLedDriver.h"
+#endif
+
+
