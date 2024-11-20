c7d5292a29f2dfb91ec800dcdb3b9f06db7e4e6c
zackees <z@zackees.com>
2024-11-20 14:08:16 -0800

    refactor: simplify EspI2SDemo for ESP32 compatibility

--- examples/EspI2SDemo/EspI2SDemo.ino
+++ examples/EspI2SDemo/EspI2SDemo.ino
@@ -1,49 +1,22 @@
+#ifdef ESP32  // This example is only for the ESP32
+
+#include "esp32-hal.h"
+
+#if !(CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32)
+// this is only for ESP32 and ESP32-S3
+#else
 
 #include <FastLED.h>
-
-// How many leds in your strip?
-#define NUM_LEDS 10
-
-// For led chips like WS2812, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-// Clock pin only needed for SPI based chipsets when not using hardware SPI
-#define DATA_PIN 3
-
-// Define the array of leds
-CRGB leds[NUM_LEDS];
-
-// Time scaling factors for each component
-#define TIME_FACTOR_HUE 60
-#define TIME_FACTOR_SAT 100
-#define TIME_FACTOR_VAL 100
+#include "platforms/esp/32/yvez_i2c.h"
 
 void setup() {
-    Serial.begin(115200);
-    FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is assumed
-    FastLED.setBrightness(128);  // Set global brightness to 50%
-    delay(2000);  // If something ever goes wrong this delay will allow upload.
+    // TODO: Implement
 }
 
 void loop() {
-    uint32_t ms = millis();
-    
-    for(int i = 0; i < NUM_LEDS; i++) {
-        // Use different noise functions for each LED and each color component
-        uint8_t hue = inoise16(ms * TIME_FACTOR_HUE, i * 1000, 0) >> 8;
-        uint8_t sat = inoise16(ms * TIME_FACTOR_SAT, i * 2000, 1000) >> 8;
-        uint8_t val = inoise16(ms * TIME_FACTOR_VAL, i * 3000, 2000) >> 8;
-        
-        // Map the noise to full range for saturation and value
-        sat = map(sat, 0, 255, 30, 255);
-        val = map(val, 0, 255, 100, 255);
-        
-        leds[i] = CHSV(hue, sat, val);
-    }
-
-    FastLED.show();
-    
-    // Small delay to control the overall speed of the animation
-    //FastLED.delay(1);
-
+    // TODO: implement
+    FastLED.delay(1000);
 }
+
+#endif  // CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32
+#endif  // ESP32
