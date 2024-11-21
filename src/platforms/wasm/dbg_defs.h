59341d7acec93870ddd89cc86c8a6c6c531b504b
zackees <z@zackees.com>
2024-11-20 22:17:18 -0800

    update FASTLED_DBG

--- src/platforms/wasm/dbg_defs.h
+++ src/platforms/wasm/dbg_defs.h
@@ -5,5 +5,5 @@
 #include <iostream>
 using std::endl;
 using std::cout;
-#define _FASTLED_DGB(X) cout << X
+#define _FASTLED_DGB(X) cout << __FILE__ << "(" << __LINE__ << "): " << X
 #define FASTLED_DBG(X) _FASTLED_DGB(X) << endl
