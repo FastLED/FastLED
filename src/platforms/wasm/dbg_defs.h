35cf2b107cdb01fbbc27cd58013439a363177a5c
zackees <z@zackees.com>
2024-11-20 23:40:05 -0800

    nit dbg_defs

--- src/platforms/wasm/dbg_defs.h
+++ src/platforms/wasm/dbg_defs.h
@@ -5,5 +5,6 @@
 #include <iostream>
 using std::endl;
 using std::cout;
-#define _FASTLED_DGB(X) cout << __FILE__ << "(" << __LINE__ << "): " << X
+#define _FASTLED_DBG_FILE_OFFSET 12  // strlen("fastled/src/")
+#define _FASTLED_DGB(X) cout << (&__FILE__[_FASTLED_DBG_FILE_OFFSET]) << "(" << __LINE__ << "): " << X
 #define FASTLED_DBG(X) _FASTLED_DGB(X) << endl
