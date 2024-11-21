dea6b458d521f979e511cc9eb74466d0b5c01c8f
zackees <z@zackees.com>
2024-11-20 17:05:57 -0800

    feat: add blinking LED functionality in EspI2SDemo

--- examples/EspI2SDemo/EspI2SDemo.ino
+++ examples/EspI2SDemo/EspI2SDemo.ino
@@ -11,15 +11,28 @@
 #include <FastLED.h>
 #include "platforms/esp/32/yvez_i2s.h"
 
+// Note that this isn't the final api and this is intentionally
+// overly strict. The final api will be more flexible.
 YvezI2S::CRGBArray6Strips leds;
 YvezI2S* i2s = nullptr;
 
+void Blink(CRGB color, int times);
 
 void setup() {
     // TODO: Implement
     memset(leds.get(), 0, sizeof(leds));
     i2s = new YvezI2S(&leds, 22, 23);
 }
+
+void loop() {
+    Blink(CRGB(4, 0, 0), 1);
+    Blink(CRGB(0, 4, 0), 2);
+    Blink(CRGB(0, 0, 4), 3);
+    delay(1000);
+}
+
+
+/// Implementations
 
 void Fill(CRGB color) {
     CRGB* start = leds.get();
@@ -38,13 +51,6 @@ void Blink(CRGB color, int times) {
         i2s->showPixels();
         delay(250);
     }
-}
-
-void loop() {
-    Blink(CRGB(4, 0, 0), 1);
-    Blink(CRGB(0, 4, 0), 2);
-    Blink(CRGB(0, 0, 4), 3);
-    delay(1000);
 }
 
 #endif  // CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32
