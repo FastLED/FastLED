8264929b42f1991d886e5d9b28cff793dd99e17c
zackees <z@zackees.com>
2024-11-21 15:53:48 -0800

    update

--- src/platforms/esp/32/yves_i2s.cpp
+++ src/platforms/esp/32/yves_i2s.cpp
@@ -1,4 +1,4 @@
-#if defined(ESP32) && defined(ENABLE_ESP32_I2S_YVEZ_DRIVER) && !ENABLE_ESP32_I2S_YVEZ_DRIVER
+#if defined(ESP32) && defined(ENABLE_ESP32_I2S_YVEZ_DRIVER) && ENABLE_ESP32_I2S_YVEZ_DRIVER
 
 
 #include "platforms/esp/32/yves_i2s.h"
