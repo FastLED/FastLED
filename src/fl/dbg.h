7cad39f75d59df068703b3d5357054763b738d1b
zackees <z@zackees.com>
2024-11-22 03:30:01 -0800

    improve warn

--- src/fl/dbg.h
+++ src/fl/dbg.h
@@ -8,6 +8,8 @@
 #ifndef FASTLED_HAS_DBG
 // FLDPRINT is a macro that can be defined to enable debug printing.
 #define FASTLED_DBG(X)
+#endif
 
-
+#ifndef FASTLED_DBG_IF
+#define FASTLED_DBG_IF(COND, MSG) if (COND) FASTLED_DBG(MSG)
 #endif
