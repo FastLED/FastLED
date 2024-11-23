63043270d125bc662a0dfb037d8bd8f9230527c8
zackees <z@zackees.com>
2024-11-22 20:37:55 -0800

    feat(json): add JSON parsing and serialization functions

---
+++ src/fl/json.cpp
@@ -0,0 +1,21 @@
+#include "fl/json.h"
+#include "fl/str.h"
+
+namespace fl {
+
+bool parseJson(const char* json, FLArduinoJson::JsonDocument* doc, Str* _error) {
+    FLArduinoJson::DeserializationError error = deserializeJson(*doc, json);
+    if (error) {
+        if (_error) {
+            *_error = error.c_str();
+        }
+        return false;
+    }
+    return true;
+}
+
+void toJson(const FLArduinoJson::JsonDocument& doc, Str* jsonBuffer) {
+    serializeJson(doc, *jsonBuffer);
+}
+
+} // namespace fl
--- src/fl/json.h
+++ src/fl/json.h
@@ -1,3 +1,12 @@
 #pragma once
 
 #include "third_party/arduinojson/json.h"
+
+namespace fl {
+
+class Str;
+
+bool parseJson(const char* json, FLArduinoJson::JsonDocument* doc, Str* error = nullptr);
+void toJson(const FLArduinoJson::JsonDocument& doc, Str* jsonBuffer);
+
+} // namespace fl
