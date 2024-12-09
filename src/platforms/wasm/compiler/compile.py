ab88e8c0d408423a47ec4fb790cde414c26b4e82
zackees <z@zackees.com>
2024-12-08 16:16:03 -0800

    feat: add GraphicsManagerThreeJS for enhanced graphics rendering

--- src/platforms/wasm/compiler/compile.py
+++ src/platforms/wasm/compiler/compile.py
@@ -526,17 +526,13 @@ def main() -> int:
             shutil.copy2(INDEX_HTML_SRC, fastled_js_dir / "index.html")
             print("Copying index.css to output directory")
             shutil.copy2(INDEX_CSS_SRC, fastled_js_dir / "index.css")
-            print("Copying index.js to output directory")
-            shutil.copy2(INDEX_JS_SRC, fastled_js_dir / "index.js")
-            print("Copying ui_manager.js to output directory")
-            shutil.copy2(
-                FASTLED_COMPILER_DIR / "ui_manager.js", fastled_js_dir / "ui_manager.js"
-            )
-            print("Copying graphics_manager.js to output directory")
-            shutil.copy2(
-                FASTLED_COMPILER_DIR / "graphics_manager.js",
-                fastled_js_dir / "graphics_manager.js",
-            )
+
+            # copy all js files in FASTLED_COMPILER_DIR to output directory
+            for _file in FASTLED_COMPILER_DIR.iterdir():
+                if _file.suffix == ".js":
+                    print(f"Copying {_file.name} to output directory")
+                    shutil.copy2(_file, fastled_js_dir / _file.name)
+
             fastled_js_mem = build_dir / "fastled.js.mem"
             fastled_wasm_map = build_dir / "fastled.wasm.map"
             fastled_js_symbols = build_dir / "fastled.js.symbols"
