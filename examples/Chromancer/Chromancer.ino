b32e3b2df1575bab77b5120f4ff197b0098420c5
zackees <z@zackees.com>
2024-12-09 19:55:12 -0800

    refactor: update include paths in Chromancer example

--- examples/Chromancer/Chromancer.ino
+++ examples/Chromancer/Chromancer.ino
@@ -15,11 +15,11 @@
 #include "ripple.h"
 #include <FastLED.h>
 #include "detail.h"
-#include "screenmap.h"
-#include "math_macros.h"
-#include "json.h"
+#include "fl/screenmap.h"
+#include "fl/math_macros.h"
+#include "fl/json.h"
 #include "fl/ui.h"
-#include "fixed_map.h"
+#include "fl/map.h"
 
 #include "screenmap.json.h"
 #include "fl/str.h"
