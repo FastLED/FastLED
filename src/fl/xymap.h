eed1dd13980410f3daf9d663771b62e0a232cee1
zackees <z@zackees.com>
2024-12-13 21:08:09 -0800

    feat: add new constructors and methods for XYMap

--- src/fl/xymap.h
+++ src/fl/xymap.h
@@ -3,16 +3,15 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "crgb.h"
 #include "fl/force_inline.h"
+#include "fl/lut.h"
 #include "fl/ptr.h"
 #include "fl/xmap.h" // Include xmap.h for LUT16
-#include "fl/lut.h"
-#include "crgb.h"
 #include "namespace.h"
 
 namespace fl {
 class ScreenMap;
-
 
 FASTLED_FORCE_INLINE uint16_t xy_serpentine(uint16_t x, uint16_t y,
                                             uint16_t width, uint16_t height) {
@@ -41,114 +40,51 @@ class XYMap {
     enum XyMapType { kSerpentine = 0, kLineByLine, kFunction, kLookUpTable };
 
     static XYMap constructWithUserFunction(uint16_t width, uint16_t height,
-                                           XYFunction xyFunction, uint16_t offset = 0) {
-        XYMap out(width, height, kFunction);
-        out.xyFunction = xyFunction;
-        out.mOffset = offset;
-        return out;
-    }
+                                           XYFunction xyFunction,
+                                           uint16_t offset = 0);
 
-    static XYMap constructRectangularGrid(uint16_t width, uint16_t height, uint16_t offset = 0) {
-        XYMap out(width, height, kLineByLine);
-        out.mOffset = offset;
-        return out;
-    }
+    static XYMap constructRectangularGrid(uint16_t width, uint16_t height,
+                                          uint16_t offset = 0);
 
     static XYMap constructWithLookUpTable(uint16_t width, uint16_t height,
-                                          const uint16_t *lookUpTable, uint16_t offset = 0) {
-        XYMap out(width, height, kLookUpTable);
-        out.mLookUpTable = fl::LUT16Ptr::New(width * height);
-        memcpy(out.mLookUpTable->getData(), lookUpTable,
-               width * height * sizeof(uint16_t));
-        out.mOffset = offset;
-        return out;
-    }
+                                          const uint16_t *lookUpTable,
+                                          uint16_t offset = 0);
 
     // is_serpentine is true by default. You probably want this unless you are
     // using a different layout
-    XYMap(uint16_t width, uint16_t height, bool is_serpentine = true, uint16_t offset = 0)
-        : type(is_serpentine ? kSerpentine : kLineByLine),
-          width(width), height(height), mOffset(offset) {}
+    XYMap(uint16_t width, uint16_t height, bool is_serpentine = true,
+          uint16_t offset = 0);
 
     XYMap(const XYMap &other) = default;
 
     fl::ScreenMap toScreenMap() const;
 
-    void mapPixels(const CRGB* input, CRGB* output) const {
-        uint16_t pos = 0;
-        for (uint16_t y = 0; y < height; y++) {
-            for (uint16_t x = 0; x < width; x++) {
-                uint16_t i = pos++;
-                output[i] = input[mapToIndex(x, y)];
-            }
-        }
-    }
+    void mapPixels(const CRGB *input, CRGB *output) const;
 
-    void convertToLookUpTable() {
-        if (type == kLookUpTable) {
-            return;
-        }
-        mLookUpTable = fl::LUT16Ptr::New(width * height);
-        uint16_t *data = mLookUpTable->getData();
-        for (uint16_t y = 0; y < height; y++) {
-            for (uint16_t x = 0; x < width; x++) {
-                data[y * width + x] = mapToIndex(x, y);
-            }
-        }
-        type = kLookUpTable;
-        xyFunction = nullptr;
-    }
+    void convertToLookUpTable();
 
-    void setRectangularGrid() {
-        type = kLineByLine;
-        xyFunction = nullptr;
-        mLookUpTable.reset();
-    }
+    void setRectangularGrid();
 
     uint16_t operator()(uint16_t x, uint16_t y) const {
         return mapToIndex(x, y);
     }
 
-    uint16_t mapToIndex(uint16_t x, uint16_t y) const {
-        uint16_t index;
-        switch (type) {
-        case kSerpentine:
-            x = x % width;
-            y = y % height;
-            index = xy_serpentine(x, y, width, height);
-            break;
-        case kLineByLine:
-            index = xy_line_by_line(x, y, width, height);
-            break;
-        case kFunction:
-            x = x % width;
-            y = y % height;
-            index = xyFunction(x, y, width, height);
-            break;
-        case kLookUpTable:
-            index = mLookUpTable->getData()[y * width + x];
-            break;
-        default:
-            return 0;
-        }
-        return index + mOffset;
-    }
+    uint16_t mapToIndex(uint16_t x, uint16_t y) const;
 
-    uint16_t getWidth() const { return width; }
-    uint16_t getHeight() const { return height; }
-    uint16_t getTotal() const { return width * height; }
-    XyMapType getType() const { return type; }
+    uint16_t getWidth() const;
+    uint16_t getHeight() const;
+    uint16_t getTotal() const;
+    XyMapType getType() const;
 
   private:
-    XYMap(uint16_t width, uint16_t height, XyMapType type)
-        : type(type), width(width), height(height), mOffset(0) {}
+    XYMap(uint16_t width, uint16_t height, XyMapType type);
 
     XyMapType type;
     uint16_t width;
     uint16_t height;
     XYFunction xyFunction = nullptr;
     fl::LUT16Ptr mLookUpTable; // optional refptr to look up table.
-    uint16_t mOffset = 0;  // offset to be added to the output
+    uint16_t mOffset = 0;      // offset to be added to the output
 };
 
-}  // namespace fl
+} // namespace fl
