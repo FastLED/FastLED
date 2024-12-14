92e3400ad6134df34a6a7f9f612a9dab30f4997f
zackees <z@zackees.com>
2024-12-13 21:10:32 -0800

    feat(xmap): add XMap class for coordinate mapping

--- src/fl/xmap.h
+++ src/fl/xmap.h
@@ -3,9 +3,9 @@
 #include <stdint.h>
 #include <string.h>
 
-#include "fl/ptr.h"
 #include "fl/force_inline.h"
 #include "fl/lut.h"
+#include "fl/ptr.h"
 
 #include "namespace.h"
 
@@ -23,123 +23,47 @@ FASTLED_FORCE_INLINE uint16_t x_reverse(uint16_t x, uint16_t length) {
 // typedef for xMap function type
 typedef uint16_t (*XFunction)(uint16_t x, uint16_t length);
 
-
-// XMap holds either a function or a look up table to map x coordinates to a 1D index.
+// XMap holds either a function or a look up table to map x coordinates to a 1D
+// index.
 class XMap {
-public:
+  public:
+    enum Type { kLinear = 0, kReverse, kFunction, kLookUpTable };
 
-    enum Type {
-        kLinear = 0,
-        kReverse,
-        kFunction,
-        kLookUpTable
-    };
-
-    static XMap constructWithUserFunction(uint16_t length, XFunction xFunction, uint16_t offset = 0) {
-        XMap out = XMap(length, kFunction);
-        out.xFunction = xFunction;
-        out.mOffset = offset;
-        return out;
-    }
+    static XMap constructWithUserFunction(uint16_t length, XFunction xFunction,
+                                          uint16_t offset = 0);
 
     // When a pointer to a lookup table is passed in then we assume it's
     // owned by someone else and will not be deleted.
-    static XMap constructWithLookUpTable(uint16_t length, const uint16_t *lookUpTable, uint16_t offset = 0) {
-        XMap out = XMap(length, kLookUpTable);
-        out.mData = lookUpTable;
-        out.mOffset = offset;
-        return out;
-    }
+    static XMap constructWithLookUpTable(uint16_t length,
+                                         const uint16_t *lookUpTable,
+                                         uint16_t offset = 0);
 
     // is_reverse is false by default for linear layout
-    XMap(uint16_t length, bool is_reverse = false, uint16_t offset = 0) {
-        type = is_reverse ? kReverse : kLinear;
-        this->length = length;
-        this->mOffset = offset;
-    }
+    XMap(uint16_t length, bool is_reverse = false, uint16_t offset = 0);
 
-    XMap(const XMap &other) {
-        type = other.type;
-        length = other.length;
-        xFunction = other.xFunction;
-        mData = other.mData;
-        mLookUpTable = other.mLookUpTable;
-        mOffset = other.mOffset;
-    }
+    XMap(const XMap &other);
 
     // define the assignment operator
-    XMap& operator=(const XMap &other) {
-        if (this != &other) {
-            type = other.type;
-            length = other.length;
-            xFunction = other.xFunction;
-            mData = other.mData;
-            mLookUpTable = other.mLookUpTable;
-            mOffset = other.mOffset;
-        }
-        return *this;
-    }
+    XMap &operator=(const XMap &other) ;
 
-    void convertToLookUpTable() {
-        if (type == kLookUpTable) {
-            return;
-        }
-        mLookUpTable.reset();
-        mLookUpTable = fl::LUT16Ptr::New(length);
-        uint16_t* dataMutable = mLookUpTable->getData();
-        mData = mLookUpTable->getData();
-        for (uint16_t x = 0; x < length; x++) {
-            dataMutable[x] = mapToIndex(x);
-        }
-        type = kLookUpTable;
-        xFunction = nullptr;
-    }
+    void convertToLookUpTable();
 
-    uint16_t mapToIndex(uint16_t x) const {
-        uint16_t index;
-        switch (type) {
-            case kLinear:
-                index = x_linear(x, length);
-                break;
-            case kReverse:
-                index = x_reverse(x, length);
-                break;
-            case kFunction:
-                x = x % length;
-                index = xFunction(x, length);
-                break;
-            case kLookUpTable:
-                index = mData[x];
-                break;
-            default:
-                return 0;
-        }
-        return index + mOffset;
-    }
+    uint16_t mapToIndex(uint16_t x) const;
 
-    uint16_t operator()(uint16_t x) const {
-        return mapToIndex(x);
-    }
+    uint16_t operator()(uint16_t x) const { return mapToIndex(x); }
 
-    uint16_t getLength() const {
-        return length;
-    }
+    uint16_t getLength() const;
 
-    Type getType() const {
-        return type;
-    }
+    Type getType() const;
 
-
-private:
-    XMap(uint16_t length, Type type)
-        : length(length), type(type), mOffset(0) {
-    }
+  private:
+    XMap(uint16_t length, Type type);
     uint16_t length = 0;
     Type type = kLinear;
     XFunction xFunction = nullptr;
     const uint16_t *mData = nullptr;
     fl::LUT16Ptr mLookUpTable;
-    uint16_t mOffset = 0;  // offset to be added to the output
+    uint16_t mOffset = 0; // offset to be added to the output
 };
 
-}  // namespace fl
+} // namespace fl
