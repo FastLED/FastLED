199986942504ad92a67d39ab538577a638dbc5c8
David Madison <dmadison@users.noreply.github.com>
2024-12-15 10:36:12 -0500

    Add lib8static defines to lib8tion docs group

--- src/lib8tion/lib8static.h
+++ src/lib8tion/lib8static.h
@@ -3,7 +3,12 @@
 
 #pragma once
 
+/// @addtogroup lib8tion
+/// @{
+
 /// Define a LIB8TION member function as static inline with an "unused" attribute
 #define LIB8STATIC __attribute__ ((unused)) static inline
 /// Define a LIB8TION member function as always static inline
 #define LIB8STATIC_ALWAYS_INLINE __attribute__ ((always_inline)) static inline
+
+/// @} lib8tion
