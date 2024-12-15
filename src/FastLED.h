33ab46631601042fde2df361e52035cb58eb0e9e
zackees <z@zackees.com>
2024-12-14 19:07:12 -0800

    refactor: update FastLED header to conditionally use namespace

--- src/FastLED.h
+++ src/FastLED.h
@@ -854,3 +854,8 @@ using fl::Slider;
 #define FASTLED_DESCRIPTION(text) fl::Description g_description(text)
 #endif // FASTLED_UI
 
+
+#ifndef FASTLED_NO_AUTO_NAMESPACE
+using namespace fl;
+#endif
+
