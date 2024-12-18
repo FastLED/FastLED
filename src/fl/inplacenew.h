9af06515f3a98b1a12e3a256cfbe63ffe0b24fb9
zackees <z@zackees.com>
2024-12-17 17:56:39 -0800

    try to make inplace new more compatible

--- src/fl/inplacenew.h
+++ src/fl/inplacenew.h
@@ -2,10 +2,15 @@
 
 // This file must not be in the fl namespace, it must be in the global namespace.
 
-#if __has_include(<new>)
-#include <new>
-#else
+#ifdef __AVR__
 inline void* operator new(size_t, void* ptr) noexcept {
     return ptr;
 }
+#elif __has_include(<new>)
+#include <new>
+#elif __has_include(<new.h>)
+#include <new.h>
+#elif __has_include("new.h")
+#include "new.h"
 #endif
+
