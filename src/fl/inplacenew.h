a48751bc2fce9ebcda21ded91e2d34e9fbdc0b17
zackees <z@zackees.com>
2024-12-17 17:50:04 -0800

    fix operator new experiment

--- src/fl/inplacenew.h
+++ src/fl/inplacenew.h
@@ -2,7 +2,7 @@
 
 // This file must not be in the fl namespace, it must be in the global namespace.
 
-#if __has_include(<new>) || __has_include(<new.h>) || __has_include("new") || __has_include("new.h")
+#if __has_include(<new>)
 #include <new>
 #else
 inline void* operator new(size_t, void* ptr) noexcept {
