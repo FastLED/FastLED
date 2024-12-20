971dbcd4f954c6dff595ee93666a1d84bfeaf9f1
Zachary Vorhies <zachvorhies@protonmail.com>
2024-12-20 14:28:52 -0700

    update

--- examples/TeensyParallel/TeensyParallel.ino
+++ examples/TeensyParallel/TeensyParallel.ino
@@ -10,6 +10,59 @@ void loop() {}
 #include "third_party/object_fled/src/ObjectFLED.h"
 #include <FastLED.h>
 
+#if 0
+
+#include "crgb.h"
+#include "eorder.h"
+#include "pixel_iterator.h"
+
+template <int DATA_PIN, int T1, int T2, int T3,
+          EOrder RGB_ORDER = RGB,
+          int XTRA0 = 0,
+          bool FLIP = false,
+          int WAIT_TIME = 5>
+class ClocklessController : public CPixelLEDController<RGB_ORDER>
+{
+private:
+    // -- Verify that the pin is valid
+    static_assert(FastPin<DATA_PIN>::validpin(), "Invalid pin specified");
+
+    // -- The actual controller object for ESP32
+    RmtController5 mRMTController;
+
+public:
+    ClocklessController()
+        : mRMTController(DATA_PIN, T1, T2, T3)
+    {
+    }
+
+    void init() override { }
+    virtual uint16_t getMaxRefreshRate() const { return 400; }
+
+protected:
+
+    // Wait until the last draw is complete, if necessary.
+    virtual void* beginShowLeds() override {
+        void* data = CPixelLEDController<RGB_ORDER>::beginShowLeds();
+        mRMTController.waitForDrawComplete();
+        return data;
+    }
+
+    // Prepares data for the draw.
+    virtual void showPixels(PixelController<RGB_ORDER> &pixels) override
+    {
+        PixelIterator iterator = pixels.as_iterator(this->getRgbw());
+        mRMTController.loadPixelData(iterator);
+    }
+
+    // Send the data to the strip
+    virtual void endShowLeds(void* data) override {
+        CPixelLEDController<RGB_ORDER>::endShowLeds(data);
+        mRMTController.showPixels();
+    }
+};
+#endif // 0
+
 //configure your test cube with display object = leds, draw buffer = testCube[Z][Y][X].
 //configure a separate cube to pre-clear any physical LEDs you actually have connected
 //  with display object = blanks, draw buffer = blankLeds[Z][Y][X].
