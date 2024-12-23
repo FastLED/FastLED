8e70b9f3fb324976c5f6e7dc0f8edf54029b61a5
Zachary Vorhies <zachvorhies@protonmail.com>
2024-12-22 19:39:00 -0700

    refactor: optimize LED blur example for 2D effects

--- examples/Blur2d/Blur2d.ino
+++ examples/Blur2d/Blur2d.ino
@@ -1,10 +1,8 @@
-// Description: This example shows how to blur a strip of LEDs. It uses the blur1d function to blur the strip and fadeToBlackBy to dim the strip. A bright pixel moves along the strip.
-// Author: Zach Vorhies
+// Description: This example shows how to blur a strip of LEDs in 2d.
 
-
-#include <FastLED.h>
 #include "fl/ui.h"
 #include "fl/xymap.h"
+#include <FastLED.h>
 
 using namespace fl;
 
@@ -12,43 +10,36 @@ using namespace fl;
 #define HEIGHT 22
 
 #define NUM_LEDS (WIDTH * HEIGHT)
-#define BLUR_AMOUNT 16
-#define DATA_PIN 3  // Change this to match your LED strip's data pin
+#define BLUR_AMOUNT 172
+#define DATA_PIN 3 // Change this to match your LED strip's data pin
 #define BRIGHTNESS 255
+#define SERPENTINE true
 
 CRGB leds[NUM_LEDS];
 uint8_t pos = 0;
 bool toggle = false;
-XYMap xymap(WIDTH, HEIGHT, false);
-
+XYMap xymap(WIDTH, HEIGHT, SERPENTINE);
 
 void setup() {
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(BRIGHTNESS);
+    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS)
+      .setScreenMap(xymap);  // Necessary when using the FastLED web compiler to display properly on a web page.
+    FastLED.setBrightness(BRIGHTNESS);
 }
 
 void loop() {
-  // Add a bright pixel that moves
-  //leds[pos] = CHSV(pos * 2, 255, 255);
-  // Blur the entire strip
-  blur2d(leds, WIDTH, HEIGHT, BLUR_AMOUNT, xymap);
-  //fadeToBlackBy(leds, NUM_LEDS, 1);
-
-  EVERY_N_MILLISECONDS(250) {
-    int x = random(WIDTH);
-    int y = random(HEIGHT);
-    uint8_t r = random(255);
-    uint8_t g = random(255);
-    uint8_t b = random(255);
-    CRGB c = CRGB(r, g, b);
-    leds[xymap(x, y)] = c;
-    if (x < WIDTH - 1 && y < HEIGHT - 1) {
-      leds[xymap(x+1, y)] = c;
-      leds[xymap(x, y+1)] = c;
-      leds[xymap(x+1, y+1)] = c;
+    static int x = random(WIDTH);
+    static int y = random(HEIGHT);
+    static CRGB c = CRGB(0, 0, 0);
+    blur2d(leds, WIDTH, HEIGHT, BLUR_AMOUNT, xymap);
+    EVERY_N_MILLISECONDS(1000) {
+        x = random(WIDTH);
+        y = random(HEIGHT);
+        uint8_t r = random(255);
+        uint8_t g = random(255);
+        uint8_t b = random(255);
+        c = CRGB(r, g, b);
     }
-  }
-
-  FastLED.show();
-  delay(20);
+    leds[xymap(x, y)] = c;
+    FastLED.show();
+    delay(20);
 }
