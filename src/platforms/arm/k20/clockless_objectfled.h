3065129d2fa64c5b5a9c3084a66ee203a8fbb3cf
zackees <z@zackees.com>
2025-01-05 23:05:29 -0800

    fix: update latch delay for WS2812-5VB

--- src/platforms/arm/k20/clockless_objectfled.h
+++ src/platforms/arm/k20/clockless_objectfled.h
@@ -26,7 +26,7 @@
 #include "fl/vector.h"
 
 #ifndef FASTLED_OBJECTFLED_LATCH_DELAY
-#define FASTLED_OBJECTFLED_LATCH_DELAY 250  // WS2812-5VB
+#define FASTLED_OBJECTFLED_LATCH_DELAY 300  // WS2812-5VB
 #endif
 
 namespace fl {
