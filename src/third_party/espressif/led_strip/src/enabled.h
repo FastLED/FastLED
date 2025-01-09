17d0add0f96b461e290ab34822062215127949b7
zackees <z@zackees.com>
2025-01-09 11:51:10 -0800

    cleanup enabled for rmt

--- src/third_party/espressif/led_strip/src/enabled.h
+++ src/third_party/espressif/led_strip/src/enabled.h
@@ -1,23 +1,10 @@
 #pragma once
 
 
-#if __has_include("soc/soc_caps.h")
-  #include "soc/soc_caps.h"
-#endif
+#include "soc/soc_caps.h"
 
-#if defined(SOC_RMT_SUPPORTED) && SOC_RMT_SUPPORTED
-#define FASTLED_ESP32_HAS_RMT 1
-#else
-#define FASTLED_ESP32_HAS_RMT 0
-#endif
+#define FASTLED_ESP32_HAS_RMT (SOC_RMT_TX_CANDIDATES_PER_GROUP > 0)
 
-#ifndef FASTLED_ESP_HAS_CLOCKLESS_SPI
-#if !__has_include("driver/rmt_types.h") && !CONFIG_IDF_TARGET_ESP32C2
-#define FASTLED_ESP_HAS_CLOCKLESS_SPI 0
-#else
-#define FASTLED_ESP_HAS_CLOCKLESS_SPI 1
-#endif
-#endif
 
 #ifndef ESP32
 // No led strip component when not in ESP32 mode.
@@ -26,9 +13,20 @@
  #if !defined(FASTLED_RMT5)
   #include "platforms/esp/esp_version.h"
   #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
-   #define FASTLED_RMT5 1
+   #define FASTLED_IDF5 1
   #else
-   #define FASTLED_RMT5 0
+   #define FASTLED_IDF5 0
   #endif  // ESP_IDF_VERSION
  #endif  // FASTLED_RMT5
 #endif  // ESP32
+
+#define FASTLED_RMT5 FASTLED_IDF5
+
+
+#ifndef FASTLED_ESP_HAS_CLOCKLESS_SPI
+#if !FASTLED_IDF5
+#define FASTLED_ESP_HAS_CLOCKLESS_SPI 0
+#else
+#define FASTLED_ESP_HAS_CLOCKLESS_SPI 1
+#endif
+#endif
