cd431dba221580fb24069275defdf868fc0d88b1
zackees <z@zackees.com>
2025-01-16 15:08:07 -0800

    https://github.com/FastLED/FastLED/issues/1848

--- src/platforms/esp/32/clock_cycles.h
+++ src/platforms/esp/32/clock_cycles.h
@@ -5,7 +5,7 @@
 #include "platforms/esp/esp_version.h"
 
 #if __has_include(<hal/cpu_hal.h>)
-  // esp-idf v5.0.0+
+// esp-idf v5.0.0+
 #include <hal/cpu_hal.h>
 #define __cpu_hal_get_cycle_count esp_cpu_get_cycle_count
 #elif __has_include(<hal/cpu_ll.h>)
@@ -14,7 +14,12 @@
 inline uint32_t __cpu_hal_get_cycle_count() {
   return static_cast<uint32_t>(cpu_ll_get_cycle_count());
 }
-#define __cpu_hal_get_cycle_count cpu_hal_get_cycle_count
+#elif __has_include(<esp_cpu.h>)
+  // older esp-idf versions (?)
+#include <esp_cpu.h>
+inline uint32_t __cpu_hal_get_cycle_count() {
+  return static_cast<uint32>(esp_cpu_get_cycle_count());
+}
 #else
 #error "We don't know how to get the cycle counts on your esp-idf version. Please file a bug at https://github.com/fastled/fastled/issues"
 #endif  // ESP_IDF_VERSION
