fe9d4077fcc3c3d66da42d41f5621cfe0b76a9a2
zackees <z@zackees.com>
2025-01-21 17:12:15 -0800

    chore: update version to 3.9.12 and release notes

--- src/FastLED.h
+++ src/FastLED.h
@@ -16,13 +16,13 @@
 /// * 1 digit for the major version
 /// * 3 digits for the minor version
 /// * 3 digits for the patch version
-#define FASTLED_VERSION 3009011
+#define FASTLED_VERSION 3009012
 #ifndef FASTLED_INTERNAL
 #  ifdef  FASTLED_SHOW_VERSION
 #    ifdef FASTLED_HAS_PRAGMA_MESSAGE
-#      pragma message "FastLED version 3.009.011"
+#      pragma message "FastLED version 3.009.012"
 #    else
-#      warning FastLED version 3.009.011  (Not really a warning, just telling you here.)
+#      warning FastLED version 3.009.012  (Not really a warning, just telling you here.)
 #    endif
 #  endif
 #endif
