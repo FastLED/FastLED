95b37684838847aca6f3d5c35c40f85da679dde8
zackees <z@zackees.com>
2025-01-27 11:37:26 -0800

    fix

--- src/platforms/esp/32/clock_cycles.h
+++ src/platforms/esp/32/clock_cycles.h
@@ -10,7 +10,9 @@
 #define __cpu_hal_get_cycle_count esp_cpu_get_cycle_count
 #elif __has_include(<esp32-hal.h>)
 #include <esp32-hal.h>  // Relies on the Arduino core for ESP32
-inline uint32_t __cpu_hal_get_cycle_count cpu_hal_get_cycle_count
+inline uint32_t __cpu_hal_get_cycle_count() {
+  return static_cast<uint32_t>(cpu_hal_get_cycle_count());
+}
 #elif __has_include(<hal/cpu_ll.h>)
   // esp-idf v4.3.0+
 #include <hal/cpu_ll.h>
@@ -28,7 +30,10 @@ inline uint32_t __cpu_hal_get_cycle_count() {
   return static_cast<uint32_t>(xthal_get_ccount());
 }
 #else // Last fallback, if this fails then please file a bug at github.com/fastled/FastLED/issues and let us know what board you are using.
-#error "Cannot find a way to get the cycle count for this board. Please file a bug at github.com/FastLED/FastLED/issues"
+#include <esp32-hal.h>  // Relies on the Arduino core for ESP32
+inline uint32_t __cpu_hal_get_cycle_count() {
+  return static_cast<uint32_t>(cpu_hal_get_cycle_count());
+}
 #endif  // ESP_IDF_VERSION
 
 
