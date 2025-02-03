3dab6af24a3d0a6b513028622db24510a03363d0
zackees <z@zackees.com>
2025-02-03 00:16:52 -0800

    update

--- src/platforms/wasm/compiler/build_archive.sh
+++ src/platforms/wasm/compiler/build_archive.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+
+# if src/ does not exist, then do the copy
+if [ ! -d "src" ]; then
+    echo "Copying src/ directory"
+    python compile.py --only-copy
+    mv src/wasm.ino src/wasm.ino.cpp
+fi
+
+
+
 # Create build directory
 mkdir -p build
 cd build
