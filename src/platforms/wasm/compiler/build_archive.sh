34753e1f036960f8dd9c08c9bd62aa171228616f
zackees <z@zackees.com>
2025-02-03 20:10:00 -0800

    add comments for build speed increase

--- src/platforms/wasm/compiler/build_archive.sh
+++ src/platforms/wasm/compiler/build_archive.sh
@@ -16,6 +16,17 @@ cd build
 # Set build mode (QUICK, DEBUG, or RELEASE)
 export BUILD_MODE=${1:-QUICK}
 
+    # cmake_configure_command_list: list[str] = [
+    #     "cmake",
+    #     "-S",
+    #     str(PROJECT_ROOT / "tests"),
+    #     "-B",
+    #     str(BUILD_DIR),
+    #     "-G",
+    #     "Ninja",
+    #     "-DCMAKE_VERBOSE_MAKEFILE=ON",
+    # ]
+
 # Configure with CMake
 emcmake cmake ..
 
