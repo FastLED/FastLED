0cf0e2c3e96df5cf0cb1a7475d65d3d443835ddd
zackees <z@zackees.com>
2025-02-04 12:56:10 -0800

    build(wasm): update CMakeLists.txt for improved build configuration

--- src/platforms/wasm/compiler/build_archive.sh
+++ src/platforms/wasm/compiler/build_archive.sh
@@ -16,11 +16,16 @@ cd build
 # Set build mode: QUICK, DEBUG, or RELEASE (default is QUICK)
 export BUILD_MODE=${1:-QUICK}
 
+# Check for an optional second parameter to activate PROFILE
+PROFILE_FLAG="-DPROFILE=ON"
+
+
 if [ "$first_compile" = true ]; then
     # Configure with CMake using the Ninja generator.
     # If you want verbose build output, you can enable CMAKE_VERBOSE_MAKEFILE.
     echo "Configuring build with CMake..."
-    emcmake cmake -G Ninja -DCMAKE_VERBOSE_MAKEFILE=ON ..
+    emcmake cmake -G Ninja -DCMAKE_VERBOSE_MAKEFILE=ON ${PROFILE_FLAG} ..
+    # emcmake cmake -G Ninja
 else
     # If the build directory already exists, reconfigure with CMake.
     echo "Skipping CMake configuration as build directory already exists."
