f546e0fdc51367b02a50ed05ec9f741db5dcbf4f
Zachary Vorhies <zachvorhies@protonmail.com>
2025-02-20 18:52:21 -0700

    Initial support for Arduino GIGA R1 WiFi added (#1892)
    Co-authored-by: Rasmus <122612909+RubiCubix@users.noreply.github.com>

---
+++ src/platforms/arm/giga/led_sysdef_arm_giga.h
@@ -0,0 +1,31 @@
+#ifndef __INC_LED_SYSDEFS_ARM_GIGA_H
+#define __INC_LED_SYSDEFS_ARM_GIGA_H
+
+#define FASTLED_ARM
+
+#ifndef INTERRUPT_THRESHOLD
+#define INTERRUPT_THRESHOLD 1
+#endif
+
+// Default to allowing interrupts
+#ifndef FASTLED_ALLOW_INTERRUPTS
+#define FASTLED_ALLOW_INTERRUPTS 1
+#endif
+
+#if FASTLED_ALLOW_INTERRUPTS == 1
+#define FASTLED_ACCURATE_CLOCK
+#endif
+
+// reusing/abusing cli/sei defs for due
+#define cli()  __disable_irq();
+#define sei() __enable_irq();
+
+#define FASTLED_NO_PINMAP
+
+typedef volatile uint32_t RoReg;
+typedef volatile uint32_t RwReg;
+
+#define F_CPU 480000000
+
+#endif
+
