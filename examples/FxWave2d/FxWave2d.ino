597c8d5172884b303b699085f6ace0d99fa0d957
zackees <z@zackees.com>
2025-04-13 20:56:28 -0700

    Add wave simulation effect to FxWave2d example

---
+++ examples/FxWave2d/FxWave2d.ino
@@ -0,0 +1,49 @@
+
+
+#define TRY_WAVE_FIX 1
+
+#include "fl/ui.h"
+#include "fx/2d/wave.h"
+#include <Arduino.h>
+#include <FastLED.h>
+
+using namespace fl;
+
+#define HEIGHT 32
+#define WIDTH 32
+#define NUM_LEDS ((WIDTH) * (HEIGHT))
+
+CRGB leds[NUM_LEDS];
+
+UITitle title("FxWave2D Demo");
+UIDescription description("Shows the use of the Wave2d effect.");
+
+UIButton button("Trigger");
+WaveSimulation2D<WIDTH, HEIGHT> waveSim;
+
+XYMap xyMap(WIDTH, HEIGHT, false);
+
+void setup() {
+    Serial.begin(115200);
+    FastLED.addLeds<NEOPIXEL, 2>(leds, NUM_LEDS).setScreenMap(xyMap);
+}
+
+void loop() {
+    // Your code here
+    if (button) {
+        int x = random() % WIDTH;
+        int y = random() % HEIGHT;
+        waveSim.set(x, y, 1);
+    }
+    waveSim.update();
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            float value = waveSim.get(x, y);
+            uint8_t value8 = static_cast<uint8_t>(value * 255);
+            uint32_t idx = xyMap.mapToIndex(x, y);
+            leds[idx] = CRGB(value8, value8, value8);
+            FASTLED_WARN("leds[" << idx << "] = " << value8);
+        }
+    }
+    FastLED.show();
+}
