ac5065724d116ae5ce95a06168bbaff97caef20e
zackees <z@zackees.com>
2025-04-15 23:02:17 -0700

    Add fire effect with palette selection and speed control

---
+++ examples/FireCylinder/FireCylinder.ino
@@ -0,0 +1,111 @@
+// Perlin noise fire procedure
+// 16x16 rgb led matrix demo
+// Yaroslaw Turbin, 22.06.2020
+// https://vk.com/ldirko
+// https://www.reddit.com/user/ldirko/
+// https://www.reddit.com/r/FastLED/comments/hgu16i/my_fire_effect_implementation_based_on_perlin/
+// Based on the code found at: https://editor.soulmatelights.com/gallery/1229-
+
+// idea in make perlin noise with time offset X and Z coord
+// this automatic scroll fire pattern
+// and distort fire noise.
+// then substract Y based coodrd value to shift
+// fire color (not brightness) in palette.
+// this fadeout color from bottom matrix to up.
+// this need some palette tweak for good looking fire color
+
+#include "FastLED.h"
+#include "fl/ui.h"
+#include "fl/xymap.h"
+#include "fx/time.h"
+
+using namespace fl;
+
+#define HEIGHT 100
+#define WIDTH 100
+#define SERPENTINE true
+#define BRIGHTNESS 255
+
+TimeScale timeScale(0, 1.0f);
+
+UISlider scaleXY("Scale", 20, 1, 100, 1);
+UISlider speedY("SpeedY", 1, 1, 6, .1);
+UISlider invSpeedZ("Inverse SpeedZ", 20, 1, 100, 1);
+UISlider brightness("Brightness", 255, 0, 255, 1);
+UINumberField palette("Palette", 0, 0, 2);
+
+CRGB leds[HEIGHT * WIDTH];
+
+DEFINE_GRADIENT_PALETTE(firepal){
+    // define fire palette
+    0,   0,   0,   0,  // black
+    32,  255, 0,   0,  // red
+    190, 255, 255, 0,  // yellow
+    255, 255, 255, 255 // white
+};
+
+DEFINE_GRADIENT_PALETTE(electricGreenFirePal){
+    0,   0,   0,   0,  // black
+    32,  0,   70,  0,  // dark green
+    190, 57,  255, 20, // electric neon green
+    255, 255, 255, 255 // white
+};
+
+DEFINE_GRADIENT_PALETTE(electricBlueFirePal) {
+    0,   0,   0,   0,    // Black
+    32,  0,   0,  70,    // Dark blue
+    128, 20, 57, 255,    // Electric blue
+    255, 255, 255, 255   // White
+};
+
+XYMap xyMap(HEIGHT, WIDTH, SERPENTINE);
+
+void setup() {
+    Serial.begin(115200);
+    FastLED.addLeds<NEOPIXEL, 3>(leds, HEIGHT * WIDTH).setScreenMap(xyMap);
+    FastLED.setCorrection(TypicalLEDStrip);
+}
+
+uint8_t getPaletteIndex(uint32_t millis32, int i, int width, int max_width, uint32_t y_speed) {
+    // get palette index
+    uint16_t scale = scaleXY.as<uint16_t>();
+    uint16_t x = i * scale;
+    uint32_t y = width * scale + y_speed;
+    uint16_t z = millis32 / invSpeedZ.as<uint16_t>();
+    uint32_t xx = 0;
+    uint16_t noise16 = inoise16(x << 8, xx << 8, y << 8, z << 8);
+    uint8_t noise_val = noise16 >> 8;
+    int8_t subtraction_factor = abs8(width - (max_width - 1)) * 255 / (max_width - 1);
+    return qsub8(noise_val, subtraction_factor);
+}
+
+CRGBPalette16 getPalette() {
+    // get palette
+    switch (palette) {
+    case 0:
+        return firepal;
+    case 1:
+        return electricGreenFirePal;
+    case 2:
+        return electricBlueFirePal;
+    default:
+        return firepal;
+    }
+}
+
+void loop() {
+    FastLED.setBrightness(brightness);
+    CRGBPalette16 myPal = getPalette();
+    uint32_t now = millis();
+    timeScale.setScale(speedY);
+    uint32_t y_speed = timeScale.update(now);
+    for (int height = 0; height < HEIGHT; height++) {
+        for (int width = 0; width < WIDTH; width++) {
+            uint8_t palette_index = getPaletteIndex(now, height, width, WIDTH, y_speed);
+            CRGB c = ColorFromPalette(myPal, palette_index, BRIGHTNESS);
+            int index = xyMap((HEIGHT - 1) - height, (WIDTH - 1) - width);
+            leds[index] = c;
+        }
+    }
+    FastLED.show();
+}
