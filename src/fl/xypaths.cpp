f40e54ffda42183acf9b7feadd87b542a11db44c
zackees <z@zackees.com>
2025-04-18 05:37:32 -0700

    Update x and y translation parameters in at16 function

--- src/fl/xypaths.cpp
+++ src/fl/xypaths.cpp
@@ -30,7 +30,7 @@ void XYPath::initLutOnce() {
 }
 
 pair_xy<uint16_t> XYPath::at16(uint16_t alpha, uint16_t scale,
-                               uint16_t x_translate, uint16_t y_translate) {
+                               uint16_t x_translate_left, uint16_t y_translate_down) {
     if (mSteps > 0) {
         initLutOnce();
         if (mLut) {
@@ -43,8 +43,8 @@ pair_xy<uint16_t> XYPath::at16(uint16_t alpha, uint16_t scale,
     float scalef = static_cast<float>(scale);
     float alpha_f = static_cast<float>(alpha) / scalef;
     pair_xy<float> xy = at(alpha_f);
-    return {static_cast<uint16_t>(xy.x * scalef) + x_translate,
-            static_cast<uint16_t>(xy.y * scalef) + y_translate};
+    return {static_cast<uint16_t>(xy.x * scalef) - x_translate_left,
+            static_cast<uint16_t>(xy.y * scalef) - y_translate_down};
 }
 
 void XYPath::buildLut(uint16_t steps) {
