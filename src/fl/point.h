9667c59824b6b681b8529e792b510bf7a3a5124c
zackees <z@zackees.com>
2025-04-19 21:12:35 -0700

    Refactor point_xy_traits to point_xy_math for operations

--- src/fl/point.h
+++ src/fl/point.h
@@ -1,49 +1,7 @@
 
 namespace fl {
 
-template<typename PairXyType>
-struct point_xy_traits {
-    using value_type = typename PairXyType::value_type;
-    static constexpr PairXyType zero() {
-        return PairXyType();
-    }
-
-    static constexpr PairXyType add(const PairXyType& a, const PairXyType& b) {
-        return PairXyType(a.x + b.x, a.y + b.y);
-    }
-
-    static constexpr PairXyType sub(const PairXyType& a, const PairXyType& b) {
-        return PairXyType(a.x - b.x, a.y - b.y);
-    }
-
-    static constexpr PairXyType mul(const PairXyType& a, const PairXyType& b) {
-        return PairXyType(a.x * b.x, a.y * b.y);
-    }
-
-    static constexpr PairXyType div(const PairXyType& a, const PairXyType& b) {
-        return PairXyType(a.x / b.x, a.y / b.y);
-    }
-
-    template<typename NumberT>
-    static constexpr PairXyType add(const PairXyType& a, const NumberT& b) {
-        return PairXyType(a.x + b, a.y + b);
-    }
-
-    template<typename NumberT>
-    static constexpr PairXyType sub(const PairXyType& a, const NumberT& b) {
-        return PairXyType(a.x - b, a.y - b);
-    }
-
-    template<typename NumberT>
-    static constexpr PairXyType mul(const PairXyType& a, const NumberT& b) {
-        return PairXyType(a.x * b, a.y * b);
-    }
-
-    template<typename NumberT>
-    static constexpr PairXyType div(const PairXyType& a, const NumberT& b) {
-        return PairXyType(a.x / b, a.y / b);
-    }
-};
+template<typename T> struct point_xy_math;
 
 template<typename T>
 struct point_xy {
@@ -60,40 +18,40 @@ struct point_xy {
         return *this;
     }
     point_xy& operator/=(const float& f) {
-        *this = point_xy_traits<point_xy>::div(*this, f);
+        *this = point_xy_math<point_xy>::div(*this, f);
         return *this;
     }
     point_xy& operator*=(const double& f) {
-        *this = point_xy_traits<point_xy>::mul(*this, f);
+        *this = point_xy_math<point_xy>::mul(*this, f);
         return *this;
     }
     point_xy& operator/=(const double& f) {
-        *this = point_xy_traits<point_xy>::div(*this, f);
+        *this = point_xy_math<point_xy>::div(*this, f);
         return *this;
     }
 
     point_xy& operator/=(const uint16_t& d) {
-        *this = point_xy_traits<point_xy>::div(*this, d);
+        *this = point_xy_math<point_xy>::div(*this, d);
         return *this;
     }
 
     point_xy& operator/=(const int& d) {
-        *this = point_xy_traits<point_xy>::div(*this, d);
+        *this = point_xy_math<point_xy>::div(*this, d);
         return *this;
     }
 
     point_xy& operator/=(const point_xy& p) {
-        *this = point_xy_traits<point_xy>::div(*this, p);
+        *this = point_xy_math<point_xy>::div(*this, p);
         return *this;
     }
 
     point_xy& operator+=(const point_xy& p) {
-        *this = point_xy_traits<point_xy>::add(*this, p);
+        *this = point_xy_math<point_xy>::add(*this, p);
         return *this;
     }
 
     point_xy& operator-=(const point_xy& p) {
-        *this = point_xy_traits<point_xy>::sub(*this, p);
+        *this = point_xy_math<point_xy>::sub(*this, p);
         return *this;
     }
 
@@ -104,39 +62,39 @@ struct point_xy {
     }
 
     point_xy operator-(const point_xy& p) const {
-        return point_xy_traits<point_xy>::sub(*this, p);
+        return point_xy_math<point_xy>::sub(*this, p);
     }
 
     point_xy operator+(const point_xy& p) const {
-        return point_xy_traits<point_xy>::add(*this, p);
+        return point_xy_math<point_xy>::add(*this, p);
     }
 
     point_xy operator*(const point_xy& p) const {
-        return point_xy_traits<point_xy>::mul(*this, p);
+        return point_xy_math<point_xy>::mul(*this, p);
     }
 
     point_xy operator/(const point_xy& p) const {
-        return point_xy_traits<point_xy>::div(*this, p);
+        return point_xy_math<point_xy>::div(*this, p);
     }
 
     template<typename NumberT>
     point_xy operator+(const NumberT& p) const {
-        return point_xy_traits<point_xy>::add(*this, p);
+        return point_xy_math<point_xy>::add(*this, p);
     }
 
     template<typename NumberT>
     point_xy operator-(const NumberT& p) const {
-        return point_xy_traits<point_xy>::sub(*this, p);
+        return point_xy_math<point_xy>::sub(*this, p);
     }
 
     template<typename NumberT>
     point_xy operator*(const NumberT& p) const {
-        return point_xy_traits<point_xy>::mul(*this, p);
+        return point_xy_math<point_xy>::mul(*this, p);
     }
 
     template<typename NumberT>
     point_xy operator/(const NumberT& p) const {
-        return point_xy_traits<point_xy>::div(*this, p);
+        return point_xy_math<point_xy>::div(*this, p);
     }
 
     bool operator==(const point_xy& p) const {
@@ -149,6 +107,9 @@ struct point_xy {
 };
 
 using point_xy_float = point_xy<float>;  // It's just easier if we allow negative values.
+
+// Legacy support
+
 using pair_xy_float = point_xy<float>;  // Legacy name for point_xy_float
 
 // pair_xy<T> is the legacy name for point_xy<T>
@@ -160,5 +121,50 @@ struct pair_xy : public point_xy<T> {
     pair_xy(const point_xy<T>& p) : point_xy<T>(p) {}
 };
 
+////////////// point_xy operations //////////////
+
+template<typename T>
+struct point_xy_math {
+    using value_type = typename T::value_type;
+    static constexpr T zero() {
+        return T();
+    }
+
+    static constexpr T add(const T& a, const T& b) {
+        return T(a.x + b.x, a.y + b.y);
+    }
+
+    static constexpr T sub(const T& a, const T& b) {
+        return T(a.x - b.x, a.y - b.y);
+    }
+
+    static constexpr T mul(const T& a, const T& b) {
+        return T(a.x * b.x, a.y * b.y);
+    }
+
+    static constexpr T div(const T& a, const T& b) {
+        return T(a.x / b.x, a.y / b.y);
+    }
+
+    template<typename NumberT>
+    static constexpr T add(const T& a, const NumberT& b) {
+        return T(a.x + b, a.y + b);
+    }
+
+    template<typename NumberT>
+    static constexpr T sub(const T& a, const NumberT& b) {
+        return T(a.x - b, a.y - b);
+    }
+
+    template<typename NumberT>
+    static constexpr T mul(const T& a, const NumberT& b) {
+        return T(a.x * b, a.y * b);
+    }
+
+    template<typename NumberT>
+    static constexpr T div(const T& a, const NumberT& b) {
+        return T(a.x / b, a.y / b);
+    }
+};
 
 }  // namespace fl
