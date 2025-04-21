07498feaee0b7b2a059cb38815d137106590027e
zackees <z@zackees.com>
2025-04-20 18:18:21 -0700

    Remove deprecated header files

--- src/str.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#warning "This header is deprecated. Please use str.h, this header will go away in the version 4.0."
-
-#include "fl/str.h"
--- src/xmap.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#warning "This header is deprecated. Please use xmap.h, this header will go away in the version 4.0."
-
-#include "fl/xmap.h"
