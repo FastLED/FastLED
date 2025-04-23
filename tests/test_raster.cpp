23ed9b0ec2cc0368baeb55a0daca01e19c0d3064
zackees <z@zackees.com>
2025-04-23 00:16:48 -0700

    Add XYRasterDense test cases and assertions

--- tests/test_raster.cpp
+++ tests/test_raster.cpp
@@ -12,28 +12,28 @@
 
 using namespace fl;
 
-// TEST_CASE("XYRasterDense simple test") {
-//     XYPathPtr path = XYPath::NewPointPath(0, 0);
-//     path->setDrawBounds(2, 2);
-//     SubPixel2x2 subpixel = path->at_subpixel(0);
-//     XYRasterDense raster(3, 3);
-//     SubPixel2x2::Rasterize(Slice<SubPixel2x2>(&subpixel, 1), &raster);
+TEST_CASE("XYRasterDense simple test") {
+    XYPathPtr path = XYPath::NewPointPath(0, 0);
+    path->setDrawBounds(2, 2);
+    SubPixel2x2 subpixel = path->at_subpixel(0);
+    XYRasterDense raster(3, 3);
+    SubPixel2x2::Rasterize(Slice<SubPixel2x2>(&subpixel, 1), &raster);
 
-//     point_xy<int> origin = subpixel.origin();
-//     point_xy<int> global_min = raster.global_min();
-//     point_xy<int> global_max = raster.global_max();
-//     REQUIRE_EQ(global_min, origin);
-//     REQUIRE_EQ(global_max, origin + point_xy<int>(2, 2));
+    point_xy<int> origin = subpixel.origin();
+    point_xy<int> global_min = raster.global_min();
+    point_xy<int> global_max = raster.global_max();
+    REQUIRE_EQ(global_min, origin);
+    REQUIRE_EQ(global_max, origin + point_xy<int>(2, 2));
 
-//     // now test that each point matches.
-//     for (int x = 0; x < 2; ++x) {
-//         for (int y = 0; y < 2; ++y) {
-//             uint8_t v1 = subpixel.at(x, y);
-//             uint8_t v2 = raster.at(x, y);
-//             REQUIRE_EQ(v1, v2);
-//         }
-//     }
-// }
+    // now test that each point matches.
+    for (int x = 0; x < 2; ++x) {
+        for (int y = 0; y < 2; ++y) {
+            uint8_t v1 = subpixel.at(x, y);
+            uint8_t v2 = raster.at(x, y);
+            REQUIRE_EQ(v1, v2);
+        }
+    }
+}
 
 TEST_CASE("XYRasterDense two unit test") {
     XYPathPtr path = XYPath::NewLinePath(-1, -1, 1, 1);
@@ -56,17 +56,17 @@ TEST_CASE("XYRasterDense two unit test") {
 
 
 
-// TEST_CASE("XYRasterDense with empty draw bounds should prevent any elements") {
-//     XYPathPtr path = XYPath::NewLinePath(-1, -1, 1, 1);
-//     path->setDrawBounds(-4,4);
-//     SubPixel2x2 sp0 = path->at_subpixel(0);
-//     SubPixel2x2 sp1 = path->at_subpixel(1);
-//     SubPixel2x2 subpixels[2] = {sp0, sp1};
-//     XYRasterDense raster(4, 4);
-//     auto bstart = raster.bounds();
-//     REQUIRE_EQ(rect_xy<uint16_t>(0, 0, 0, 0), bstart);
-//     rect_xy<int> empty;
-//     SubPixel2x2::Rasterize(subpixels, &raster, &empty);
-//     auto b = raster.bounds();
-//     REQUIRE_EQ(rect_xy<uint16_t>(0, 0, 0, 0), b);
-// }
+TEST_CASE("XYRasterDense with empty draw bounds should prevent any elements") {
+    XYPathPtr path = XYPath::NewLinePath(-1, -1, 1, 1);
+    path->setDrawBounds(-4,4);
+    SubPixel2x2 sp0 = path->at_subpixel(0);
+    SubPixel2x2 sp1 = path->at_subpixel(1);
+    SubPixel2x2 subpixels[2] = {sp0, sp1};
+    XYRasterDense raster(4, 4);
+    auto bstart = raster.bounds();
+    REQUIRE_EQ(rect_xy<uint16_t>(0, 0, 0, 0), bstart);
+    rect_xy<int> empty;
+    SubPixel2x2::Rasterize(subpixels, &raster, &empty);
+    auto b = raster.bounds();
+    REQUIRE_EQ(rect_xy<uint16_t>(0, 0, 0, 0), b);
+}
