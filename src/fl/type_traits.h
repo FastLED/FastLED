765cc42281e50ea1d1c5fba69fdace5e62c5965b
zackees <z@zackees.com>
2025-04-25 14:08:09 -0700

    Add support for __MKL26Z64__ microcontroller

--- src/fl/type_traits.h
+++ src/fl/type_traits.h
@@ -84,6 +84,8 @@ enum {
 #define FASTLED_STR_NEEDS_INT 1
 #elif defined(__MK20DX128__)
 #define FASTLED_STR_NEEDS_INT 1
+#elif defined(__MKL26Z64__)
+#define FASTLED_STR_NEEDS_INT 1
 
 #elif defined(ESP32)
 // #include "platforms/esp/esp_version.h"
