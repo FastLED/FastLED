537462eeec52100db161c1c93596cdf1a3cdeb1c
zackees <z@zackees.com>
2025-04-27 13:32:10 -0700

    Update bind.h to return jobid from onChanged functions

--- src/fl/bind.h
+++ src/fl/bind.h
@@ -9,17 +9,17 @@ namespace fl {
 // bind will bind the values which are updated once per frame.
 // Binds a uislider to number value, which is auto updated.
 template <typename T> int bind(fl::UISlider &slider, T *t) {
-    slider.onChanged([t](float value) { *t = value; });
-    return 0;
+    int jobid = slider.onChanged([t](float value) { *t = value; });
+    return jobid;
 }
 
 // Binds a UIButton to an integer-like value.
 template <typename T> int bind(fl::UIButton &button, T *t) {
-    button.onChanged([&button, t]() {
+    int jobid = button.onChanged([&button, t]() {
         bool clicked = button.clicked();
         *t = clicked;
     });
 
-    return 0;
+    return jobid;
 }
 } // namespace fl
