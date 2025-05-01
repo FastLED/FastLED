617f5cd2a7615ff36d0193a6411816f6279e2004
zackees <z@zackees.com>
2025-05-01 12:31:20 -0700

    Add proof of concept FFT implementation using KISS FFT

--- src/fl/fft.h
+++ src/fl/fft.h
@@ -4,16 +4,12 @@
 
 namespace fl {
 
-
-
+// Proof of concept FFT using KISS FFT. Right now this is fixed sized blocks of 512. But this is
+// intended to change with a C++ wrapper around ot/
 typedef int16_t fft_audio_buffer_t[512];
-
 
 void fft_init(); // Explicit initialization of FFT, otherwise it will be initialized on first run.
 bool fft_is_initialized();
 void fft_unit_test(const fft_audio_buffer_t &buffer);
-
-
-
 
 };
