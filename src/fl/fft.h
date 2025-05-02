70856cd5d790066cbc3936156d26db10c52d28cc
zackees <z@zackees.com>
2025-05-01 18:14:40 -0700

    Add FFT class with default values and run method

--- src/fl/fft.h
+++ src/fl/fft.h
@@ -11,43 +11,47 @@ namespace fl {
 class AudioSample;
 class FFTContext;
 
+// Example:
+//   FFT fft(512, 16);
+//   auto sample = SINE WAVE OF 512 SAMPLES
+//   fft.run(buffer, &out);
+//   FASTLED_WARN("FFT output: " << out);  // 16 bands of output.
 class FFT {
   public:
-
+    // Output bins for FFT. This is the output when the fft is run.
     using OutputBins = fl::vector_inlined<float, 16>;
-
+    // Result indicating success or failure of the FFT run (in which case there
+    // will be an error message).
     struct Result {
       Result(bool ok, const Str& error) : ok(ok), error(error) {}
       bool ok = false;
       fl::Str error;
     };
+    // Default values for the FFT.
+    FFT(int samples = DefaultSamples(), int bands = DefaultBands(),
+        float fmin = DefaultMinFrequency(), float fmax = DefaultMaxFrequency(),
+        int sample_rate = DefaultSampleRate());
+    ~FFT();
 
+    size_t sampleSize() const;
+    // Note that the sample sizes MUST match the samples size passed into the constructor.
+    Result run(const AudioSample &sample, OutputBins *out);
+    Result run(Slice<const int16_t> sample, OutputBins *out);
+    // Info on what the frequency the bins represent
+    fl::Str info() const;
+
+    // Detail.
     static int DefaultSamples() { return 512; }
     static int DefaultBands() { return 16; }
     static float DefaultMinFrequency() { return 174.6f; }
     static float DefaultMaxFrequency() { return 4698.3f; }
     static int DefaultSampleRate() { return 44100; }
 
-    // FFT(int samples, int bands, float fmin, float fmax, int sample_rate);
-    FFT(int samples = DefaultSamples(), int bands = DefaultBands(),
-        float fmin = DefaultMinFrequency(), float fmax = DefaultMaxFrequency(),
-        int sample_rate = DefaultSampleRate());
-    ~FFT();
-
+    // Disable copy and move constructors and assignment operators
     FFT(const FFT &) = delete;
     FFT &operator=(const FFT &) = delete;
     FFT(FFT &&) = delete;
     FFT &operator=(FFT &&) = delete;
-    size_t sampleSize() const;
-
-    // void fft_unit_test(const fft_audio_buffer_t &buffer, fft_output_fixed
-    // *out);
-
-    Result run(const AudioSample &sample, OutputBins *out);
-    Result run(Slice<const int16_t> sample, OutputBins *out);
-
-    // Info on what the frequency the bins represent
-    fl::Str info() const;
 
   private:
     fl::scoped_ptr<FFTContext> mContext;
