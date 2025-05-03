b3a2dd9eeaf4d8a2448a0af852f664d029df2340
Zachary Vorhies <zachvorhies@protonmail.com>
2025-05-03 15:14:13 -0700

    Remove unnecessary header includes and definitions

--- src/fl/fft.cpp
+++ src/fl/fft.cpp
@@ -1,36 +1,5 @@
-/// #include <Arduino.h>
-// #include <iostream>
-// #include "audio_types.h"
-// // #include "defs.h"
-// #include "thirdparty/cq_kernel/cq_kernel.h"
-// #include "thirdparty/cq_kernel/kiss_fftr.h"
-// #include "util.h"
 
-#define FASTLED_INTERNAL 1
-
-#include "FastLED.h"
-
-#include "third_party/cq_kernel/cq_kernel.h"
-#include "third_party/cq_kernel/kiss_fftr.h"
-
-#include "fl/array.h"
-#include "fl/audio.h"
 #include "fl/fft.h"
-#include "fl/str.h"
-#include "fl/unused.h"
-#include "fl/vector.h"
-#include "fl/warn.h"
-
-// #define SAMPLES IS2_AUDIO_BUFFER_LEN
-#define AUDIO_SAMPLE_RATE 44100
-#define SAMPLES 512
-#define BANDS 16
-#define SAMPLING_FREQUENCY AUDIO_SAMPLE_RATE
-#define MAX_FREQUENCY 4698.3
-#define MIN_FREQUENCY 174.6
-#define MIN_VAL 5000 // Equivalent to 0.15 in Q15
-
-#define PRINT_HEADER 1
 
 namespace fl {
 
