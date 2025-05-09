e6671c6d27c5666f9cfe14734c84b377d402a4a5
zackees <z@zackees.com>
2025-05-09 12:20:27 -0700

    Update alpha function parameter type in rasterize method

--- src/fl/xypath.cpp
+++ src/fl/xypath.cpp
@@ -46,7 +46,7 @@ Tile2x2_u8 XYPath::at_subpixel(float alpha) {
 
 void XYPath::rasterize(float from, float to, int steps,
                        XYRasterU8Sparse &raster,
-                       function<uint8_t(float)> *optional_alpha_gen) {
+                       XYPath::AlphaFunction *optional_alpha_gen) {
     mPathRenderer->rasterize(from, to, steps, raster, optional_alpha_gen);
 }
 
