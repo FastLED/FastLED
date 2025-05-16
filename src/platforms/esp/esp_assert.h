11e03d5684af51f3acb6801f5f85be0e28ccbaba
zackees <z@zackees.com>
2025-05-16 14:58:07 -0700

    Remove unnecessary ESP_ERROR_CHECK function call

--- src/platforms/esp/esp_assert.h
+++ src/platforms/esp/esp_assert.h
@@ -1,12 +1,10 @@
 #pragma once
 
-#include "esp_check.h"
 #include "esp_log.h"
 
 #define FASTLED_ASSERT(x, MSG)                                                 \
     {                                                                          \
         if (!(x)) {                                                            \
             ESP_LOGE("#### FastLED", "%s", (fl::StrStream() << MSG).c_str());  \
-            ESP_ERROR_CHECK(ESP_FAIL);                                         \
         }                                                                      \
     }
