637e4a406aad4123809b1466920dd0782a22c3e6
zackees <z@zackees.com>
2025-05-23 14:16:16 -0700

    update funky clouds

--- examples/FunkyClouds/FunkyClouds.ino
+++ examples/FunkyClouds/FunkyClouds.ino
@@ -1,6 +1,6 @@
 /*
 TODO:
- 
+
  example show oscis+p
  document caleidoscopes better
  write better caleidoscopes...
@@ -16,11 +16,11 @@ TODO:
  1D examples
  2d example with more than 2 sines
  speed up MSGEQ7 readings
- 
- 
+
+
  DONE:
- 25.6.      creating basic structure 
- setting up basic examples 
+ 25.6.      creating basic structure
+ setting up basic examples
  26.6.      MSGEQ7 Support
  wrote more examples
  27.6.      improved documentation
@@ -33,28 +33,29 @@ TODO:
  30.6.      RenderCustomMatrix
  added more comments
  alpha version released
- 
- 
+
+
 /*
- 
+
 /*
  Funky Clouds Compendium (alpha version)
  by Stefan Petrick
- 
- An ever growing list of examples, tools and toys 
+
+ An ever growing list of examples, tools and toys
  for creating one- and twodimensional LED effects.
- 
+
  Dedicated to the users of the FastLED v2.1 library
- by Daniel Garcia and Mark Kriegsmann. 
- 
+ by Daniel Garcia and Mark Kriegsmann.
+
  Provides basic and advanced helper functions.
  Contains many examples how to creatively combine them.
- 
+
  Tested @ATmega2560 (runs propably NOT on an Uno or
  anything else with less than 4kB RAM)
  */
 
 #include "FastLED.h"
+#include "defs.h"
 #include "funky.h"
 
 /*
@@ -63,36 +64,28 @@ TODO:
  -------------------------------------------------------------------
  */
 void setup() {
-  // use the following line only when working with a 16*16
-  // and delete everything in the function RenderCustomMatrix() 
-  // at the end of the code; edit XY() to change your matrix layout
-  // right now it is doing a serpentine mapping
-  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds+5, NUM_LEDS-5);
+    // use the following line only when working with a 16*16
+    // and delete everything in the function RenderCustomMatrix()
+    // at the end of the code; edit XY() to change your matrix layout
+    // right now it is doing a serpentine mapping
 
-  // use this line only when using a custom size matrix
-  //FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds2, CUSTOM_HEIGHT * CUSTOM_WIDTH);
-
-  FastLED.setBrightness(BRIGHTNESS);
-  FastLED.setDither(0);
-  // just for debugging:
-  //Serial.begin(9600);
-  Serial.begin(38400);
-  InitFunky();
+    // just for debugging:
+    // Serial.begin(9600);
+    Serial.begin(38400);
+    InitFunky();
 }
-
 
 /*
 -------------------------------------------------------------------
  The main program
  -------------------------------------------------------------------
  */
-void loop()
-{
-  AutoRun();
-  // Comment AutoRun out and test examples seperately here
+void loop() {
+    AutoRun();
+    // Comment AutoRun out and test examples seperately here
 
-  //Dots2();
+    // Dots2();
 
-  // For discovering parameters of examples I reccomend to
-  // tinker with a renamed copy ...
+    // For discovering parameters of examples I reccomend to
+    // tinker with a renamed copy ...
 }
