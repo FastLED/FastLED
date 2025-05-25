d033b601f72933872495ac38d130da9691186f57
zackees <z@zackees.com>
2025-05-24 19:03:45 -0700

    Add WasmSpiOutput class for Emscripten platform

--- src/platforms/wasm/fastspi_wasm.h
+++ src/platforms/wasm/fastspi_wasm.h
@@ -28,56 +28,10 @@ extern uint8_t get_brightness();
 
 class WasmSpiOutput : public fl::EngineEvents::Listener {
   public:
-    WasmSpiOutput() { fl::EngineEvents::addListener(this); }
-
-    ~WasmSpiOutput() { fl::EngineEvents::removeListener(this); }
-
-    CLEDController *tryFindOwner() {
-        if (mId == -1) {
-            mId = StripIdMap::getOrFindByAddress(
-                reinterpret_cast<uint32_t>(this));
-        }
-        if (mId == -1) {
-            return nullptr;
-        }
-        return StripIdMap::getOwner(mId);
-    }
-
-    void onEndShowLeds() override {
-        // Get the led data and send it to the JavaScript side. This is tricky
-        // because we have to find the owner of this pointer, which will be
-        // inlined in a CLEDController subclass. Therefore we are going to do
-        // address lookup to get the CLEDController for all CLEDController
-        // instances that exist and select the one in which this SpiOutput class
-        // would be inlined into.
-        CLEDController *owner = tryFindOwner();
-        if (owner == nullptr) {
-            return;
-        }
-        if (mId == -1) {
-            int new_id = StripIdMap::getId(owner);
-            if (new_id != -1) {
-                mId = new_id;
-            }
-        }
-        ColorAdjustment color_adjustment =
-            owner->getAdjustmentData(get_brightness());
-        PixelController<RGB> pixels(owner->leds(), owner->size(),
-                                    color_adjustment, DISABLE_DITHER);
-        pixels.disableColorAdjustment();
-        mRgb.clear();
-        while (pixels.has(1)) {
-            uint8_t r, g, b;
-            pixels.loadAndScaleRGB(&r, &g, &b);
-            mRgb.push_back(r);
-            mRgb.push_back(g);
-            mRgb.push_back(b);
-            pixels.advanceData();
-        }
-        ActiveStripData &active_strips =
-            fl::Singleton<ActiveStripData>::instance();
-        active_strips.update(mId, millis(), mRgb.data(), mRgb.size());
-    }
+    WasmSpiOutput() ;
+    ~WasmSpiOutput() ;
+    CLEDController *tryFindOwner();
+    void onEndShowLeds() override;
 
     void select() {}
     void init() {}
