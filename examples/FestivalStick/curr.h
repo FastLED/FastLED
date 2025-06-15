a463b7c320e77bb371145e512dfc6afb209ca3f5
zackees <z@zackees.com>
2025-06-15 14:49:51 -0700

    Refactor cork screw position calculation to use exact interpolation

--- examples/FestivalStick/curr.h
+++ examples/FestivalStick/curr.h
@@ -134,7 +134,7 @@ void loop() {
         }
     } else {
         // None splat rendering, looks aweful.
-        vec2f pos_vec2f = corkscrew.at_interp(pos);
+        vec2f pos_vec2f = corkscrew.at_exact(pos);
         vec2i16 pos_i16 = vec2i16(round(pos_vec2f.x), round(pos_vec2f.y));
         // Now map the cork screw position to the cylindrical buffer that we
         // will draw.
