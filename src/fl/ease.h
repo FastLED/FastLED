3f71e12ceccd165b89888c57d3da882b0902ef43
zackees <z@zackees.com>
2025-06-19 15:38:27 -0700

    Add inline functions for easing calculations

--- src/fl/ease.h
+++ src/fl/ease.h
@@ -4,8 +4,14 @@
 
 namespace fl {
 
+enum EaseType {
+    EASE_IN_QUAD,
+    EASE_IN_OUT_QUAD,
+    EASE_IN_OUT_CUBIC,
+};
+
 // 8-bit easing functions
-/// 8-bit quadratic ease-in function  
+/// 8-bit quadratic ease-in function
 /// Takes an input value 0-255 and returns an eased value 0-255
 /// The curve starts slow and accelerates (ease-in only)
 uint8_t easeInQuad8(uint8_t i);
@@ -15,7 +21,7 @@ uint8_t easeInQuad8(uint8_t i);
 /// The curve starts slow, accelerates in the middle, then slows down again
 uint8_t easeInOutQuad8(uint8_t i);
 
-/// 8-bit cubic ease-in/ease-out function  
+/// 8-bit cubic ease-in/ease-out function
 /// Takes an input value 0-255 and returns an eased value 0-255
 /// More pronounced easing curve than quadratic
 uint8_t easeInOutCubic8(uint8_t i);
@@ -27,7 +33,7 @@ uint8_t easeInOutApprox8(uint8_t i);
 
 // 16-bit easing functions
 /// 16-bit quadratic ease-in function
-/// Takes an input value 0-65535 and returns an eased value 0-65535  
+/// Takes an input value 0-65535 and returns an eased value 0-65535
 uint16_t easeInQuad16(uint16_t i);
 
 /// 16-bit quadratic ease-in/ease-out function
@@ -38,4 +44,20 @@ uint16_t easeInOutQuad16(uint16_t i);
 /// Takes an input value 0-65535 and returns an eased value 0-65535
 uint16_t easeInOutCubic16(uint16_t i);
 
+inline uint16_t ease16(EaseType type, uint16_t i) {
+    switch (type) {
+        case EASE_IN_QUAD: return easeInQuad16(i);
+        case EASE_IN_OUT_QUAD: return easeInOutQuad16(i);
+        case EASE_IN_OUT_CUBIC: return easeInOutCubic16(i);
+    }
 }
+
+inline uint8_t ease8(EaseType type, uint8_t i) {
+    switch (type) {
+        case EASE_IN_QUAD: return easeInQuad8(i);
+        case EASE_IN_OUT_QUAD: return easeInOutQuad8(i);
+        case EASE_IN_OUT_CUBIC: return easeInOutCubic8(i);
+    }
+}
+
+} // namespace fl
