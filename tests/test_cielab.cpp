d703f2b1cfe6562599c637e86418f9d5c78da56c
zackees <z@zackees.com>
2025-06-18 20:21:59 -0700

    Add test case for CIELAB16 basic operations

---
+++ tests/test_cielab.cpp
@@ -0,0 +1,19 @@
+// Compile with: g++ --std=c++11 test.cpp
+
+#include "test.h"
+
+#include "fl/cielab.h"
+
+using namespace fl;
+
+TEST_CASE("CIELAB16 basic operations") {
+
+    SUBCASE("Conversion from CRGB to CIELAB16") {
+        CRGB c(255, 255, 255);
+        CIELAB16 lab(c);
+        CRGB c2 = lab.ToRGB();
+        CHECK(c2.r == c.r);
+        CHECK(c2.g == c.g);
+        CHECK(c2.b == c.b);
+    }
+}
