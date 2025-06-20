2683dbadd52e558f710f3a0f448bd361fa6d0f14
zackees <z@zackees.com>
2025-06-20 15:38:49 -0700

    Add support for initializer_list on AVR platforms

--- src/fl/initializer_list.h
+++ src/fl/initializer_list.h
@@ -5,14 +5,7 @@
 
 // Define if initializer_list is available
 // Check for C++11 and if std::initializer_list exists
-#if __cplusplus >= 201103L && !defined(__AVR__) && !defined(FASTLED_NO_STD_INITIALIZER_LIST)
-// Use standard library version on most platforms
-#include <initializer_list>
-#define FASTLED_HAS_INITIALIZER_LIST 1
-namespace fl {
-    using std::initializer_list;
-}
-#elif (__cplusplus >= 201103L && defined(__AVR__)) || (defined(__AVR__) && !defined(FASTLED_NO_INITIALIZER_LIST))
+#if defined(__AVR__)
 // Emulated initializer_list for AVR platforms
 #define FASTLED_HAS_INITIALIZER_LIST 1
 namespace fl {
@@ -61,6 +54,9 @@ namespace fl {
     }
 }
 #else
-// No initializer list support - containers should not try to emulate the features
-#define FASTLED_HAS_INITIALIZER_LIST 0
+#include <initializer_list>
+#define FASTLED_HAS_INITIALIZER_LIST 1
+namespace fl {
+    using std::initializer_list;
+}
 #endif 
