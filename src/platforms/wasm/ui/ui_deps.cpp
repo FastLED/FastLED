95a5234e0d7bafa66e609d3e1ccd9cebc29db6a8
zackees <z@zackees.com>
2025-06-20 22:01:09 -0700

    Add free function replacements for UI component management

---
+++ src/platforms/wasm/ui/ui_deps.cpp
@@ -0,0 +1,20 @@
+#if defined(__EMSCRIPTEN__) || defined(FASTLED_TESTING)
+
+#include "ui_deps.h"
+#include "ui_internal.h"
+#include "ui_manager.h"
+
+namespace fl {
+
+
+void addUiComponent(fl::WeakPtr<jsUiInternal> component) {
+    jsUiManager::addComponent(component);
+}
+
+void removeUiComponent(fl::WeakPtr<jsUiInternal> component) {
+    jsUiManager::removeComponent(component);
+}
+
+} // namespace fl
+
+#endif // __EMSCRIPTEN__
