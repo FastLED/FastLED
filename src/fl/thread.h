4d03474c669ced1bfd59c374e792a29aa4fe9736
zackees <z@zackees.com>
2025-06-20 18:05:31 -0700

    Update thread.h: set FASTLED_USE_THREAD_LOCAL to FASTLED_MULTITHREADED

--- src/fl/thread.h
+++ src/fl/thread.h
@@ -1,19 +1,13 @@
 #pragma once
 
 #ifndef FASTLED_MULTITHREADED
+#if defined(__EMSCRIPTEN__)
+#define FASTLED_MULTITHREADED 1
+#else
 #define FASTLED_MULTITHREADED 0
 #endif
+#endif  // FASTLED_MULTITHREADED
 
 #ifndef FASTLED_USE_THREAD_LOCAL
-#define FASTLED_USE_THREAD_LOCAL 0
-#endif
-
-#if FASTLED_MULTITHREADED and !defined(FASTLED_MULTITHREAD_SUPPRESS_WARNING)
-#warning                                                                       \
-    "Warning: FastLED has never been used in a multi threading environment and may not work."
-#endif
-
-#if FASTLED_USE_THREAD_LOCAL and !defined(FASTLED_THREAD_LOCAL_SUPPRESS_WARNING)
-#warning                                                                       \
-    "Warning: Thread-local storage is enabled. This is experimental functionality."
-#endif
+#define FASTLED_USE_THREAD_LOCAL FASTLED_MULTITHREADED
+#endif  // FASTLED_USE_THREAD_LOCAL
