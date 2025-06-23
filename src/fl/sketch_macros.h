f332482ae6b5bf76fb9467ff2def57cb8b9b6ae2
zackees <z@zackees.com>
2025-06-23 15:36:11 -0700

    Add support for ESP8266 in sketch memory check

--- src/fl/sketch_macros.h
+++ src/fl/sketch_macros.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#if defined(__AVR__) ||defined(ARDUINO_TEENSYLC) || defined(ARDUINO_TEENSY30) || defined(STM32F1) || defined(ARDUINO_ARCH_RENESAS_UNO)
+#if defined(__AVR__) ||defined(ARDUINO_TEENSYLC) || defined(ARDUINO_TEENSY30) || defined(STM32F1) || defined(ARDUINO_ARCH_RENESAS_UNO) || defined(ESP8266)
 #define SKETCH_HAS_LOTS_OF_MEMORY 0
 #else
 #define SKETCH_HAS_LOTS_OF_MEMORY 1
