17fd4f03b0411b7892728d208b18e013f2802fc2
zackees <z@zackees.com>
2025-06-23 19:46:08 -0700

    Refactor manual libunwind search and setup

--- tests/crash_handler.h
+++ tests/crash_handler.h
@@ -4,11 +4,26 @@
 #include <csignal>
 #include <cstdio>
 #include <cstdlib>
-#include <execinfo.h>
 #include <unistd.h>
-#include <libunwind.h>
 
-void print_stacktrace() {
+
+#ifdef USE_LIBUNWIND
+#include <libunwind.h>
+#elif __has_include(<execinfo.h>)
+#define USE_EXECINFO 1
+#include <execinfo.h>
+#else
+// Disable libunwind as a fallback.
+#undef USE_LIBUNWIND
+#endif
+
+void crash_handler(int sig);
+void print_stacktrace();
+void setup_crash_handler();
+
+inline void print_stacktrace() {
+#ifdef USE_LIBUNWIND
+    // Use libunwind for better stack traces
     unw_cursor_t cursor;
     unw_context_t context;
 
@@ -16,6 +31,7 @@ void print_stacktrace() {
     unw_init_local(&cursor, &context);
 
     int depth = 0;
+    printf("Stack trace (libunwind):\n");
     while (unw_step(&cursor) > 0) {
         unw_word_t offset, pc;
         char sym[256];
@@ -33,17 +49,44 @@ void print_stacktrace() {
             printf(" -- symbol not found\n");
         }
     }
+#elif USE_EXECINFO
+    // Fallback to execinfo.h backtrace
+    void *buffer[100];
+    int nptrs;
+    char **strings;
+
+    printf("Stack trace (backtrace):\n");
+    nptrs = backtrace(buffer, 100);
+    strings = backtrace_symbols(buffer, nptrs);
+    
+    if (strings == nullptr) {
+        printf("backtrace_symbols failed\n");
+        return;
+    }
+
+    for (int j = 0; j < nptrs; j++) {
+        printf("#%-2d %s\n", j, strings[j]);
+    }
+
+    free(strings);
+#endif
 }
 
-void crash_handler(int sig) {
+inline void crash_handler(int sig) {
     fprintf(stderr, "Error: signal %d:\n", sig);
     print_stacktrace();
     exit(1);
 }
 
-void setup_crash_handler() {
-    signal(SIGSEGV, crash_handler);
-    signal(SIGABRT, crash_handler);
+inline void setup_crash_handler() {
+
+#ifdef USE_EXECINFO
+    // Install execinfo.h backtrace
+    backtrace_init();
+#elif USE_LIBUNWIND
+    // Install libunwind backtrace
+    unw_backtrace_init();
+#endif
 }
 
 #endif // CRASH_HANDLER_H
