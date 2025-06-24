6d056399cb3494dbbecd64eebed0c41641181785
zackees <z@zackees.com>
2025-06-23 20:25:36 -0700

    Add platform-specific test headers for ESP8266, ARM, Apollo3, and WASM

--- examples/CompileTest/esp_8266_test.h
+++ examples/CompileTest/esp_8266_test.h
@@ -3,11 +3,33 @@
 #include "FastLED.h"
 
 void esp_8266_tests() {
-#if FASTLED_USE_PROGMEM != 1
-#error "FASTLED_USE_PROGMEM is not 1 for AVR"
+#if FASTLED_USE_PROGMEM != 0
+#error "FASTLED_USE_PROGMEM should be 0 for ESP8266"
 #endif
 
 #if SKETCH_HAS_LOTS_OF_MEMORY != 0
-#error "SKETCH_HAS_LOTS_OF_MEMORY is not 0 for AVR"
+#error "SKETCH_HAS_LOTS_OF_MEMORY should be 0 for ESP8266"
+#endif
+
+#if FASTLED_ALLOW_INTERRUPTS != 1
+#error "FASTLED_ALLOW_INTERRUPTS should be 1 for ESP8266"
+#endif
+
+#ifndef ESP8266
+#error "ESP8266 should be defined for ESP8266 platforms"
+#endif
+
+#ifndef F_CPU
+#error "F_CPU should be defined for ESP8266"
+#endif
+
+// ESP8266 should have reasonable F_CPU (typically 80MHz or 160MHz)
+#if F_CPU < 80000000
+#error "ESP8266 F_CPU should be at least 80MHz"
+#endif
+
+// ESP8266 should have millis support
+#ifndef FASTLED_HAS_MILLIS
+#error "FASTLED_HAS_MILLIS should be defined for ESP8266"
 #endif
 }
--- examples/CompileTest/esp_test.h
+++ examples/CompileTest/esp_test.h
@@ -1,10 +1,31 @@
-
 #pragma once
 
 #include "sdkconfig.h"
+#include "esp32_general_test.h"
 
 #if defined(ESP8266)
 #include "esp_8266_test.h"
 #elif defined(CONFIG_IDF_TARGET_ESP32S3)
 #include "esp_s3_test.h"
+#elif defined(CONFIG_IDF_TARGET_ESP32S2)
+// ESP32-S2 uses same tests as general ESP32 for now
+#elif defined(CONFIG_IDF_TARGET_ESP32C3)
+// ESP32-C3 uses same tests as general ESP32 for now
+#elif defined(CONFIG_IDF_TARGET_ESP32C6)
+// ESP32-C6 uses same tests as general ESP32 for now
+#elif defined(CONFIG_IDF_TARGET_ESP32H2)
+// ESP32-H2 uses same tests as general ESP32 for now
+#elif defined(ESP32)
+// Generic ESP32
 #endif
+
+void esp_tests() {
+#if defined(ESP8266)
+    esp_8266_tests();
+#elif defined(ESP32)
+    esp32_general_tests();
+    #if defined(CONFIG_IDF_TARGET_ESP32S3)
+    esp32_s3_tests();
+    #endif
+#endif
+}
