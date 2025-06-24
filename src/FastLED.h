FastLED/FastLED

commit 037cc22037c352961d9389ae1f80df4605453074

Rev 3.10.0 - Animartrix, IS2 Esp32S3 Fixes, ESP32P4 support

src/FastLED.h
@@ -16,13 +16,13 @@
 /// * 1 digit for the major version
 /// * 3 digits for the minor version
 /// * 3 digits for the patch version
-#define FASTLED_VERSION 3009020
+#define FASTLED_VERSION 301000
 #ifndef FASTLED_INTERNAL
 #  ifdef  FASTLED_SHOW_VERSION
 #    ifdef FASTLED_HAS_PRAGMA_MESSAGE
-#      pragma message "FastLED version 3.009.020"
+#      pragma message "FastLED version 3.010.000"
 #    else
-#      warning FastLED version 3.009.020  (Not really a warning, just telling you here.)
+#      warning FastLED version 3.010.000  (Not really a warning, just telling you here.)
 #    endif
 #  endif
 #endif

docs/Doxyfile
@@ -48,7 +48,7 @@ PROJECT_NAME           = FastLED
 # could be handy for archiving the generated documentation or if some version
 # control system is used.
 
-PROJECT_NUMBER = 3.9.20
+PROJECT_NUMBER = 3.10.0
 
 # Using the PROJECT_BRIEF tag one can provide an optional one line description
 # for a project that appears at the top of each page and should give viewers a

library.properties
@@ -1,5 +1,5 @@
 name=FastLED
-version=3.9.20
+version=3.10.0
 author=Daniel Garcia
 maintainer=Daniel Garcia <dgarcia@fastled.io>
 sentence=Multi-platform library for controlling dozens of different types of LEDs along with optimized math, effect, and noise functions.

commit 1974d515fd9bec2a5fdb552472156ea4312e589b

fix: version number format was broken by 037cc22

src/FastLED.h
@@ -16,7 +16,7 @@
 /// * 1 digit for the major version
 /// * 3 digits for the minor version
 /// * 3 digits for the patch version
-#define FASTLED_VERSION 301001
+#define FASTLED_VERSION 3010001
 #ifndef FASTLED_INTERNAL
 #  ifdef  FASTLED_SHOW_VERSION
 #    ifdef FASTLED_HAS_PRAGMA_MESSAGE