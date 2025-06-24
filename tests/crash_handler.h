30888561cb395d4cf889a195a2fc6bcc8e3a3435
zackees <z@zackees.com>
2025-06-23 20:11:40 -0700

    Add Windows debugging libraries for crash handler

--- tests/crash_handler.h
+++ tests/crash_handler.h
@@ -1,92 +1,33 @@
 #ifndef CRASH_HANDLER_H
 #define CRASH_HANDLER_H
 
-#include <csignal>
-#include <cstdio>
-#include <cstdlib>
-#include <unistd.h>
-
-
-#ifdef USE_LIBUNWIND
-#include <libunwind.h>
+// Determine which crash handler implementation to use
+#ifdef _WIN32
+    #include "crash_handler_win.h"
+    namespace impl = crash_handler_win;
+#elif defined(USE_LIBUNWIND)
+    #include "crash_handler_libunwind.h"
+    namespace impl = crash_handler_libunwind;
 #elif __has_include(<execinfo.h>)
-#define USE_EXECINFO 1
-#include <execinfo.h>
+    #define USE_EXECINFO 1
+    #include "crash_handler_execinfo.h" 
+    namespace impl = crash_handler_execinfo;
 #else
-// Disable libunwind as a fallback.
-#undef USE_LIBUNWIND
+    #include "crash_handler_noop.h"
+    namespace impl = crash_handler_noop;
 #endif
 
-void crash_handler(int sig);
-void print_stacktrace();
-void setup_crash_handler();
-
-inline void print_stacktrace() {
-#ifdef USE_LIBUNWIND
-    // Use libunwind for better stack traces
-    unw_cursor_t cursor;
-    unw_context_t context;
-
-    unw_getcontext(&context);
-    unw_init_local(&cursor, &context);
-
-    int depth = 0;
-    printf("Stack trace (libunwind):\n");
-    while (unw_step(&cursor) > 0) {
-        unw_word_t offset, pc;
-        char sym[256];
-
-        unw_get_reg(&cursor, UNW_REG_IP, &pc);
-        if (pc == 0) {
-            break;
-        }
-
-        printf("#%-2d 0x%lx:", depth++, pc);
-
-        if (unw_get_proc_name(&cursor, sym, sizeof(sym), &offset) == 0) {
-            printf(" (%s+0x%lx)\n", sym, offset);
-        } else {
-            printf(" -- symbol not found\n");
-        }
-    }
-#elif USE_EXECINFO
-    // Fallback to execinfo.h backtrace
-    void *buffer[100];
-    int nptrs;
-    char **strings;
-
-    printf("Stack trace (backtrace):\n");
-    nptrs = backtrace(buffer, 100);
-    strings = backtrace_symbols(buffer, nptrs);
-    
-    if (strings == nullptr) {
-        printf("backtrace_symbols failed\n");
-        return;
-    }
-
-    for (int j = 0; j < nptrs; j++) {
-        printf("#%-2d %s\n", j, strings[j]);
-    }
-
-    free(strings);
-#endif
+// Unified interface - these functions will dispatch to the appropriate implementation
+inline void crash_handler(int sig) {
+    impl::crash_handler(sig);
 }
 
-inline void crash_handler(int sig) {
-    fprintf(stderr, "Error: signal %d:\n", sig);
-    print_stacktrace();
-    exit(1);
+inline void print_stacktrace() {
+    impl::print_stacktrace();
 }
 
 inline void setup_crash_handler() {
-
-#ifdef USE_EXECINFO
-    // Install execinfo.h backtrace
-    backtrace_init();
-#elif USE_LIBUNWIND
-    // Install libunwind backtrace
-    unw_backtrace_init();
-#endif
+    impl::setup_crash_handler();
 }
 
 #endif // CRASH_HANDLER_H
