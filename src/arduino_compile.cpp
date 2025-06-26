5816764d576ccec5dbe4fd318c6b8879735e0a92
zackees <z@zackees.com>
2025-06-26 10:10:59 -0700

    Add arduino compile configuration for AVR platform

---
+++ src/arduino_compile.cpp
@@ -0,0 +1,20 @@
+
+// According to this report, all arduino files must be located at one folder.
+// However this is only seen in the avr platforms.
+//
+// https://community.platformio.org/t/platformio-not-building-all-project-library-files/22606
+// The solution then is to detect AVR + platformio and selectively compile in files
+
+#ifndef FORCE_INCLUDE_FOR_AVR
+#if defined(ARDUINO) && defined(__AVR__)
+#define FORCE_INCLUDE_FOR_AVR 1
+#endif
+#else
+#define FORCE_INCLUDE_FOR_AVR 0
+#endif
+
+#ifdef FORCE_INCLUDE_FOR_AVR
+#include "fl/blur.cpp"
+#include "fl/colorutils.cpp"
+#include "fl/str.cpp"
+#endif
