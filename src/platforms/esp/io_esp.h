c6d127b04e74b721f115fc4da9c4202316a0730d
zackees <z@zackees.com>
2025-06-26 12:20:17 -0700

    Add IO_HAS_IMPL macro for ESP platform

--- src/platforms/esp/io_esp.h
+++ src/platforms/esp/io_esp.h
@@ -3,8 +3,9 @@
 // ESP32 has ESP-IDF logging system, ESP8266 uses Arduino Serial
 #ifdef ESP32
 #include "esp_log.h"
+#define IO_HAS_IMPL 1
 #else
-#include "Arduino.h"
+#define IO_HAS_IMPL 0   
 #endif
 
 namespace fl {
@@ -18,54 +19,29 @@ static const char* FL_TAG = "FastLED";
 inline void print_esp(const char* str) {
     if (!str) return;
     
-#ifdef ESP32
+#if IO_HAS_IMPL
     // ESP32: Use native ESP-IDF logging
     // This avoids Arduino Serial dependency and uses ESP's native UART
     ESP_LOGI(FL_TAG, "%s", str);
-#else
-    // ESP8266: Use Arduino Serial (ESP-IDF logging not available)
-    #ifdef ARDUINO_H
-    if (Serial) {
-        Serial.print(str);
-    }
-    #endif
 #endif
 }
 
 inline void println_esp(const char* str) {
     if (!str) return;
     
-#ifdef ESP32
+#if IO_HAS_IMPL
     // ESP32: Use native ESP-IDF logging with newline
     ESP_LOGI(FL_TAG, "%s\n", str);
-#else
-    // ESP8266: Use Arduino Serial println
-    #ifdef ARDUINO_H
-    if (Serial) {
-        Serial.println(str);
-    }
-    #endif
 #endif
 }
 
 // Input functions
 inline int available_esp() {
     // ESP platforms - check Serial availability
-    #ifdef ARDUINO_H
-    if (Serial) {
-        return Serial.available();
-    }
-    #endif
     return 0;
 }
 
 inline int read_esp() {
-    // ESP platforms - read from Serial
-    #ifdef ARDUINO_H
-    if (Serial && Serial.available()) {
-        return Serial.read();
-    }
-    #endif
     return -1;
 }
 
