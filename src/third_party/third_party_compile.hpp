2075639b4f20b76205ddae184fba9e12caaf9f52
Zachary Vorhies <zachvorhies@protonmail.com>
2025-06-29 23:54:31 -0700

    kiss fft -> cpp

--- src/third_party/third_party_compile.hpp
+++ src/third_party/third_party_compile.hpp
@@ -6,4 +6,8 @@
 // THIRD PARTY MODULE IMPLEMENTATIONS
 #include "third_party/object_fled/src/OjectFLED.cpp.hpp"
 
+// KISS FFT IMPLEMENTATIONS
+#include "third_party/cq_kernel/kiss_fft.cpp.hpp"
+#include "third_party/cq_kernel/kiss_fftr.cpp.hpp"
+
 #endif // FASTLED_ALL_SRC
