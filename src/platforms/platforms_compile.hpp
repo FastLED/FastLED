15af71d75f990ff0305e5d4362600619c62226b7
zackees <z@zackees.com>
2025-06-29 20:48:50 -0700

    Exclude non-applicable files for __EMSCRIPTEN__ platform

--- src/platforms/platforms_compile.hpp
+++ src/platforms/platforms_compile.hpp
@@ -4,9 +4,11 @@
 #ifdef FASTLED_ALL_SRC
 
 // PLATFORMS MODULE IMPLEMENTATIONS (HIERARCHICAL)
+#if !defined(__EMSCRIPTEN__)  // non applicable files are stripped from this platform.
 #include "platforms/arm/arm_compile.hpp"  // 1 files
 #include "platforms/avr/avr_compile.hpp"  // 1 files
 #include "platforms/esp/esp_compile.hpp"  // 7 files
+#endif
 #include "platforms/shared/shared_compile.hpp"  // 14 files
 #include "platforms/wasm/wasm_compile.hpp"  // 9 files
 
