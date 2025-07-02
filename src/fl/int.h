0766306f87f8431fa385c4bc3ef1cb7df3726cf7
zackees <z@zackees.com>
2025-07-02 03:13:45 -0700

    Update int.h to typedef i32 as int for portability

--- src/fl/int.h
+++ src/fl/int.h
@@ -63,7 +63,7 @@ namespace fl {
         // Default: assume short 16-bit, int 32-bit (uint32_t is unsigned int)
         typedef short i16;
         typedef unsigned short u16;
-        typedef long i32;
+        typedef int i32;             // 'int' is 32-bit on all desktop and most embedded targets
         typedef unsigned int u32;
         typedef long long i64;
         typedef unsigned long long u64;
