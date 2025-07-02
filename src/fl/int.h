4da36a14fbb30f5c4cb07aa3fdf30f6d0adbc50d
zackees <z@zackees.com>
2025-07-02 03:40:08 -0700

    int fix for arduino giga

--- src/fl/int.h
+++ src/fl/int.h
@@ -68,6 +68,14 @@ namespace fl {
         typedef unsigned long u32;
         typedef long long i64;
         typedef unsigned long long u64;
+    #elif defined(ARDUINO_GIGA) || defined(ARDUINO_GIGA_M7)
+        // Arduino GIGA R1 (STM32H747) – uint32_t is 'unsigned long' on this toolchain
+        typedef short i16;
+        typedef unsigned short u16;
+        typedef long i32;
+        typedef unsigned long u32;
+        typedef long long i64;
+        typedef unsigned long long u64;
     #else
         // Default: assume short 16-bit, int 32-bit (uint32_t is unsigned int)
         typedef short i16;
