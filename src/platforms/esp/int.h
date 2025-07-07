4ebfbc5575e5923e5bbfa0eeb01caf29d0486001
Zachary Vorhies <zachvorhies@protonmail.com>
2025-07-06 19:02:02 -0700

    update

--- src/platforms/esp/int.h
+++ src/platforms/esp/int.h
@@ -8,8 +8,10 @@ namespace fl {
     // This matches the Xtensa toolchain behavior similar to ARM toolchains
     typedef short i16;
     typedef unsigned short u16;
-    typedef long i32;
-    typedef unsigned long u32;
+    //typedef long i32;
+    //typedef unsigned long u32;
+    typedef int32_t i32;
+    typedef uint32_t u32;
     typedef long long i64;
     typedef unsigned long long u64;
     // Use standard types directly to ensure exact compatibility
