b8dca55a5000f3cd3288f5b7c43698df9fc68ad2
zackees <z@zackees.com>
2025-07-09 03:20:39 -0700

    update type traits

--- src/fl/type_traits.h
+++ src/fl/type_traits.h
@@ -15,6 +15,33 @@ Provides eanble_if and is_derived for compilers before C++14.
 namespace fl { // mandatory namespace to prevent name collision with
                // std::enable_if.
 
+// Define integral_constant as base for true_type and false_type
+template <typename T, T v>
+struct integral_constant {
+    static constexpr T value = v;
+    using value_type = T;
+    using type = integral_constant;
+    constexpr operator value_type() const noexcept { return value; }
+    constexpr value_type operator()() const noexcept { return value; }
+};
+
+// Define true_type and false_type
+using true_type = integral_constant<bool, true>;
+using false_type = integral_constant<bool, false>;
+
+// Define add_rvalue_reference trait (remove_reference is already defined in move.h)
+template <typename T> struct add_rvalue_reference {
+    using type = T&&;
+};
+
+template <typename T> struct add_rvalue_reference<T&> {
+    using type = T&;
+};
+
+// Define declval for use in SFINAE expressions
+template <typename T>
+typename add_rvalue_reference<T>::type declval() noexcept;
+
 // Define enable_if for SFINAE
 template <bool Condition, typename T = void> struct enable_if {};
 
