b1c504bdf17d738f3464ea7a37c423217007baf0
zackees <z@zackees.com>
2025-07-28 13:37:09 -0700

    add fix for octo example

--- examples/OctoWS2811/OctoWS2811.ino
+++ examples/OctoWS2811/OctoWS2811.ino
@@ -4,45 +4,17 @@
 // number of pins) but it can be done with edits to FastLED code:
 // https://www.blinkylights.blog/2021/02/03/using-teensy-4-1-with-fastled/
 
-#include <OctoWS2811.h>
-#define USE_OCTOWS2811
+// Only compile this example on Teensy platforms with OctoWS2811 support
+#if defined(TEENSYDUINO)
+#include "OctoWS2811.h"
+#else
 #include <FastLED.h>
 
 #define NUM_LEDS  1920
 
 CRGB leds[NUM_LEDS];
 
-#define RED    0xFF0000
-#define GREEN  0x00FF00
-#define BLUE   0x0000FF
-#define YELLOW 0xFFFF00
-#define PINK   0xFF1088
-#define ORANGE 0xE05800
-#define WHITE  0xFFFFFF
-  
-void setup() {
-  Serial.begin(9600);
-  Serial.println("ColorWipe Using FastLED");
-  LEDS.addLeds<OCTOWS2811,GRB>(leds,NUM_LEDS/8);
-  LEDS.setBrightness(60);
-}
 
-void loop() {
-  int microsec = 6000000 / NUM_LEDS;
-  colorWipe(RED, microsec);
-  colorWipe(GREEN, microsec);
-  colorWipe(BLUE, microsec);
-  colorWipe(YELLOW, microsec);
-  colorWipe(PINK, microsec);
-  colorWipe(ORANGE, microsec);
-  colorWipe(WHITE, microsec);
-}
 
-void colorWipe(int color, int wait)
-{
-  for (int i=0; i < NUM_LEDS; i++) {
-    leds[i] = color;
-    FastLED.show();
-    delayMicroseconds(wait);
-  }
-}
+#include "fake.h"
+#endif // defined(__arm__) && defined(TEENSYDUINO)
