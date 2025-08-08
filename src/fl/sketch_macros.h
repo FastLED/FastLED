9174239e4042d5a72b7b3de0933a69e534f5f4e5
zackees <z@zackees.com>
2025-08-08 11:00:33 -0700

    set blue pill as a small memory board

--- src/fl/sketch_macros.h
+++ src/fl/sketch_macros.h
@@ -6,7 +6,8 @@
   || defined(__MK20DX256__) \
   || defined(STM32F1) \
   || defined(ESP8266) \
-  || defined(ARDUINO_ARCH_RENESAS_UNO)
+  || defined(ARDUINO_ARCH_RENESAS_UNO) \
+  || defined(ARDUINO_BLUEPILL_F103C8)
 #define SKETCH_HAS_LOTS_OF_MEMORY 0
 #else
 #define SKETCH_HAS_LOTS_OF_MEMORY 1
