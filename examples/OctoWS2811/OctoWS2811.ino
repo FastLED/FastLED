24a1748d286d6d762fa1f78934afeecf9bd6dcba
zackees <z@zackees.com>
2025-08-08 23:38:40 -0700

    make teensy lc not bind to octo ws2811 example

--- examples/OctoWS2811/OctoWS2811.ino
+++ examples/OctoWS2811/OctoWS2811.ino
@@ -3,7 +3,7 @@
 /// @example OctoWS2811.ino
 
 // Platform detection logic
-#if defined(__arm__) && defined(TEENSYDUINO)
+#if defined(__arm__) && defined(TEENSYDUINO) && !defined(__MKL26Z64__)  // Teensy LC
 #include "OctoWS2811_impl.h"
 #else
 #include "platforms/sketch_fake.hpp"
