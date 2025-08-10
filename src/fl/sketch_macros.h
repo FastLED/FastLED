5635382e7c374faed8d7af72a0423b2801ea7df5
zackees <z@zackees.com>
2025-08-09 18:14:31 -0700

    attiny ARDUINO_attinyxy6 is now a board without much memory

--- src/fl/sketch_macros.h
+++ src/fl/sketch_macros.h
@@ -4,6 +4,7 @@
   || defined(__AVR_ATtiny85__) \
   || defined(__AVR_ATmega32U4__) \
   || defined(__AVR_ATtiny88__) \
+  || defined(ARDUINO_attinyxy6) \
   || defined(ARDUINO_TEENSYLC) \
   || defined(ARDUINO_TEENSY30) \
   || defined(__MK20DX128__) \
