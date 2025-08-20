13354b799dfd69dcbb43e1a1e25d92310132a2ab
zackees <z@zackees.com>
2025-08-20 16:07:47 -0700

    add test now for AdafruitApiBridge

--- src/platforms/adafruit/clockless.h
+++ src/platforms/adafruit/clockless.h
@@ -17,26 +17,12 @@
 #include "fl/unique_ptr.h"
 #include "eorder.h"
 #include "pixel_controller.h"
+#include "platforms/adafruit/driver.h"
 
 class PixelIterator;
 
 namespace fl {
 
-/// Interface for Adafruit NeoPixel driver - implementation in clockless.cpp
-class IAdafruitNeoPixelDriver {
-public:
-    /// Static factory method to create driver implementation
-    static fl::unique_ptr<IAdafruitNeoPixelDriver> create();
-
-    virtual ~IAdafruitNeoPixelDriver() = default;
-    
-    /// Initialize the driver with data pin and RGBW mode
-    virtual void init(int dataPin) = 0;
-    
-    /// Output pixels to the LED strip
-    virtual void showPixels(PixelIterator& pixelIterator) = 0;
-
-};
 
 /// WS2812/NeoPixel clockless controller using Adafruit_NeoPixel library as the underlying driver
 /// 
