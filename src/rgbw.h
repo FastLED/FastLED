871b64cb3e063d23e33707708f8c4a991da79395
zackees <z@zackees.com>
2025-08-22 12:41:09 -0700

    rgbw namespace fix

--- src/rgbw.h
+++ src/rgbw.h
@@ -5,37 +5,19 @@
 
 #include "fl/rgbw.h"
 
-using Rgbw = fl::Rgbw;
-using RgbwInvalid = fl::RgbwInvalid;
-using RgbwDefault = fl::RgbwDefault;
-using RgbwWhiteIsOff = fl::RgbwWhiteIsOff;
-using RGBW_MODE = fl::RGBW_MODE;
+// Since FastLED 3.10.3  the RGBW functions are in the fl namespace.
+// This file is a compatibility layer to allow the old functions to be used.
+// It is not necessary to include this file if you are using FastLED 3.10.3 or later.
 
-enum {
-    kRGBWDefaultColorTemp = fl::kRGBWDefaultColorTemp,
-};
-
-constexpr RGBW_MODE kRGBWInvalid = fl::kRGBWInvalid;
-constexpr RGBW_MODE kRGBWNullWhitePixel = fl::kRGBWNullWhitePixel;
-constexpr RGBW_MODE kRGBWExactColors = fl::kRGBWExactColors;
-constexpr RGBW_MODE kRGBWBoostedWhite = fl::kRGBWBoostedWhite;
-constexpr RGBW_MODE kRGBWMaxBrightness = fl::kRGBWMaxBrightness;
-constexpr RGBW_MODE kRGBWUserFunction = fl::kRGBWUserFunction;
-
-
-template <fl::RGBW_MODE MODE>
-void rgb_2_rgbw(uint16_t w_color_temperature, uint8_t r, uint8_t g, uint8_t b,
-                uint8_t r_scale, uint8_t g_scale, uint8_t b_scale,
-                uint8_t *out_r, uint8_t *out_g, uint8_t *out_b,
-                uint8_t *out_w) {
-    fl::rgb_2_rgbw(MODE, w_color_temperature, r, g, b, r_scale, g_scale,
-                   b_scale, out_r, out_g, out_b, out_w);
-}
-
-inline void rgbw_partial_reorder(fl::EOrderW w_placement, uint8_t b0,
-                                 uint8_t b1, uint8_t b2, uint8_t w,
-                                 uint8_t *out_b0, uint8_t *out_b1,
-                                 uint8_t *out_b2, uint8_t *out_b3) {
-    fl::rgbw_partial_reorder(w_placement, b0, b1, b2, w, out_b0, out_b1, out_b2,
-                             out_b3);
-}
+using fl::Rgbw;
+using fl::RgbwInvalid;
+using fl::RgbwDefault;
+using fl::RgbwWhiteIsOff;
+using fl::RGBW_MODE;
+using fl::kRGBWDefaultColorTemp;
+using fl::kRGBWInvalid;
+using fl::kRGBWNullWhitePixel;
+using fl::kRGBWExactColors;
+using fl::kRGBWBoostedWhite;
+using fl::kRGBWMaxBrightness;
+using fl::kRGBWUserFunction;
