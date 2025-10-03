0be3503b97453442d72f60d0d04e0bd62e9eb7a1
zackees <z@zackees.com>
2025-10-02 18:42:39 -0700

    feat(chipsets): add ESP32-S3 LCD driver support for WS2812 chipset

--- src/platforms/chipsets_specialized_ws2812.h
+++ src/platforms/chipsets_specialized_ws2812.h
@@ -40,7 +40,7 @@ class WS2812Controller800Khz:
 		RGB_ORDER
 	> {};
 #define FASTLED_WS2812_HAS_SPECIAL_DRIVER 1
-#elif defined(FASTLED_ESP32_LCD_DRIVER)
+#elif defined(FASTLED_ESP32_LCD_DRIVER) && defined(CONFIG_IDF_TARGET_ESP32S3)
 #include "platforms/esp/32/clockless_lcd_esp32s3.h"
 template <fl::u8 DATA_PIN, EOrder RGB_ORDER = fl::GRB>
 class WS2812Controller800Khz:
