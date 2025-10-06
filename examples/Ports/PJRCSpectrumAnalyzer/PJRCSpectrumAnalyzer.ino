3fe69466e3a671392aa55dc9b1dcfed2d7979ab0
zackees <z@zackees.com>
2025-10-06 04:04:25 -0700

    feat(PJRCSpectrumAnalyzer): add platform detection and memory requirements

--- examples/Ports/PJRCSpectrumAnalyzer/PJRCSpectrumAnalyzer.ino
+++ examples/Ports/PJRCSpectrumAnalyzer/PJRCSpectrumAnalyzer.ino
@@ -2,8 +2,12 @@
 /// @brief   PJRCSpectrumAnalyzer example with platform detection
 /// @example PJRCSpectrumAnalyzer.ino
 
+#include "fl/sketch_macros.h"
+
 // Platform detection logic
-#if defined(__arm__) && defined(TEENSYDUINO)
+// Requires lots of memory for 60x32 LED matrix + Audio library + FFT
+// SKETCH_HAS_LOTS_OF_MEMORY = 0 for Teensy 3.0/3.1/3.2 (all have insufficient RAM)
+#if defined(__arm__) && defined(TEENSYDUINO) && SKETCH_HAS_LOTS_OF_MEMORY
 #include "PJRCSpectrumAnalyzer.h"
 #else
 #include "platforms/sketch_fake.hpp"
