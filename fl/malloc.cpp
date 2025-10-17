c8cc15a5cbd7936147744fd532871813c73baf63
zackees <z@zackees.com>
2025-10-17 01:11:59 -0700

    refactor(malloc): remove unnecessary include and use size_t directly

--- fl/malloc.cpp
+++ fl/malloc.cpp
@@ -1,18 +1,18 @@
 // fl_malloc.cpp  (implementation file)
 // C++11, AVR- and bare-metal-friendly
 
-#include <cstddef>   // size_t (C++ header; harmless even on old toolchains)
+
 #include <stdlib.h>  // ::malloc, ::realloc, ::free
 
 #include "fl/malloc.h"
 
 namespace fl {
 
-void* malloc(std::size_t size) noexcept {
+void* malloc(size_t size) noexcept {
     return ::malloc(size);
 }
 
-void* realloc(void* ptr, std::size_t size) noexcept {
+void* realloc(void* ptr, size_t size) noexcept {
     return ::realloc(ptr, size);
 }
 
