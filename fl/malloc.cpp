a88fd99139b5d00c2831d33ebca6cc81d0b00595
zackees <z@zackees.com>
2025-10-17 01:11:10 -0700

    refactor(malloc): simplify platform-specific checks

--- fl/malloc.cpp
+++ fl/malloc.cpp
@@ -1,36 +1,23 @@
-#if defined(__APPLE__)
-#include <cstdlib>
-#else
-#include <malloc.h>
-#endif
+// fl_malloc.cpp  (implementation file)
+// C++11, AVR- and bare-metal-friendly
+
+#include <cstddef>   // size_t (C++ header; harmless even on old toolchains)
+#include <stdlib.h>  // ::malloc, ::realloc, ::free
 
 #include "fl/malloc.h"
 
-
 namespace fl {
 
-void* malloc(size_t size) {
-#if defined(__APPLE__)
-   return std::malloc(size);
-#else
-   return ::malloc(size);
-#endif
+void* malloc(std::size_t size) noexcept {
+    return ::malloc(size);
 }
 
-void* realloc(void* ptr, size_t size) {
-#if defined(__APPLE__)
-   return std::realloc(ptr, size);
-#else
-   return ::realloc(ptr, size);
-#endif
+void* realloc(void* ptr, std::size_t size) noexcept {
+    return ::realloc(ptr, size);
 }
 
-void free(void* ptr) {
-#if defined(__APPLE__)
-   std::free(ptr);
-#else
-   ::free(ptr);
-#endif
+void free(void* ptr) noexcept {
+    ::free(ptr);
 }
 
 }  // namespace fl
