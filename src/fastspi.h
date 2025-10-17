c2e69b7f6482cdbfda7a092217e573c34a8a2f55
zackees <z@zackees.com>
2025-10-17 13:45:52 -0700

    refactor(fastspi): support ESP32 hardware SPI

--- src/fastspi.h
+++ src/fastspi.h
@@ -47,7 +47,7 @@ class SPIOutput : public fl::StubSPIOutput {};
 
 #else
 
-#if !defined(FASTLED_ALL_PINS_HARDWARE_SPI)
+#if !defined(FASTLED_ALL_PINS_HARDWARE_SPI) && !defined(ESP32)
 /// Hardware SPI output
 template<fl::u8 _DATA_PIN, fl::u8 _CLOCK_PIN, fl::u32 _SPI_CLOCK_DIVIDER>
 class SPIOutput : public AVRSoftwareSPIOutput<_DATA_PIN, _CLOCK_PIN, _SPI_CLOCK_DIVIDER> {};
