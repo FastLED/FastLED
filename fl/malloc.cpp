94f4c294abf99f6fe67319a99320741e9c3c372c
zackees <z@zackees.com>
2025-10-17 01:05:11 -0700

    fix(malloc): add platform-specific handling for malloc/free on macOS

--- fl/malloc.cpp
+++ fl/malloc.cpp
@@ -1,4 +1,8 @@
+#if defined(__APPLE__)
+#include <cstdlib>
+#else
 #include <malloc.h>
+#endif
 
 #include "fl/malloc.h"
 
@@ -6,15 +10,27 @@
 namespace fl {
 
 void* malloc(size_t size) {
+#if defined(__APPLE__)
+   return std::malloc(size);
+#else
    return ::malloc(size);
+#endif
 }
 
 void* realloc(void* ptr, size_t size) {
+#if defined(__APPLE__)
+   return std::realloc(ptr, size);
+#else
    return ::realloc(ptr, size);
+#endif
 }
 
 void free(void* ptr) {
+#if defined(__APPLE__)
+   std::free(ptr);
+#else
    ::free(ptr);
+#endif
 }
 
 }  // namespace fl
