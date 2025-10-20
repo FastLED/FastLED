7ec1128b75594f21d26b7fb0be75cbb44daad218
zackees <z@zackees.com>
2025-10-19 18:07:51 -0700

    refactor(delay): optimize delaycycles for ESP32

--- src/fl/delay.cpp
+++ src/fl/delay.cpp
@@ -72,33 +72,6 @@ template<> FASTLED_FORCE_INLINE void delaycycles<5>() {
   FL_NOP;
 }
 
-// Specialization for a gigantic amount of cycles on ESP32
-#if defined(ESP32)
-template<> FASTLED_FORCE_INLINE void delaycycles<4294966398>() {
-  // specialization for a gigantic amount of cycles, apparently this is needed
-  // or esp32 will blow the stack with cycles = 4294966398.
-  const fl::u32 termination = 4294966398 / 10;
-  const fl::u32 remainder = 4294966398 % 10;
-  for (fl::u32 i = 0; i < termination; i++) {
-    FL_NOP; FL_NOP; FL_NOP; FL_NOP; FL_NOP;
-    FL_NOP; FL_NOP; FL_NOP; FL_NOP; FL_NOP;
-  }
-
-  // remainder
-  switch (remainder) {
-    case 9: FL_NOP;
-    case 8: FL_NOP;
-    case 7: FL_NOP;
-    case 6: FL_NOP;
-    case 5: FL_NOP;
-    case 4: FL_NOP;
-    case 3: FL_NOP;
-    case 2: FL_NOP;
-    case 1: FL_NOP;
-  }
-}
-#endif
-
 // ============================================================================
 // Millisecond and Microsecond delay implementations
 // ============================================================================
