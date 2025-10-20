6bc5233b67a507aa2d4f2ba3cebfcfe4b53a4dd3
zackees <z@zackees.com>
2025-10-20 15:08:59 -0700

    platform fix

--- src/platforms/esp/delay_cycles_esp32.h
+++ src/platforms/esp/delay_cycles_esp32.h
@@ -33,9 +33,9 @@ inline void delaycycles();
 /// This prevents stack overflow on ESP32 with cycles = 4294966398
 template<>
 FASTLED_FORCE_INLINE void delaycycles<4294966398>() {
-  const u32 termination = 4294966398 / 10;
-  const u32 remainder = 4294966398 % 10;
-  for (u32 i = 0; i < termination; i++) {
+  const fl::u32 termination = 4294966398 / 10;
+  const fl::u32 remainder = 4294966398 % 10;
+  for (fl::u32 i = 0; i < termination; i++) {
     FL_NOP;
     FL_NOP;
     FL_NOP;
