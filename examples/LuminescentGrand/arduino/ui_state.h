53bffacd6429ad2fe4058f4eba993678555a187c
zackees <z@zackees.com>
2025-10-20 21:01:52 -0700

    fix lumin grrand

--- examples/LuminescentGrand/arduino/ui_state.h
+++ examples/LuminescentGrand/arduino/ui_state.h
@@ -9,7 +9,7 @@ extern ColorSelector color_selector;
 extern CountingButton vis_selector;
 
 struct ui_state {
-  ui_state() { memset(this, 0, sizeof(*this)); }
+  ui_state() { fl::memset(this, 0, sizeof(*this)); }
   int which_visualizer;
   int color_scheme;
   //float color_wheel_pos;
