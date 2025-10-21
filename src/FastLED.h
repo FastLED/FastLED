459b9e0ef86985d41e9f566f365c22d7d98ddbfd
zackees <z@zackees.com>
2025-10-20 20:26:07 -0700

    feat(FastLED): add delay functions from fl:: namespace

--- src/FastLED.h
+++ src/FastLED.h
@@ -974,6 +974,10 @@ template<typename T> using fl_vector = fl::vector<T>;
 template<typename Key, typename Value, typename Compare = fl::less<Key>> using fl_map = fl::fl_map<Key, Value, Compare>;
 using fl_string = fl::string;
 
+// Delay function from fl:: namespace
+using fl::delayMicroseconds;
+using fl::delayMillis;
+
 #define FASTLED_TITLE(text) fl::UITitle g_title(text)
 #define FASTLED_DESCRIPTION(text) fl::UIDescription g_description(text)
 #define FASTLED_HELP(text) fl::UIHelp g_help(text)
