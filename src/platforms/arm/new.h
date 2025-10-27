5f15d8eb5b7e05b936b52a25bb2cf750b62cc065
zackees <z@zackees.com>
2025-10-27 15:26:41 -0700

    feat(platform): add support for modern ARM platforms with <new> header

--- src/platforms/arm/new.h
+++ src/platforms/arm/new.h
@@ -2,18 +2,15 @@
 #pragma once
 
 // ARM placement new operator - in global namespace
-// Most ARM platforms have <new> header, handle exceptions like STM32F1
+// Most ARM platforms have <new> header available
 
-#if defined(__STM32F1__)
-    // Roger Clark STM32 has broken/missing placement new
-    #include "platforms/arm/inplacenew.h"
-#elif !defined(__has_include)
+#if !defined(__has_include)
     // Platforms without __has_include support - assume no <new> header
     #include "platforms/arm/inplacenew.h"
 #elif __has_include(<new>)
-    // Modern ARM platforms with standard library support
+    // Modern ARM platforms with standard library support (includes STM32F1 with modern toolchains)
     #include <new>
 #else
-    // Fallback to manual definition
+    // Fallback to manual definition for platforms without <new> header
     #include "platforms/arm/inplacenew.h"
 #endif
