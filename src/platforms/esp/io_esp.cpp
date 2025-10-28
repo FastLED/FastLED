983608cef13e912ec7d4f47cb25e95267c1f4d02
zackees <z@zackees.com>
2025-10-27 18:05:49 -0700

    refactor(io_esp): simplify print and println functions

--- src/platforms/esp/io_esp.cpp
+++ src/platforms/esp/io_esp.cpp
@@ -9,22 +9,14 @@ void print_esp(const char* str) {
 
     // ESP32/ESP8266: Use Arduino Serial library
     // This is lightweight and avoids POSIX stdio linker issues
-    #ifdef ARDUINO_H
-    if (Serial) {
-        Serial.print(str);
-    }
-    #endif
+    Serial.print(str);
 }
 
 void println_esp(const char* str) {
     if (!str) return;
 
     // ESP32/ESP8266: Use Arduino Serial library with newline
-    #ifdef ARDUINO_H
-    if (Serial) {
-        Serial.println(str);
-    }
-    #endif
+    Serial.println(str);
 }
 
 // Input functions
