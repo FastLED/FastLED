cd06bc8d2a2ff7d26cd94aa379567a417a8a0611
zackees <z@zackees.com>
2025-11-15 14:29:44 -0800

    refactor(fl): remove deprecated function_list.h header

--- src/fl/function_list.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// DEPRECATED: FunctionList has been moved to ftl/function.h
-// This header is kept for backward compatibility but will be removed in a future version.
-// Please update your includes to use:
-//   #include "ftl/function.h"
-
-#include "ftl/function.h"
