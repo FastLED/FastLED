d8b523568615fe753572033607ff9c0c86b02ef6
zackees <z@zackees.com>
2025-12-11 21:30:56 -0800

    update for delay.h

--- src/fl/delay.h
+++ src/fl/delay.h
@@ -34,7 +34,23 @@ u32 esp_clk_cpu_freq_impl();
 
 // Helper macro to get compile-time CPU frequency
 // Note: Uses preprocessor for compatibility with constexpr functions in C++11
-#if defined(F_CPU)
+#if defined(STM32F2XX)
+  // STM32F2: 120 MHz
+  #define GET_CPU_FREQUENCY() 120000000UL
+#elif defined(STM32F4)
+  // STM32F4: 100 MHz
+  #define GET_CPU_FREQUENCY() 100000000UL
+#elif defined(STM32F1) || defined(__STM32F1__) || defined(STM32F10X_MD)
+  // STM32F1: 72 MHz
+  #define GET_CPU_FREQUENCY() 72000000UL
+#elif defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__)
+  // Other ARM Cortex-M3/M4: check F_CPU or use conservative 72 MHz default
+  #if defined(F_CPU) && (F_CPU > 0)
+    #define GET_CPU_FREQUENCY() F_CPU
+  #else
+    #define GET_CPU_FREQUENCY() 72000000UL
+  #endif
+#elif defined(F_CPU)
   #define GET_CPU_FREQUENCY() F_CPU
 #elif defined(CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ)
   // ESP-IDF style (can be tuned via menuconfig)
