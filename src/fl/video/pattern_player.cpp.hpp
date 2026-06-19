57c778fcf41a49aba80f18be3bac8e2e40e3f4e8
David-paul O Folorunsho-roberts <timifroberts@gmail.com>
2026-06-19 22:10:01 +0100

    feat(video): add fl::PatternPlayer high-level mapped video file player

---
+++ src/fl/video/pattern_player.cpp.hpp
@@ -0,0 +1,84 @@
+#include "fl/video/pattern_player.h"
+#include "fl/stl/chrono.h"
+
+namespace fl {
+namespace video {
+
+PatternPlayer::PatternPlayer(const char* filepath)
+    : mFilepath(filepath),
+      mHasScreenMap(false),
+      mLastDrawTime(0),
+      mFps(30.0f) {
+}
+
+PatternPlayer::~PatternPlayer() FL_NO_EXCEPT {
+    end();
+}
+
+bool PatternPlayer::begin(int cs_pin, int numLeds, float fps) {
+    if (!mFs.beginSd(cs_pin)) {
+        return false;
+    }
+    return begin(mFs, numLeds, fps);
+}
+
+bool PatternPlayer::begin(FileSystem& fs, int numLeds, float fps) {
+    mFs = fs;
+    mFps = fps;
+    mVideo = mFs.openVideo(mFilepath.c_str(), numLeds, fps, 2);
+    if (!mVideo) {
+        return false;
+    }
+    if (mVideo.hasEmbeddedScreenMap()) {
+        const fl::string& json = mVideo.embeddedScreenMapJson();
+        fl::string parseErr;
+        if (ScreenMap::ParseJson(json.c_str(), "strip1", &mScreenMap, &parseErr)) {
+            mHasScreenMap = true;
+            return true;
+        }
+    }
+    fl::string sidecarPath = mFilepath;
+    size_t lastSlash = sidecarPath.find_last_of('/');
+    if (lastSlash != fl::string::npos) {
+        sidecarPath = sidecarPath.substr(0, lastSlash + 1) + "screenmap.json";
+    } else {
+        sidecarPath = "screenmap.json";
+    }
+    if (mFs.readScreenMap(sidecarPath.c_str(), "strip1", &mScreenMap)) {
+        mHasScreenMap = true;
+    }
+    return true;
+}
+
+void PatternPlayer::end() {
+    if (mVideo) {
+        mVideo.end();
+    }
+    mFs.end();
+    mHasScreenMap = false;
+}
+
+bool PatternPlayer::hasNewFrame() const {
+    if (!mVideo) {
+        return false;
+    }
+    return (fl::millis() - mLastDrawTime) >= (1000.0f / mFps);
+}
+
+void PatternPlayer::drawFrame(CRGB* leds, int numLeds) {
+    if (mVideo) {
+        mLastDrawTime = fl::millis();
+        mVideo.draw(mLastDrawTime, fl::span<CRGB>(leds, numLeds));
+    }
+}
+
+const ScreenMap& PatternPlayer::getScreenMap() const {
+    return mScreenMap;
+}
+
+bool PatternPlayer::hasScreenMap() const {
+    return mHasScreenMap;
+}
+
+} // namespace video
+} // namespace fl
---
+++ src/fl/video/pattern_player.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "fl/system/file_system.h"
+#include "fl/fx/video.h"
+#include "fl/math/screenmap.h"
+#include "fl/stl/string.h"
+#include "fl/stl/noexcept.h"
+
+namespace fl {
+namespace video {
+
+class PatternPlayer {
+public:
+    PatternPlayer(const char* filepath);
+    ~PatternPlayer() FL_NO_EXCEPT;
+
+    bool begin(int cs_pin, int numLeds, float fps = 30.0f);
+    bool begin(FileSystem& fs, int numLeds, float fps = 30.0f);
+    void end();
+
+    bool hasNewFrame() const;
+    void drawFrame(CRGB* leds, int numLeds);
+
+    const ScreenMap& getScreenMap() const;
+    bool hasScreenMap() const;
+
+private:
+    fl::string mFilepath;
+    FileSystem mFs;
+    Video mVideo;
+    ScreenMap mScreenMap;
+    bool mHasScreenMap;
+    fl::u32 mLastDrawTime;
+    float mFps;
+};
+
+} // namespace video
+
+using PatternPlayer = video::PatternPlayer;
+
+} // namespace fl
