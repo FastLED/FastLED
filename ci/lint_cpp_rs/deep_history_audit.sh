22148c72bbfa9adbe7a0d8eb1e6d6b67b49234f3
zackees <z@zackees.com>
2026-08-25 18:27:42 -0700

    test(lint): keep history audits out of CI

--- ci/lint_cpp_rs/deep_history_audit.sh
+++ ci/lint_cpp_rs/deep_history_audit.sh
@@ -6,5 +6,5 @@ cd "$project_root"
 
 uv run soldr --no-cache cargo test \
   --manifest-path ci/lint_cpp_rs/Cargo.toml \
-  deep_live_history_matches_per_file_git_for_mark_and_sam \
+  live_history \
   -- --ignored --nocapture
